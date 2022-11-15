93d030d9fd67909d76ae686f0713d18343f632d1
Your Name <you@example.com>
2022-11-15 13:35:40 +0100

    a

---
+++ scrip.js
@@ -0,0 +1,22 @@
+
+function nombre() {  
+        texto="eduardorodriguez";
+       
+        for (i = 0; i<=texto.lenght;i ++){
+
+            if(i % 2 ==0){
+
+        document.querySelector("#h1").innerHTML=document.querySelector("#h1").innerHTML + texto.sub
+        }else{
+
+
+        }
+
+            } 
+
+
+
+}
+ 
+
+}
