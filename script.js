645ad6414acf6872448e2cca2e66d16e455e4a81
Your Name <you@example.com>
2022-11-15 18:34:40 +0100

    primer ejercicio javascript

---
+++ script.js
@@ -0,0 +1,12 @@
+function minombreEnMayusculas() {
+    texto = "oscarpereaalvarez"
+    for (i = 0; i < texto.length; i++) {
+        if (i % 2 == 0) {
+            document.querySelector("#h1").innerHTML = document.querySelector("#h1").innerHTML + texto.substr(i,1).toLowerCase();
+}
+else
+            document.querySelector("#h1").innerHTML= document.querySelector("#h1").innerHTML + texto.substring(i, i+1).toUpperCase(); 
+
+        }
+
+    }
