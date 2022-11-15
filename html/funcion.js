f434144138649ccbffcbc5a883107d656bd39fea
Your Name <you@example.com>
2022-11-15 11:48:16 +0100

    m2

---
+++ html/funcion.js
@@ -0,0 +1,19 @@
+function ejercicioFor(){
+    texto="edu palomino"
+    alert("longitud: "+texto.lenght-1)
+    //alert(texto.substring(0,6).toUpperCase());
+
+        for (let i=1;i<=11;i++){
+            if(1%2==0){
+                document.querySelector("#h1").innerHTML=
+                document.querySelector("#1").innerHTML+i;
+                texto.substring().toUpperCase();
+
+            }else{
+                document.querySelector("#h1").innerHTML=
+                document.querySelector("#h1").innerHTML+i;
+                texto.substring(texto)
+            }
+            alert(texto);
+        }
+}
