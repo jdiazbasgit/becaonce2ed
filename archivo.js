eedf1ee84253b4c2aef10f7b760bc23699f241f3
Elruedas001 <116722622+Elruedas001@users.noreply.github.com>
2022-11-16 09:19:31 +0100

    a

---
+++ archivo.js
@@ -0,0 +1,9 @@
+function NombreAlterno() {
+    texto = "denisflorinilban"
+    for (i = 0; i < text.length; i++) {
+        if (i % 2 === 0) {
+            document.querySelector("#h1").innerHTML = document.querySelector("#h1").innerHTML + texto.subspring(i, i + 1).toUpperCase();
+            document.querySelector("#h1").innerHTML = document.querySelector("#h1").innerHTML + texto.subspring(i, i + 1).toLowerCase();
+        }
+    }
+}
