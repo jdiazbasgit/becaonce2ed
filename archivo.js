db155116e2f6743cfcbf390eb1cb6f2ea25f0e86
Elruedas001 <116722622+Elruedas001@users.noreply.github.com>
2022-11-16 09:34:23 +0100

    a

--- archivo.js
+++ archivo.js
@@ -2,9 +2,9 @@ function NombreAlterno() {
     texto = "denisflorinilban"
     for (i = 0; i < text.length; i++) {
         if (i % 2 == 0) {
-            document.querySelector("#h1").innerHTML = document.querySelector("#h1").innerHTML + texto.substring(i, i + 1).toUpperCase();
+            document.querySelector("#h1").innerHTML = document.querySelector("#h1").innerHTML + texto.substring(i, i + 1).tolowerCase();
         }
         else
-            document.querySelector("#h1").innerHTML = document.querySelector("#h1").innerHTML + texto.substring(i, i + 1).toLowerCase();
+            document.querySelector("#h1").innerHTML = document.querySelector("#h1").innerHTML + texto.substring(i, i + 1).toUpperCase();
         }
     }
