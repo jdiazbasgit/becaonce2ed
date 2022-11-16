6d2fb348af4b9c5deb0278a5c873e072d80971bd
edupc1 <edupalomino.to@gmail.com>
2022-11-16 22:18:48 +0100

    numeroprimo1

--- numeroPrimos.js
+++ numeroPrimos.js
@@ -8,31 +8,43 @@
    // recorro en array de numeros y por cada uno de los numero primos creamos un tr, un td, le damos contenido al td con el valor del numero primo 
    //añadimos el td al tr
    //añadimos el tr a la tabla
-var tabla= document.querySelector("#tabla");
+var tabla= document.querySelector("#tabla1");
 var cadena = 100;
 var j = 2;
-var primos = [];
+var numerosPrimos = [];
+var contador=0;
 
 for (j=2; j < cadena; j++) {
 
-  if (primo(j)) {
-    primos.push(j);
+  if (crearFilaPrimo(j)) {
+    numerosPrimos.push(j);
   }
   
 }
-
-
+//console.log(numerosPrimos);
 
 function primo(numero) {
-
+  var tr=document.createElement("tr");
+  var td=document.createElement("td");
+  td.innerHTML="primo" ;
+  tr.appendChild(td);
   for (var i = 2; i < numero; i++) {
 
     if (numero % i === 0) {
       return false;
+      tabla.appendChild(tr);
+      i++;
     }
 
   }
 
   return numero !== 1;
 }
+
+function crearFilaPrimo(){
+  //primo();
+}
+
+
+
 
