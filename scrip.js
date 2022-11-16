2de822791c0b2fdca98f013cdfe3a0bd7a1cefcd
Your Name <you@example.com>
2022-11-16 08:57:26 +0100

    a

--- scrip.js
+++ scrip.js
@@ -1,22 +1,13 @@
 
 function nombre() {  
-        texto="eduardorodriguez";
-       
+        texto="eduardoRodriguez"     
         for (i = 0; i<=texto.lenght;i ++){
-
             if(i % 2 ==0){
 
-        document.querySelector("#h1").innerHTML=document.querySelector("#h1").innerHTML + texto.sub
+            document.querySelector("#h1").innerHTML=  document.querySelector("#h1").innerHTML + texto.substring(i, i+1).toUpperCase(); 
         }else{
-
-
+            document.querySelector("#h1").innerHTML=  document.querySelector("#h1").innerHTML + texto.substring(i, i+1).toUpperCase();
         }
 
             } 
-
-
-
-}
- 
-
 }
