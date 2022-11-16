d347a19a0fc40ce00cb50ac6b51a347bf0667a0a
OscarP72 <116722711+OscarP72@users.noreply.github.com>
2022-11-16 12:12:52 +0100

    ejercicio java

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ function minombreEnMayusculas() {
     texto = "oscarpereaalvarez"
     for (i = 0; i < texto.length; i++) {
         if (i % 2 == 0) {
-            document.querySelector("#h1").innerHTML = document.querySelector("#h1").innerHTML + texto.substring(i, i+1).toLowerCase();
-}
-else
-            document.querySelector("#h1").innerHTML= document.querySelector("#h1").innerHTML + texto.substring(i, i+1).toUpperCase(); 
-
+            document.querySelector("#h1").innerHTML = document.querySelector("#h1").innerHTML + texto.substring(i, i + 1).toLowerCase();
         }
+        else
+            document.querySelector("#h1").innerHTML = document.querySelector("#h1").innerHTML + texto.substring(i, i + 1).toUpperCase();
 
     }
+
+}
