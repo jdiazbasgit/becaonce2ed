cac352ce081ec331653e14ffb9eab9319817dd98
Elruedas001 <116722622+Elruedas001@users.noreply.github.com>
2022-11-16 12:13:29 +0100

    s

--- archivo.js
+++ archivo.js
@@ -4,7 +4,8 @@ function NombreAlterno() {
         if (i % 2 == 0) {
             document.querySelector("#h1").innerHTML = document.querySelector("#h1").innerHTML + texto.substring(i, i + 1).toLowerCase();
         }
-        else
+        else {
             document.querySelector("#h1").innerHTML = document.querySelector("#h1").innerHTML + texto.substring(i, i + 1).toUpperCase();
         }
     }
+}
