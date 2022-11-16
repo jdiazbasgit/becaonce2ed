9907ae1c4020a1eb2856ca8337968e33f9c2ef86
OscarP72 <116722711+OscarP72@users.noreply.github.com>
2022-11-16 12:13:08 +0100

    ejercicio ja

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ function minombreEnMayusculas() {
         if (i % 2 == 0) {
             document.querySelector("#h1").innerHTML = document.querySelector("#h1").innerHTML + texto.substring(i, i + 1).toLowerCase();
         }
-        else
+        else {
             document.querySelector("#h1").innerHTML = document.querySelector("#h1").innerHTML + texto.substring(i, i + 1).toUpperCase();
-
+        }
     }
 
 }
