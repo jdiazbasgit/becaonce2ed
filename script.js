9852702349b3c6c1cfa57e98a76e289fe24472ce
OscarP72 <116722711+OscarP72@users.noreply.github.com>
2022-11-16 09:01:15 +0100

    ejercicio corregido

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function minombreEnMayusculas() {
     texto = "oscarpereaalvarez"
     for (i = 0; i < texto.length; i++) {
         if (i % 2 == 0) {
-            document.querySelector("#h1").innerHTML = document.querySelector("#h1").innerHTML + texto.substr(i,1).toLowerCase();
+            document.querySelector("#h1").innerHTML = document.querySelector("#h1").innerHTML + texto.substring(i, i+1).toLowerCase();
 }
 else
             document.querySelector("#h1").innerHTML= document.querySelector("#h1").innerHTML + texto.substring(i, i+1).toUpperCase(); 
