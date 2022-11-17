6dd2be6c90610a427f306917c1efe06b635fac58
Your Name <you@example.com>
2022-11-17 14:17:35 +0100

    ejercico js

---
+++ ejercicio/primos.js
@@ -0,0 +1,20 @@
+
+primos = [1, 2, 3]
+function numerosPrimos() {
+
+    for (let i = 4; i < 100; i++) {
+        soyPrimo = true
+
+        for (let j = i-1; j > 1; j--) {
+            if (i % j == 0) {
+                soyPrimo = false;
+                break
+            }
+        }
+        if (soyPrimo) {
+            primos.push(i)
+        }
+    }
+    console.log(primos)
+
+}
