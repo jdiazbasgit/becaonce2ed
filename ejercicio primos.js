e7f606d9dd2f6c0dd13e7c772be22c5ebdac0fab
Elruedas001 <116722622+Elruedas001@users.noreply.github.com>
2022-11-17 14:00:48 +0100

    a

--- ejercicio primos.js
+++ ejercicio primos.js
@@ -1,12 +1,18 @@
-function numeros_primos() {
-    numeros = []
-    soy_primo = True
-    for (i = 2; i <= 100; i++) {
-        for (j = i; j >= 1; j--) {
+
+    primos = [1, 2, 3]
+    function numerosPrimos() {
+    for (i = 4; i < 100; i++) {
+        soyPrimo = true
+        for (j = i - 1; j > 1; j--) {
             if (i % j == 0) {
-                
+                soyPrimo = false;
+                break
             }
         }
+        if (soyPrimo) {
+            primos.push(i)
+        }
     }
+    console.log(primos)
 }
 
