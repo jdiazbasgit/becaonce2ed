5b8f62a67bf7068a2d6943b32728409a8b01a4bb
Your Name <you@example.com>
2022-11-17 08:47:50 +0100

    a

---
+++ ejercicio numeros primos/elementos.js
@@ -0,0 +1,15 @@
+function numeroPrimos(){
+   for (i = 0; i < texto.length; i++) {
+      if (i % 2 === 0) {
+var tabla = document.querySelector("#tabla");
+var primos=[];
+var tr=Document.Create.Element("tr");
+var td=Document.Create.Element("td");
+
+//recorremos los numerops del 1 al 100 en un for
+// si es primo lo añadimos al array
+primos.push(3);
+   // recorro en array de numeros y por cada uno de los numero primos creamos un tr, un td, 
+   //le damos contenido al td con el valor del numero primo 
+   //añadimos el td al tr
+   //añadimos el tr a la tabla
