aef4e74274cde6bf664be7dde9aff0769b2497aa
Your Name <you@example.com>
2022-11-17 11:57:47 +0100

    a

---
+++ primos.js
@@ -0,0 +1,22 @@
+primos = []
+function numerosprimos() {
+    var numero = 100;
+
+    //if (numero == 0 || numero == 1 || numero == 4) return false;
+
+    for (let x = 2; x < numero; x++) {
+        if (numero % x == 0) return false;
+
+        else (numero % x 1 = 0 && x % numero == numero && numero % x == 1)
+
+        for (let j = x; x; x > numero; x--)
+        {
+            if (numero % x == 0) return false;
+
+            else (numero % x 1 = 0 && x % numero == numero && numero % x == 1)
+            return true
+
+        }
+    }
+    console.log(numerosprimos())
+}
