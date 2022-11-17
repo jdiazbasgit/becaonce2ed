74cbb8ca1c376d2a57767be6b365cfdf75802b3d
OscarP72 <116722711+OscarP72@users.noreply.github.com>
2022-11-17 13:50:05 +0100

    numeros primos

---
+++ numeroprimo.js
@@ -0,0 +1,22 @@
+primos = [1, 2, 3]
+
+function numerosPrimos() {
+   
+    for (i = 4; i < 100; i++) {
+        soyPrimo = true
+        for (j = i-1; j > 1; j--) {
+            if (i % j == 0) {
+                soyPrimo = false;
+                break
+            }
+        }
+        if (soyPrimo) {
+            primos.push(i)
+        }
+    }
+    console.log(primos)
+}
+
+
+
+
