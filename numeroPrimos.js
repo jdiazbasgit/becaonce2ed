cb9c09fd6cececfaca13e43ef706ab38966940ca
Your Name <you@example.com>
2022-11-17 14:22:56 +0100

    subidoactualizado

--- numeroPrimos.js
+++ numeroPrimos.js
@@ -1,49 +1,45 @@
-//var tabla = document.querySelector("#tabla");
- //crear un Array vacio que se llame primos
- //var primos=[];
-//function numeroPrimos(){
-    //recorremos los numerops del 1 al 100 en un for
-    // si es primo lo añadimos al array
-//primos.push(3);
-   // recorro en array de numeros y por cada uno de los numero primos creamos un tr, un td, le damos contenido al td con el valor del numero primo 
-   //añadimos el td al tr
-   //añadimos el tr a la tabla
-var tabla= document.querySelector("#tabla1");
-var cadena = 100;
-var j = 2;
-var numerosPrimos = [];
-var contador=0;
-
-for (j=2; j < cadena; j++) {
-
-  if (crearFilaPrimo(j)) {
-    numerosPrimos.push(j);
-  }
-  
-}
-//console.log(numerosPrimos);
-
 function primo(numero) {
-  var tr=document.createElement("tr");
-  var td=document.createElement("td");
-  td.innerHTML="primo" ;
-  tr.appendChild(td);
   for (var i = 2; i < numero; i++) {
-
     if (numero % i === 0) {
       return false;
-      tabla.appendChild(tr);
-      i++;
     }
-
   }
 
-  return numero !== 1;
+  return true;
 }
 
-function crearFilaPrimo(){
-  //primo();
+
+function crearFilaPrimo() {
+  var numerosPrimos = [];
+  //var cadena = 100;
+
+  for (j = 0; j < 100; j++) {
+    var tr = document.createElement("tr");
+    var td = document.createElement("td");
+    var td1 = document.createElement("td");
+
+    if (primo(j)) {
+      numerosPrimos.push(j);
+      td.innerHTML = j;
+      td1.innerHTML = "Es primo";
+
+    } else {
+      td.innerHTML = j;
+      td1.innerHTML = "No es primo";
+      //color en las celdas
+      td.setAttribute("style","background-color:#EC2013; color:white");
+      td1.setAttribute("style","background-color:#13C2EC; color:white");
+    }
+
+    tr.appendChild(td);
+    tr.appendChild(td1);
+
+    tabla1.appendChild(tr);
+
+  }
 }
+
+
 
 
 
