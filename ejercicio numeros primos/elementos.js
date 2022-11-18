4529a1f478083423256b4d024d4da0dc003c0ffb
Your Name <you@example.com>
2022-11-18 12:42:05 +0100

    aa

--- ejercicio numeros primos/elementos.js
+++ ejercicio numeros primos/elementos.js
@@ -1,26 +1,19 @@
+primos =[1, 2, 3]
 function numeroPrimos() {
-   numeros = []
-   numeros.push(1)[1];
-   numeros.push(2)[1, 2];
-   numeros.push(3)[1, 2, 3];
-   numeros.push(5)[1, 2, 3, 5];
-   numeros.push(7)[1, 2, 3, 5, 7];
-   numeros.push(10)[1, 2, 3, 5, 7, 10];
-   for (i = 0; i <= numeros.legth; i++) {
-      if (i % 2 == 0) true {
-         if (i % 1 == 1) true{
-            if (i % i == 0) true{
-               console.log("es primo");
-            }
+   for (let i = 4; i < 100; i++) {
+      soyPrimo = true
 
+      for (let j = i - 1; j > 1; j--) {
+         if (i % j == 0) {
+            soyPrimo = false;
+            break
          }
       }
-      var tabla = document.querySelector("#tabla");
-      var numeros = [];
-      var tr = Document.Create.Element("tr");
-      var td = Document.Create.Element("td");
+      if (soyPrimo) {
+         primos.push(i)
+      }
 
-      document.querySelector("#Tabla").InnerHTML = document.querySelector("#tabla").innerHTML + numeros.substring(i, i + 1)
    }
-
+   console.log(primos)
+}
 
