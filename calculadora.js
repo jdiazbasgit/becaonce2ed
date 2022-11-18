8286194535ffe57f9fb8a5b0a9251fc2b0e011a4
OscarP72 <116722711+OscarP72@users.noreply.github.com>
2022-11-18 14:22:17 +0100

    calculadora

---
+++ calculadora.js
@@ -0,0 +1,23 @@
+calculadoraA = []
+
+function calculadoraA() {
+    var n1, n2, n3, n4, n5, n6, n7, n8, n9
+    switch (resultado) {
+        case n1:
+            resultado = (n1)
+            console.log (1)
+            break;
+        case n2:
+            resultado = (id = "2")
+            console.log(2)
+            break;
+
+
+
+
+
+
+
+    }
+
+}
