d4118cc8412da20af6cf87389c1cc828314eddb7
Your Name <you@example.com>
2022-11-18 14:28:59 +0100

    repasar

---
+++ calculadora/calculadorajs.js
@@ -0,0 +1,12 @@
+function numerosdeCalculadora(pulsar)
+
+
+switch(pulsar){
+    case 1 :
+        document.querySelector("#1").innerHTML=document.querySelector("#1").innerHTML+pulsar
+ 
+
+}
+
+
+    
--- ejercicio/ejercicio-1.js
+++ ejercicio/ejercicio-1.js
@@ -9,6 +9,7 @@ function minombrEnMayusculas() {
             document.querySelector("#h1").innerHTML = document.querySelector("#h1").innerHTML + texto.substring(i,i+1).toUpperCase();
         }
         else 
+        
             document.querySelector("#h1").innerHTML = document.querySelector("#h1").innerHTML + texto.substring(i, i+1).toLowerCase();
         }
 
