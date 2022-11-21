01170d4024d7e8854686183b9ef72fe458927c2b
Your Name <you@example.com>
2022-11-21 08:36:27 +0100

    Calculadora beta

---
+++ calculadora.js
@@ -0,0 +1,60 @@
+function calculadora() {
+ 
+    var resultado = document.getElementById("resultado");
+    var reset = document.getElementById("reset");
+    var suma = document.getElementById("suma");
+    var resta = document.getElementById("resta");
+    var multiplicacion = document.getElementById("multiplicacion");
+    var division = document.getElementById("division");
+    var igual = document.getElementById("igual");
+    var uno = document.getElementById("uno");
+    var dos = document.getElementById("dos");
+    var tres = document.getElementById("tres");
+    var cuatro = document.getElementById("cuatro");
+    var cinco = document.getElementById("cinco");
+    var seis = document.getElementById("seis");
+    var siete = document.getElementById("siete");
+    var ocho = document.getElementById("ocho");
+    var nueve = document.getElementById("nueve");
+    var cero = document.getElementById("diez");
+}
+function load(resultado) {
+    var uno = document.getElementById("uno");
+    uno.addEventListener("click", modifyText, false);
+}
+function load(resultado) {
+    var dos = document.getElementById("dos");
+    dos.addEventListener("click", modifyText, false);
+}
+function load(resultado) {
+    var tres = document.getElementById("tres");
+    tres.addEventListener("click", modifyText, false);
+}
+function load(resultado) {
+    var cuatro = document.getElementById("cuatro");
+    cuatro.addEventListener("click", modifyText, false);
+}
+function load(resultado) {
+    var cinco = document.getElementById("cinco");
+    cinco.addEventListener("click", modifyText, false);
+}
+function load() {
+    var seis = document.getElementById("seis");
+    seis.addEventListener("click", modifyText, false);
+}
+function load(resultado) {
+    var siete = document.getElementById("siete");
+    siete.addEventListener("click", modifyText, false);
+}
+function load(resultado) {
+    var ocho = document.getElementById("ocho");
+    ocho.addEventListener("click", modifyText, false);
+}
+function load(resultado) {
+    var nueve = document.getElementById("nueve");
+    nueve.addEventListener("click", modifyText, false);
+}
+function load(resultado) {
+    var cero = document.getElementById("cero");
+    cero.addEventListener("click", modifyText, false);
+}
