0a612c3868641e52982ab3464c8f0ba121463d02
Your Name <you@example.com>
2022-11-21 13:21:16 +0100

    sub3

--- paises.js
+++ paises.js
@@ -1,21 +1,38 @@
-cargarPaises=()=>{
-    fetch("https://restcountries.com/v3.1/region/europe/").then(response=>response.json())
-    .then(datos=>{
+/*cargarPaises=()=>{
+    fetch("https://restcountries.com/v3.1/region/europe/")
+    .then((response)=>response.json())
+    .then((datos)=>{
         for(i=0;datos.length;i++){
             console.log(datos[i].name.common)
         
         }
     })
-};
+};*/
 
 cargarPaises=(continente)=>{
     fetch("https://restcountries.com/v3.1/region/"+continente)
-    .then((response)=>response.json())
-    .then((paises)=>{
-        paises.forEach(pais => {
-            console.log("La capital de"+pais.name.common+" es "+pais.capital+" y tiene "+pais.population+" habitantes")
-            //console.log(`La capital de ${pais.name.common} es ${pais.capital} y tiene ${pais.population} habitantes`)
-            //template string
-        });
+    .then((response) => response.json())
+    .then((paises) => {
+
+      paises.forEach(pais => {
+          //console.log("la capital de "+pais.name.common+" es "+pais.capital+" y tiene "+pais.population+" habitantes")
+          console.log (`la capital de ${pais.name.common} es ${pais.capital} y tiene ${pais.population} habitantes`)
+          // template string
+      });
+        
     })
+    .catch(error=>{
+    });
 }
+function cargarPaises1() {
+    fetch("https://restcountries.com/v3.1/region/europe")
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (datos) {
+        for (i = 0; i < datos.length; i++) {
+          console.log(datos[i].name.common);
+        }
+      });
+  }
+  
