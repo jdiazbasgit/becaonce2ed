35fa3a9d61d85bfbd9cff517a544534f45400341
Your Name <you@example.com>
2022-11-21 14:26:44 +0100

    mirar

--- imagenes/paises.js
+++ imagenes/paises.js
@@ -1,6 +1,16 @@
 cargarPaises= (continente)=>{
     fetch("https://restcountries.com/v3.1/region/" + continente)
-    .then((response) => response.json())
-    .then
+    .then((response) => response.json(continente))
+    .then((paises)=>{
+        
+
+        paises.forEach(pais=> {
+            paises.querySelector("#continente").value= paises.querySelector("#continente").value + value
+           
+            
+
+            
+        });
+    }) 
 
 }
