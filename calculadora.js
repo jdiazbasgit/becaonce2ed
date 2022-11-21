057390c3b754de86f180112b62529b931ab38a0a
Your Name <you@example.com>
2022-11-21 09:39:41 +0100

    Calculadora

--- calculadora.js
+++ calculadora.js
@@ -1,60 +1,28 @@
-function calculadora() {
- 
     var resultado = document.getElementById("resultado");
-    var reset = document.getElementById("reset");
-    var suma = document.getElementById("suma");
-    var resta = document.getElementById("resta");
-    var multiplicacion = document.getElementById("multiplicacion");
-    var division = document.getElementById("division");
-    var igual = document.getElementById("igual");
+  
     var uno = document.getElementById("uno");
+    darEvento(uno)
     var dos = document.getElementById("dos");
+    darEvento(dos)
     var tres = document.getElementById("tres");
+    darEvento(tres)
     var cuatro = document.getElementById("cuatro");
+    darEvento(cuatro)
     var cinco = document.getElementById("cinco");
+    darEvento(cinco)
     var seis = document.getElementById("seis");
+    darEvento(seis)
     var siete = document.getElementById("siete");
+    darEvento(siete)
     var ocho = document.getElementById("ocho");
+    darEvento(ocho)
     var nueve = document.getElementById("nueve");
-    var cero = document.getElementById("diez");
-}
-function load(resultado) {
-    var uno = document.getElementById("uno");
-    uno.addEventListener("click", modifyText, false);
-}
-function load(resultado) {
-    var dos = document.getElementById("dos");
-    dos.addEventListener("click", modifyText, false);
-}
-function load(resultado) {
-    var tres = document.getElementById("tres");
-    tres.addEventListener("click", modifyText, false);
-}
-function load(resultado) {
-    var cuatro = document.getElementById("cuatro");
-    cuatro.addEventListener("click", modifyText, false);
-}
-function load(resultado) {
-    var cinco = document.getElementById("cinco");
-    cinco.addEventListener("click", modifyText, false);
-}
-function load() {
-    var seis = document.getElementById("seis");
-    seis.addEventListener("click", modifyText, false);
-}
-function load(resultado) {
-    var siete = document.getElementById("siete");
-    siete.addEventListener("click", modifyText, false);
-}
-function load(resultado) {
-    var ocho = document.getElementById("ocho");
-    ocho.addEventListener("click", modifyText, false);
-}
-function load(resultado) {
-    var nueve = document.getElementById("nueve");
-    nueve.addEventListener("click", modifyText, false);
-}
-function load(resultado) {
+    darEvento(nueve)
     var cero = document.getElementById("cero");
-    cero.addEventListener("click", modifyText, false);
+    darEvento(cero)
+
+function darEvento(elemento) {
+    elemento.addEventListener("click", function (event) {
+        resultado.value = resultado.value + event.target.innerHTML
+        });
 }
