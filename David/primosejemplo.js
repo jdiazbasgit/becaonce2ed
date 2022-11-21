280a3c6add185257f5f56265841ace621e633845
Your Name <you@example.com>
2022-11-21 09:33:21 +0100

    subir

---
+++ David/primosejemplo.js
@@ -0,0 +1,44 @@
+function operar(apretar) {
+    var input = document.querySelector("#boton");
+    switch (apretar) {
+        case 1:
+            console.log("1");
+            input.value = input.value + "1"
+            break;
+        case 2:
+            console.log("2");
+            input.value = input.value + "2"
+            break;
+        case 3:
+            console.log("3");
+            input.value = input.value + "3"
+            break;
+        case 4:
+            console.log("4");
+            input.value = input.value + "4"
+            break;
+        case 5:
+            console.log("5");
+            input.value = input.value + "5"
+            break;
+        case 6:
+            console.log("6");
+            input.value = input.value + "6"
+            break;
+        case 7:
+            console.log("7");
+            input.value = input.value + "7"
+            break;
+        case 8:
+            console.log("8");
+            input.value = input.value + "8"
+            break;
+        case 9:
+            console.log("9");
+            input.value = input.value + "9"
+            break;
+
+
+    }
+
+}
