07719123865879290e2ed701658016d57cda6a04
OscarP72 <116722711+OscarP72@users.noreply.github.com>
2022-11-22 13:31:53 +0100

    a

--- paisesoscar.js
+++ paisesoscar.js
@@ -2,12 +2,12 @@ cargarPaises = continente => {
     fetch("https://restcountries.com/v3.1/region/" + continente)
         .then(response => response.json())
         .then(paises => {
-            
+
             var paisesSelect = document.querySelector("#paises");
-            paisesSelect.innerHTML=""
+            paisesSelect.innerHTML = ""
             let seleccionaPais = document.createElement("option")
             seleccionaPais.value = "0"
-            seleccionaPais.text = "Selecciona pais)";
+            seleccionaPais.text = "Selecciona pais";
             paisesSelect.appendChild(seleccionaPais);
             paises.forEach(pais => {
 
@@ -30,3 +30,27 @@ vaciarElemento = (elemento) => {
         elemento.removeChild(hijo);
     })
 }
+
+cargaPais = pais => {
+    fetch("https://restcountries.com/v2/name/" + nombre)
+        .then(response => response.json())
+        .then(datosgenerales => {
+            var datosGeneralesSelect = document.querySelector("#datosgenerales");
+            vaciarElemento = (elemento);
+            let datosGenerales = document.createElement("option")
+            datosGenerales.text = "datosgenerales"
+            datosGenerales.forEach(pais => {
+                let datosgenerales = ["Pais","Nombre","Capital","Población","Lengua"]
+
+              let option = document.createElement("option")
+                option.value = pais.name.capital.population.languages
+                option.text=option.value;
+                datosGenerales.appendChild(option)
+})
+
+
+        })
+}
+
+
+
