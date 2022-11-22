262832d104119152291fbf7c6a22d82edd737d68
David Lira <114002545+DavidLira23@users.noreply.github.com>
2022-11-22 13:53:11 +0100

    Paises

--- js/mundo.js
+++ js/mundo.js
@@ -12,71 +12,104 @@
 
 
 cargarPaises = (continente) => {
-    var IdPaises = document.getElementById("pais");
+  var IdPaises = document.getElementById("pais");
 
-    fetch("https://restcountries.com/v3.1/region/" + continente)
-      .then((response) => response.json())
-      .then((paises) => {
-  
-        paises.forEach(pais => {
-          //console.log("la capital de "+pais.name.common+" es "+pais.capital+" y tiene "+pais.population+" habitantes")
-          //console.log (`la capital de ${pais.name.common} es ${pais.capital} y tiene ${pais.population} habitantes`)
-          // template string
+  fetch("https://restcountries.com/v3.1/region/" + continente)
+    .then((response) => response.json())
+    .then((paises) => {
 
-          var option = document.createElement("option");
-          option.text=pais.name.common;
-          IdPaises.add(option);
-        });
+      paises.forEach(pais => {
+        //console.log("la capital de "+pais.name.common+" es "+pais.capital+" y tiene "+pais.population+" habitantes")
+        //console.log (`la capital de ${pais.name.common} es ${pais.capital} y tiene ${pais.population} habitantes`)
+        // template string
 
-        
-        /*for (i = 0; i < datos.length; i++) {
-          console.log(datos[i].name.official);
-        }*/
-  
-  
-  
-      }).catch(error => {
-  
-  
+        var option = document.createElement("option");
+        option.value = pais.name.common;
+        option.text = pais.name.common;
+        IdPaises.add(option);
       });
-    //cargarPaises1();
-  };
-  
-  /*function cargarPaises1() {
-    fetch("https://restcountries.com/v3.1/region/europe")
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (datos) {
-        for (i = 0; i < datos.length; i++) {
-          console.log(datos[i].name.common);
-        }
-      });
-  }
-  
-  const btn = document.querySelector('#continente');
-  
-  btn.addEventListener('click', (event) => {
-    cargarPaises('europe');
-  });*/
+
+
+      /*for (i = 0; i < datos.length; i++) {
+        console.log(datos[i].name.official);
+      }*/
+
+
+
+    }).catch(error => {
+      alert(error);
+    });
+  //cargarPaises1();
+};
+
+/*function cargarPaises1() {
+  fetch("https://restcountries.com/v3.1/region/europe")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (datos) {
+      for (i = 0; i < datos.length; i++) {
+        console.log(datos[i].name.common);
+      }
+    });
+}
+ 
+const btn = document.querySelector('#continente');
+ 
+btn.addEventListener('click', (event) => {
+  cargarPaises('europe');
+});*/
 
 
 var Continente = document.querySelector('#continent');
 
 Continente.addEventListener('change', (event) => {
-    var cboPais= document.getElementById("pais");
-    cboPais.innerHTML = "";
+  var cboPais = document.getElementById("pais");
+  cboPais.innerHTML = "";
 
-    if(!event.target.value==''){
-        var option = document.createElement("option");
-        option.text="Seleccione un país";
-        cboPais.add(option);
-
-        document.querySelector('#continent').remove;
-        cargarPaises(event.target.value);
-    } else{
-        alert("Por favor, seleccione un continente");
-    }
+  if (!event.target.value == '') {
+    var option = document.createElement("option");
+    option.text = "Seleccione un país";
+    cboPais.add(option);
+    cargarPaises(event.target.value);
+  } else {
+    alert("Por favor, seleccione un continente");
+  }
 });
 
-  
+
+
+var Pais = document.querySelector('#pais');
+
+Pais.addEventListener('change', (event) => {
+  fetch("https://restcountries.com/v3.1/name/" + Pais.value)
+    .then((response) => response.json())
+    .then((countriesList) => {
+
+      countriesList.forEach(countryData => {
+        document.querySelector('#region').innerHTML=countryData.region;
+        document.querySelector('#capital').innerHTML=countryData.capital;
+        document.querySelector('#area').innerHTML=countryData.area;
+
+
+
+
+
+        document.querySelector('#nombre').innerHTML=countryData.currencies[0];
+        document.querySelector('#simbolo').innerHTML=countryData.currencies[0];
+
+
+
+
+        document.querySelector("#flag-container img").src = countryData.flags.png;
+        document.querySelector("#shield-container img").src = countryData.coatOfArms.png;
+        document.querySelector('#arabe').innerHTML=countryData.translations.ara.official;
+        document.querySelector('#francia').innerHTML=countryData.translations.fra.official;
+        document.querySelector('#italia').innerHTML=countryData.translations.ita.official;
+      });
+
+    }).catch(error => {
+      alert(error);
+    });
+});
+
