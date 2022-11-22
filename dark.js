3289a854d8ac87d7752f2544f617889d3034ec1d
DevSniffer <116722849+DevSniffer@users.noreply.github.com>
2022-11-22 11:36:15 +0100

    a

--- dark.js
+++ dark.js
@@ -1,24 +1,84 @@
 elegirContinente=(continente)=>{
+    var selectorpaises = document.getElementById("seleccionaPais")
+    selectorpaises.innerHTML = ""
+    
+    var placeholder = document.createElement("option")
+    placeholder.textContent = "Selecciona País";
+    placeholder.value = null;
+    selectorpaises.appendChild(placeholder);
+
     fetch("https://restcountries.com/v3.1/region/"+continente)
     .then(response=>response.json())
     .then(pais=>{
         pais.forEach(pais => {
             console.log("La capital de "+pais.name.common+" es "+pais.capital)
-            document.querySelector("#seleccionaPais")
-        document.createElement("option").innerHTML = pais.name.common
-            /*<option class="opcionescontinentes" value="null">SeleccionaPaís</option>
-            
-        });
-        /*for(i=0; i<datos.length; i++){
-            console.log(datos[i].name.official)*/ //con forEach es más facil
-        
+           // var selectorpaises = document.getElementById("seleccionaPais")
+            var nombredepais = pais.name.common;
+            var nuevaoption = document.createElement("option")
+            nuevaoption.textContent = nombredepais;
+            nuevaoption.value = nombredepais;
+            selectorpaises.appendChild(nuevaoption);      
+        }
+    )/*.catch(error=>{
+        //este catch como forma de tipificar un fallo
+    })*/
+})}
+
+
+elegirPais=(pais)=>{
+    /*var selectorpaises = document.getElementById("seleccionaPais")
+    selectorpaises.innerHTML = ""
+    
+    var placeholder = document.createElement("option")
+    placeholder.textContent = "Selecciona País";
+    placeholder.value = null;
+    selectorpaises.appendChild(placeholder);*/
+
+    fetch("https://restcountries.com/v3.1/name/"+pais)
+    .then(response=>response.json())
+    .then(datos=>{
+        //document.getElementById("nombreDePais").innerHTML = pais
+        document.querySelectorAll(".nombreDePaisClass").innerHTML = pais
+        pais.forEach(pais => {
+            console.log("La capital de "+pais.name.common+" es "+pais.capital)
+            var selectorpaises = document.getElementById("seleccionaPais")
+            var nombredepais = pais.name.common;
+            var nuevaoption = document.createElement("option")
+            nuevaoption.textContent = nombredepais;
+            nuevaoption.value = nombredepais;
+            selectorpaises.appendChild(nuevaoption);      
         }
     ).catch(error=>{
         //este catch como forma de tipificar un fallo
     })
 })}
 
-/*
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*var selectorpaises = document.getElementById("seleccionaPais")
+selectorpaises.childNodes.forEach(hijo =>{
+selectorpaises.remove(hijo)
+})*/
+
+    /*<option class="opcionescontinentes" value="null">SeleccionaPaís</option>
+            
+        });
+        /*for(i=0; i<datos.length; i++){
+            console.log(datos[i].name.official)*/ //con forEach es más facil
+        /*
 
 document.getElementById("seleccionaContinente").addEventListener("change", myFunction);
 
