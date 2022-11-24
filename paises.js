feac32b099c45ee167f4fac7d089970de186602d
Your Name <you@example.com>
2022-11-24 14:26:38 +0100

    falta poco

--- paises.js
+++ paises.js
@@ -36,24 +36,25 @@ var tabla = document.createElement("table")
 let td1 = document.createElement("td")
 let tr1 = document.createElement("tr")
 let tr11 = document.createElement("tr")
-main. appendChild(div1)
-div1.appendchild(td1)
-div1.appendchild(tr1)
-div1.appendchild(tr11)
+div1.appendChild(tabla)
+main.appendChild(div1)
+div1.appendChild(td1)
+div1.appendChild(tr1)
+div1.appendChild(tr11)
 tr1.innerHTML = "capital" + pais.capital
 let td2 = document.createElement("td")
 let tr2 = document.createElement("tr")
 let tr22 = document.createElement("tr")
-div1.appendchild(td2)
-div1.appendchild(tr2)
-div1.appendchild(tr22)
+div1.appendChild(td2)
+div1.appendChild(tr2)
+div1.appendChild(tr22)
 tr2.innerHTML = "poblacion" + pais.population
 let td3 = document.createElement("td")
 let tr3 = document.createElement("tr")
 let tr33 = document.createElement("tr")
-div1.appendchild(td3)
-div1.appendchild(tr3)
-div1.appendchild(tr33)
+div1.appendChild(td3)
+div1.appendChild(tr3)
+div1.appendChild(tr33)
 tr3.innerHTML = "area" + pais.area
 let div2 = document.createElement("div")
 div2.classList.add(col-sm-6)
@@ -61,29 +62,24 @@ main.appendChild(div2)
 let td4 = document.createElement("td")
 let tr4 = document.createElement("tr")
 let tr44 = document.createElement("tr")
-div2.appendchild(td4)
-div2.appendchild(tr4)
-div2.appendchild(tr44)
+div2.appendChild(td4)
+div2.appendChild(tr4)
+div2.appendChild(tr44)
 div1.appendChild(tabla)
 tr4.innerHTML= "dominio" + pais.domain
 
 }
 dameDatosGenerales = () => { 
   let paisesSelect = document.querySelector("#paises")
-  dameDatos("https://restcountries.com/v3.1/alpha/" + "/name/" + selectpaises.value)
+  dameDatos("https://restcountries.com/v3.1/alpha/" + "/name/" + paisesSelect.value)
   .then(response => response.json())
   .then(pais=>{
     ponerDatosGenerales(pais[0])
   })
 }
-
-
 //https://restcountries.com/v3.1/alpha
 
-
-
-
-dameDatosGenerales = () => {}
+//dameDatosGenerales = () => {}
 dameDatosGeograficos = () => {}
 dameBanderas = () => {}
 dameCodigodeMoneda = () => {}
