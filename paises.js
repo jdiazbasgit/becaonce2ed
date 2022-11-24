b9fb3670868aa88bcb8b7102be463516e07f9d39
Your Name <you@example.com>
2022-11-24 11:06:41 +0100

    a

--- paises.js
+++ paises.js
@@ -1,6 +1,6 @@
 
 
-opcionescontinente = continente => {
+opcionesContinente = continente => {
 
     var paisesSelect = document.querySelector("#paises");
     paisesSelect.innerHTML = ""
@@ -10,7 +10,7 @@ opcionescontinente = continente => {
         .then(pais => {
             let seleccionaPais = document.createElement("option")
             seleccionaPais.value = "0"
-            seleccionaPais.text = "selecciona pais"
+            seleccionaPais.text = "seleccionarPais"
             paisesSelect.appendChild(seleccionaPais);
             pais.forEach(pais => {
 
@@ -29,23 +29,7 @@ opcionescontinente = continente => {
 
 //buscar datosGenerales..
 
-vaciarElementos = (elementos) => {
-    paisesSelect.childNodes.forEach(hijo => {
-        paisesSelect.removechild(hijo);
-    })
-}
-damedatos = (URL) => fetch(URL){
-}
-datosGenerales => {
-    var main = document.querySelectorAll("main");
-    main.innerHTML
-    main.classlist.add("row");
-    main.classlist.add("col megacol");
-    let div1 = document.createElement("div");
-    div1.classList.add("");
-    let h3 = document.createElement("h3");
-    h31.classlist.add("text-success")
-}
+
 
 
 
