5a1a194ad67a594109d37f262805d18c7ce46a87
jdiazbasgit <git@studiotapal.es>
2022-11-24 13:53:07 +0100

    a

---
+++ html/promesa.js
@@ -0,0 +1,27 @@
+sinPromesa = () => {
+  dejarMovilEnTienda = true;
+  movilroto = true;
+  setTimeout(() => {
+    movilroto = false;
+    console.log("el movil esta arrglado, puedes venir a recogerlo");
+  }, 5000);
+  if (movilroto) console.log("no puedo llamar, el movil esta roto");
+  else console.log("hola cariño como estas");
+};
+
+conPromesa = () => {
+    dejoElMovil().then((result) => {
+    console.log(result);
+    if (movilroto) console.log("no puedo llamar, el movil esta roto");
+    else console.log("hola cariño como estas");
+  });
+};
+
+dejoElMovil = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      movilroto = false;
+      resolve("el movil esta arreglado, puedes venir a recogerlo");
+    }, 5000);
+  });
+};
