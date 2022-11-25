c7b539048f697b4342eed8b4ed3258767978e662
Your Name <you@example.com>
2022-11-25 14:26:48 +0100

    ejercicio json

--- Paises.js
+++ Paises.js
@@ -1,3 +1,58 @@
+cargaInicialPais = (pais) => {
+    fetch ("https://restcountries.com/v3.1/" .name.alpha.leng + continente)
+        .then((response) => response.json())
+        .then(paises => {
+
+        })           
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+CargaPais = (pais) => {
+var urlrescountries = "https://restcountries.com/v3.1/";
+const main= document.querySelector ("main");
+var capital:// (string);
+var poblacion: // (number)
+var area://  (number)
+va rmoneda:// Moneda [])
+var frontera:// string  
+var bandera:// string
+var escudo:// string
+var traducciones:// traduccion[]
+
 cargarPaises = (continente) => {
     fetch("https://restcountries.com/v3.1/region/" + continente)
         .then((response) => response.json())
@@ -26,45 +81,49 @@ vaciarElemento = (elemento) => {
         elemento.renove.renoveChild(hijo);
     })
 }
-/*bandera https://goo.gl/maps/uDWEUaXNcZTng1fP6
+ 
+dameDatos =(URL) => fetch (URL)
+dameDatos = (URL) cc3 =>{ 
+dameDatos = (URL) (urlrescountries + cc3) (then response => response json)
+.then (pais => pais.border)
+}
+cargarDatosGenrasles= () => {
+ 
+}
+ 
+cargarDatosGenrasles= () => {
+ 
+}
+pintarBanderaEscudo = () => {
+    cargarBanderaEscudo(urlrescountries + /alpha/ + pais.flag.coatOfArms)
+        .then((response) => response.json())
+        .then(paises => {
+        }
+
+    )}  
+    console.log (banderas)
+    var main = document.querySelector("main");
+    main.innerHTML =""
+    main.classList.add.apply("text-center")
+    let h21 = document.createElement("h2");
+    let th1 = document.createElement("th");
+    main.appendChild(main)
+     
+
+vaciarElemento = (elemento) => {
+elemento.childNodes.forEach(hijo => {
+elemento.renove.renoveChild(hijo);
+})
+}
+
+
+        /*bandera https://goo.gl/maps/uDWEUaXNcZTng1fP6
 escudo https://mainfacts.com/media/images/coats_of_arms/pe.svg
 */
-https://restcountries.com/v3.1/name/peru
+/*https://restcountries.com/v3.1/name/peru
 
-cargarBanderas = () => {
-    let selectpaises = document.querySelector(#pais)
-    dameDatos(urlrescountries + /name/ + selectpaises.value)
-        .then((response) => response.json())
-        .then(paises => {
-        } 
-}
-pintarBanderas = () => {
-    dameDatos(urlrescountries + /alpha/ + flags)
-        .then((response) => response.json())
-        .then(paises => {
-        }
-        }
-
-pintarEscudos = () => {
-    dameDatos(urlrescountries + /alpha/ + pais.flag.coatOfArmsº)
-        .then((response) => response.json())
-        .then(paises => {
-        }
-/*
-            console.log (banderas)
-            var main = document.querySelector("main");
-            main.innerHTML =""
-            main.classList.add.apply("text-center")
-            let h21 = document.createElement("h2");
-            let th1 = document.createElement("th");
-            main.appendChild(main)
-            table.appendChild(table)
-            tr1.appendChild(tr)
-            th1.appendChild(th1)
-
- vaciarElemento = (elemento) => {
-    elemento.childNodes.forEach(hijo => {
-        elemento.renove.renoveChild(hijo);
+ 
+           
    
     })
  } 
---
+++ ejercicioEmpleadoJson.js
@@ -0,0 +1,60 @@
+// creamos class para json
+
+class Empleado {
+    constructor(nombre, dni, fechaNacimiento, fechaAlta, fechaBaja) {
+        this.nombre = nombre;
+        this.dni = dni;
+        this.fechaNacimiento = fechaNacimiento;
+        this.fechaAlta = fechaAlta;
+        this.fechaBaja = fechaBaja;
+    }
+}
+
+class Empresa {
+    constructor(cif, nombreEmpresa) {
+        this.cif = cif;
+        this.nombreEmpresa = nombreEmpresa;
+    }
+}
+
+class DatoLaborales {
+    constructor(salario) {
+        this.salario = salario;
+
+    }
+}
+
+class cargo {
+    constructor(cargos) {
+        this.cargo = cargo;
+
+    }
+}
+
+class DatosPersonales {
+    constructor(hijo) {
+        this.hijo = hijo;
+    }
+}
+
+class Hijo {
+    constructor(hijos, hijas) {
+        this.hijos = hijos;
+        this.hijas = hijas;
+    }
+}
+
+class EstadoCivil {
+    constructor(descripcion) {
+        this.descripcion = descripcion;
+
+    }
+}
+
+// creo los New datos de las clases como segundo paso
+//nombre, dni, fechaNacimiento, fechaAlta, fechaBaja
+
+nombre = new nombre()
+
+
+
