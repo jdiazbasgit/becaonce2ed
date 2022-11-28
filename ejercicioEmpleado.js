73195a71c5ab9668000bb8a4d91fc028d05e15ce
Your Name <you@example.com>
2022-11-28 12:12:09 +0100

    ejercicio1

--- ejercicioEmpleado.js
+++ ejercicioEmpleado.js
@@ -1,40 +1,54 @@
-class Empleado{
-    constructor()
+class Empleado {
+    constructor(nombre, dni, fechaAlta, fechaBaja) {
+        this.nombre = nombre;
+        this.dni = dni;
+        this.fechaAlta = fechaAlta;
+        this.fechaBaja = fechaBaja;
+    }
+
 }
 
-class datosPersonales{
-    constructor(hijo, estadoCivil){
-        this.hijo=hijo;
-        this.esstadoCivil=estadoCivil;
+class Hijo {
+    constructor(hijo, estadoCivil) {
+        this.hijo = hijo;
+        this.esstadoCivil = estadoCivil;
     }
 }
 
-class EstadoCivil{
-    constructor(descripcion){
-        this.descripcion=descripcion;
+class EstadoCivil {
+    constructor(descripcion) {
+        this.descripcion = descripcion;
     }
 }
-class Cargo{
-    constructor(descripcion){
-        this.descripcion=descripcion;
+class DatosPersonales {
+    constructor(hijo, estadoCivil) {
+        this.hijo = hijo;
+        this.estadoCivil = estadoCivil;
+    }
+}
+class Cargo {
+    constructor(descripcion) {
+        this.descripcion = descripcion;
 
     }
 }
-class Empresa{
-    constructor(cif,descripcion){
-        this.cif=cif;
-        this.descripcion=descripcion;
+class Empresa {
+    constructor(cif, descripcion) {
+        this.cif = cif;
+        this.descripcion = descripcion;
     }
 }
-class DatosPersonales{
-    constructor(estadoCivil, hijo){
-        this.estadoCivil=estadoCivil;
-        this.hijo=hijo;
+class DatosLaboral {
+    constructor(salario, cargo) {
+        this.cargo = cargo;
+        this.salario = salario;
     }
 }
-class DatosLaboral{
-    constructor(salario,cargo){
-        this.cargo=cargo;
-        this.salario=salario;
-    }
-}
+var Empleado = new Empleado("Pepe Calvo", "5544777B", "19/01/2020", null);
+DatosPersonales = new DatosPersonales("3", "Soltero");
+Cargo = new Cargo("Becario")
+Empresa = new Empresa("11111", "BBVA IT");
+DatosLaboral = new DatosLaboral("1.582", Cargo());
+
+
+//alert(Empleado.)
