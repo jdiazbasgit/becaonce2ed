6b33e18f35f02c11e0bf47e7a9106c0ef8cfde2f
Your Name <you@example.com>
2022-11-28 14:24:20 +0100

    a

--- ejercicioEmpleadoJson.js
+++ ejercicioEmpleadoJson.js
@@ -1,15 +1,26 @@
 // creamos class para json
 
 class Empleado {
-    constructor(nombre, dni, fechaNacimiento, fechaAlta, fechaBaja) {
+    constructor(nombre, apellidos, dni, fechaNacimiento, sexo, fechaAlta, fechaBaja) {
         this.nombre = nombre;
+        this.apellidos = apellidos;
         this.dni = dni;
         this.fechaNacimiento = fechaNacimiento;
+        this.sexo = sexo;
         this.fechaAlta = fechaAlta;
         this.fechaBaja = fechaBaja;
     }
 }
-
+class Domicilio {
+    constructor(calle, numero, piso, portal, distrito, codigoPostal){ 
+      this.calle = calle
+      this.numero = numero
+      this.piso = piso
+      this.portal = portal
+     his.distrito = distrito
+     his.codigoPostal = codigoPostal
+}
+}
 class Empresa {
     constructor(cif, nombreEmpresa) {
         this.cif = cif;
@@ -26,7 +37,7 @@ class DatoLaborales {
 
 class cargo {
     constructor(cargos) {
-        this.cargo = cargo;
+        this.cargos = cargos;
 
     }
 }
@@ -39,22 +50,40 @@ class DatosPersonales {
 
 class Hijo {
     constructor(hijos, hijas) {
-        this.hijos = hijos;
-        this.hijas = hijas;
+        this.chicos = chicos;
+        this.chicasa = chicas;
     }
 }
 
 class EstadoCivil {
     constructor(descripcion) {
-        this.descripcion = descripcion;
+        this.estadoCivil = this.estadoCivil;
 
     }
 }
-
 // creo los New datos de las clases como segundo paso
-//nombre, dni, fechaNacimiento, fechaAlta, fechaBaja
+//nombre, dni, fechaNacimiento, fechaAlta, fechaBaja.....
+// Empleado "name":"eduardo"
+empleado = new empleado("nombre", "apellidos","dni", "fechaNacimiento", "sexo", "fecnaAlta", "fechadeBaja")
 
-nombre = new nombre()
+//domicilio calle, numero, piso, portal, distrito, codigoPostal
+domicilio = domicilio("nombre: calle, numero, piso, portal, distrito, codigoPostal")
+
+//Empresa
+cif = new cif("B45147632")
+descrpicion = new descripcion("Arelance")
+
+//Datos Laborales
+salario = new salario()
+cargos = new cargos("Alumno")
+
+//Datos Personales
+chicos = new chicos("0")
+chicas = new chicas("2")
+
+// Estado Civil
+estadoCivil = new  estadoCivil("Soltero", "Casado", "Viudo", "Divorciado")
+
 
 
 
