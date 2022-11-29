df7a79b41a04fc783ad7cac5a077ad3155e7cf40
OscarP72 <116722711+OscarP72@users.noreply.github.com>
2022-11-29 14:26:03 +0100

    a

--- empleadojson.js
+++ empleadojson.js
@@ -45,24 +45,29 @@ class DatosPersonales {
 }
 
 class Empleado {
-    constructor(nombre,apellidos, dni, fechadenacimiento, fechadealta, fechadebaja, DatosPersonales, DatosLaborales, Empresa) {
+    constructor(nombre, apellidos, dni, fechadenacimiento, fechadealta, fechadebaja, datosPersonales, datosLaborales, empresa) {
         this.nombre = nombre;
         this.apellidos = apellidos;
         this.dni = dni;
         this.fechadenacimiento = fechadenacimiento;
         this.fechadealta = fechadealta;
         this.fechadebaja = fechadebaja;
-        this.DatosPersonales = DatosPersonales;
-        this.DatosLaborales = DatosLaborales;
-        this.Empresa = Empresa;
+        this.datosPersonales = datosPersonales;
+        this.datosLaborales = datosLaborales;
+        this.empresa = empresa;
     }
 }
 
-estadoCivil = new EstadoCivil("descripcion");
-hijo = new Hijo("chicos", "chicas");
-empresa = new Empresa("cif", "nombre");
-datosLaborales = new DatosLaborales(salario,"cargo");
-cargo = new Cargo("descripcion");
-datosPersonales = new DatosPersonales("estadocivil", "Hijo");
-empleado = new Empleado("nombre","apellidos","dni","DatosPersonales","DatosLaborales","Empresa")new date(dd-mm-yy,"fechadenacimiento","fechadealta","fechadebaja");
+//estadoCivil = new EstadoCivil("descripcion");
+//hijo = new Hijo("chicos", "chicas");
+//empresa = new Empresa("cif", "nombre");
+//datosLaborales = new DatosLaborales(salario,"cargo");
+//cargo = new Cargo("descripcion");
+//datosPersonales = new DatosPersonales("estadocivil", "Hijo");
 
+juan = new Empleado("Juan", "Rodriguez", "52128521P", new Date("1970/02/15"), new Date("2008/02/08"), null, new DatosPersonales(new Hijo(3, 2), new EstadoCivil("casado")), new DatosLaborales(1500, new Cargo("jefe de sección")), new Empresa("B12345678","Telefonica"));
+console.log(`El señor ${juan.nombre} ${juan.apellidos} con documento ${juan.dni} nacido el ${juan.fechadenacimiento} que empezo a trabajar con nosotros el ${juan.fechadealta} y que sigue con nosotros en la ${juan.fechadebaja} con hijos${juan.DatosPersonales} que trabaja de ${juan.datosLaborales} y su nombre es ${juan.empresa.nombre}`)
+console.log (juan)
+
+oscar= new Empleado ("Oscar","Perea Alvarez","52125125P", new DatosPersonales(new Hijo(3,2),new EstadoCivil("divorciado")),new DatosLaborales(3000, new Cargo("Analista Programador")),new Empresa("B123456789","Fundación Once"))
+console.log (oscar)
