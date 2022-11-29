5455c371badc8ee279c1283d0776c8b199499ff0
Your Name[201~ <you@example.com>
2022-11-29 17:48:10 +0100

    a

--- empleadojson.js
+++ empleadojson.js
@@ -65,9 +65,10 @@ class Empleado {
 //cargo = new Cargo("descripcion");
 //datosPersonales = new DatosPersonales("estadocivil", "Hijo");
 
-juan = new Empleado("Juan", "Rodriguez", "52128521P", new Date("1970/02/15"), new Date("2008/02/08"), null, new DatosPersonales(new Hijo(3, 2), new EstadoCivil("casado")), new DatosLaborales(1500, new Cargo("jefe de sección")), new Empresa("B12345678","Telefonica"));
-console.log(`El señor ${juan.nombre} ${juan.apellidos} con documento ${juan.dni} nacido el ${juan.fechadenacimiento} que empezo a trabajar con nosotros el ${juan.fechadealta} y que sigue con nosotros en la ${juan.fechadebaja} con hijos${juan.DatosPersonales} que trabaja de ${juan.datosLaborales} y su nombre es ${juan.empresa.nombre}`)
-console.log (juan)
+//juan = new Empleado("Juan", "Rodriguez", "52128521P", new Date("1970/02/15"), new Date("2008/02/08"), null, new DatosPersonales(new Hijo(3, 2), new EstadoCivil("casado")), new DatosLaborales(1500, new Cargo("jefe de sección")), new Empresa("B12345678","Telefonica"));
+//console.log(`El señor ${juan.nombre} ${juan.apellidos} con documento ${juan.dni} nacido el ${juan.fechadenacimiento} que empezo a trabajar con nosotros el ${juan.fechadealta} y que sigue con nosotros en la ${juan.fechadebaja} con hijos${juan.DatosPersonales} que trabaja de ${juan.datosLaborales} y su nombre es ${juan.empresa.nombre}`)
+//console.log (juan)
 
 oscar= new Empleado ("Oscar","Perea Alvarez","52125125P", new DatosPersonales(new Hijo(3,2),new EstadoCivil("divorciado")),new DatosLaborales(3000, new Cargo("Analista Programador")),new Empresa("B123456789","Fundación Once"))
+console.log(`El señor ${oscar.nombre} ${oscar.apellidos} con documento ${oscar.dni} tiene ${oscar.DatosPersonales.Hijo.chicos} hijos ${oscar.DatosPersonales.Hijo.chicas} hijas y estado civil de ${oscar.DatosPersonales.EstadoCivil} ocupa el cargo de ${oscar.DatosLaborales.Cargo} y tiene un sueldo de ${oscar.DatosLaborales.salario} trabaja en la empresa ${oscar.Empresa.nombre} con un CIF ${oscar.Empresa.cif}`)
 console.log (oscar)
