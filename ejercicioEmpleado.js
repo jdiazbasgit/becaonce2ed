2707e87a0fc7331ac17e49d43619399901a40ac6
Your Name <you@example.com>
2022-11-29 14:19:56 +0100

    ejercicioresuelto

--- ejercicioEmpleado.js
+++ ejercicioEmpleado.js
@@ -20,14 +20,15 @@ class DatosPersonales {
 }
 
 class Hijo {
-    constructor(hijos) {
-        this.hijos = hijos;
+    constructor(chico, chica) {
+        this.chico = chico;
+        this.chica=chica;
     }
 }
 
 class EstadoCivil {
     constructor(descripcion){
-        this.descripcion = descripcion;
+        this.descripcion=descripcion;
     }
 }
 class DatosLaboral {
@@ -50,11 +51,16 @@ class Empresa {
     }
 }
 
-var Empleado = new Empleado("Pepe Calvo", "5544777B", "19/01/2020", null);
-DatosPersonales = new DatosPersonales("3", "Soltero");
-Cargo = new Cargo("Becario")
-Empresa = new Empresa("11111", "BBVA IT");
-DatosLaboral = new DatosLaboral("1.582", Cargo());
+var edu = new Empleado("Edu","PC", "5544777B","19/07/1992","02/02/2020", null,new DatosPersonales(new Hijo(1,0), new EstadoCivil("soltero")),new DatosLaboral("3000","Analista Programador"), new Empresa("111444111","Arelance"));
+//DatosPersonales = new DatosPersonales("3", "Soltero");
+//Cargo = new Cargo("Becario")
+//Empresa = new Empresa("11111", "BBVA IT");
+//DatosLaboral = new DatosLaboral("1.582", Cargo());
+
+edu.nombre="david";
+
+
+alert(edu.nombre);
 
 
 //alert(Empleado.)
