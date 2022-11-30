542a4760268b7b2f783013cfb9a2676d72cd641b
OscarP72 <116722711+OscarP72@users.noreply.github.com>
2022-11-30 11:22:05 +0100

    a

--- empleadojson.js
+++ empleadojson.js
@@ -20,6 +20,12 @@ class Empresa {
 
     }
 }
+class Cargo {
+    constructor(descripcion) {
+        this.descripcion = descripcion;
+
+    }
+}
 
 class DatosLaborales {
     constructor(salario, cargo) {
@@ -29,46 +35,36 @@ class DatosLaborales {
     }
 }
 
-class Cargo {
-    constructor(descripcion) {
-        this.descripcion = descripcion;
-
-    }
-}
-
 class DatosPersonales {
-    constructor(Hijo, EstadoCivil) {
-        this.Hijo = Hijo;
-        this.EstadoCivil = EstadoCivil
+    constructor(hijo, estadoCivil) {
+        this.hijo = hijo;
+        this.estadoCivil = estadoCivil;
     }
 
 }
 
 class Empleado {
-    constructor(nombre, apellidos, dni, fechadenacimiento, fechadealta, fechadebaja, datosPersonales, datosLaborales, empresa) {
+    constructor(nombre, apellidos, dni, fechaDeNacimiento, fechaDeAlta, fechaDeBaja, datosPersonales, datosLaborales, empresa) {
         this.nombre = nombre;
         this.apellidos = apellidos;
         this.dni = dni;
-        this.fechadenacimiento = fechadenacimiento;
-        this.fechadealta = fechadealta;
-        this.fechadebaja = fechadebaja;
+        this.fechaDeNacimiento = fechaDeNacimiento;
+        this.fechaDeAlta = fechaDeAlta;
+        this.fechaDeBaja = fechaDeBaja;
         this.datosPersonales = datosPersonales;
         this.datosLaborales = datosLaborales;
         this.empresa = empresa;
     }
+   
 }
 
-//estadoCivil = new EstadoCivil("descripcion");
-//hijo = new Hijo("chicos", "chicas");
-//empresa = new Empresa("cif", "nombre");
-//datosLaborales = new DatosLaborales(salario,"cargo");
-//cargo = new Cargo("descripcion");
-//datosPersonales = new DatosPersonales("estadocivil", "Hijo");
 
-//juan = new Empleado("Juan", "Rodriguez", "52128521P", new Date("1970/02/15"), new Date("2008/02/08"), null, new DatosPersonales(new Hijo(3, 2), new EstadoCivil("casado")), new DatosLaborales(1500, new Cargo("jefe de sección")), new Empresa("B12345678","Telefonica"));
-//console.log(`El señor ${juan.nombre} ${juan.apellidos} con documento ${juan.dni} nacido el ${juan.fechadenacimiento} que empezo a trabajar con nosotros el ${juan.fechadealta} y que sigue con nosotros en la ${juan.fechadebaja} con hijos${juan.DatosPersonales} que trabaja de ${juan.datosLaborales} y su nombre es ${juan.empresa.nombre}`)
-//console.log (juan)
 
-oscar= new Empleado ("Oscar","Perea Alvarez","52125125P", new DatosPersonales(new Hijo(3,2),new EstadoCivil("divorciado")),new DatosLaborales(3000, new Cargo("Analista Programador")),new Empresa("B123456789","Fundación Once"))
-console.log(`El señor ${oscar.nombre} ${oscar.apellidos} con documento ${oscar.dni} tiene ${oscar.DatosPersonales.Hijo.chicos} hijos ${oscar.DatosPersonales.Hijo.chicas} hijas y estado civil de ${oscar.DatosPersonales.EstadoCivil} ocupa el cargo de ${oscar.DatosLaborales.Cargo} y tiene un sueldo de ${oscar.DatosLaborales.salario} trabaja en la empresa ${oscar.Empresa.nombre} con un CIF ${oscar.Empresa.cif}`)
-console.log (oscar)
+
+juan = new Empleado("Juan", "Rodriguez", "52128521P", new Date("1970/02/15").toLocaleDateString(), new Date("2008/02/08").toLocaleDateString(), null, new DatosPersonales(new Hijo(3, 2), new EstadoCivil("casado")), new DatosLaborales(1500, new Cargo("jefe de sección")), new Empresa("B12345678", "Telefonica"));
+console.log(`El señor ${juan.nombre} ${juan.apellidos} con documento ${juan.dni} nacido el ${juan.fechadenacimiento} que empezo a trabajar con nosotros el ${juan.fechadealta} y que sigue con nosotros en la ${juan.fechadebaja} con ${juan.datosPersonales.hijo.chicos} hijos, que trabaja de ${juan.datosLaborales.cargo.descripcion} en la empresa ${juan.empresa.nombre}`)
+console.log(juan)
+
+oscar = new Empleado("Oscar", "Perea Alvarez", "52125125P",null,null,null, new DatosPersonales(new Hijo(3, 2), new EstadoCivil("divorciado")), new DatosLaborales(3000, new Cargo("Analista Programador")), new Empresa("B123456789", "Fundación Once"))
+console.log(`El señor ${oscar.nombre} ${oscar.apellidos} con documento ${oscar.dni} tiene ${oscar.datosPersonales.hijo.chicos} hijos y ${oscar.datosPersonales.hijo.chicas} hijas y estado civil de ${oscar.datosPersonales.estadoCivil.descripcion} ocupa el cargo de ${oscar.datosLaborales.cargo.descripcion} y tiene un sueldo de ${oscar.datosLaborales.salario} trabaja en la empresa ${oscar.empresa.nombre} con un CIF ${oscar.empresa.cif}`)
+console.log(oscar)
