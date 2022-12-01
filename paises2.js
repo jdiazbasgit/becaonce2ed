a1e40c3f895e8e0e91fc9d9339bc3b1aa74e1d32
Danielcojico <116722667+Danielcojico@users.noreply.github.com>
2022-12-01 13:41:59 +0100

    a

--- paises2.js
+++ paises2.js
@@ -14,7 +14,7 @@ elegirContinente = (continente) => {
         console.log("La capital de " + pais.name.common + " es " + pais.capital)
 
         var nombredepais = pais.name.common;
-        var nuevaoption.value = nombredepais;
+        var nuevaoption; value = nombredepais;
         selectorpaises.appendChild(nuevaoption);
       }
       )
@@ -24,31 +24,45 @@ elegirContinente = (continente) => {
     })
 }
 
-elegirPais = (pais) => 
+elegirPais = (pais) =>
 
 
   selectorpaises.innerHTML = ""
-  var placeholder = document.createElement("option")
-  placeholder.textContent = "Selecciona Pais";
-  placeholder.value = null;
-  selectorpaises.appendChild(placeholder)
+placeholder = document.createElement("option")
+placeholder.textContent = "Selecciona Pais";
+placeholder.value = null;
+selectorpaises.appendChild(placeholder)
 
-  fetch("https://restcountries.com/v3.1/name/" + pais)
-    .then(response => response.json())
-    .then(datos => {
+fetch("https://restcountries.com/v3.1/name/" + pais)
+  .then(response => response.json())
 
-      document.querySelectorAll(".nombreDePaisClass").innerHTML = pais
-      pais.forEach(pais => {
 
-        console.log("La capital de " + pais.name.common + " es " + pais.capital)
-        var selectorpaises = document.getElementById("seleccionaPais")
-        var nombreDePais = pais.name.common;
-        var nuevaoption = document.createElement("option")
-        nuevaoption.textContent = nombreDePais;
-        nuevaoption.value = nombredepais;
-        selectorpaises.appendChild(nuevaoption);
- ).catch(error => {
+document.querySelectorAll(".nombreDePaisClass").innerHTML = pais
+pais.forEach("Pais=>")
 
-        })
-    })
+console.log(`La capital de${pais.name.common}es${pais.capital}`)
+var selectorpaises = document.getElementById("seleccionaPais")
+var nombreDePais = pais.name.common;
+var nuevaoption = document.createElement("option")
+nuevaoption.textContent = nombreDePais;
+nuevaoption.value = nombredepais;
+selectorpaises.appendChild(nuevaoption);
+[].catch(error)
+var capital
+var poblacion
+var area
+var bandera
+var escudo
+var moneda
+
+
+function cargarDatos(pais)
+dameDatos("https://restcountries.com/v2/all" + pais)
+  .then(paises => { let miPais = paises })
+capital = miPais.capital
+poblacion = miPais.population
+area = miPais.area
+bandera = miPais.flags.svg
+moneda = miPais.currencies
+escudo = miPais.coatofArms.svg
 
