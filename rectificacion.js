4220cc732239416645972e9aeade255fd639ed8d
OscarP72 <116722711+OscarP72@users.noreply.github.com>
2022-12-01 13:50:41 +0100

    a

--- rectificacion.js
+++ rectificacion.js
@@ -1,3 +1,7 @@
+
+
+var urlRestCountries = "https://restcountries.com/v3.1/";
+
 var capital
 var poblacion
 var area
@@ -15,10 +19,14 @@ var traduccionesDatos = []
 class Traduccion {
     constructor(traduccionDelNombre, paisesDelIdioma) {
         this.traduccionDelNombre = traduccionDelNombre;
-        this.paisesDelIdioma = paisesDelIdioma:
+        this.paisesDelIdioma = paisesDelIdioma;
     }
 }
-
+dameDatos = (url) => fetch(url)
+    .then(response => {
+        if (response.status == 200)
+            return response.json()
+    })
 
 function cargarDatos(pais) {
     dameDatos("https://restcountries.com/v3.1/name/" + pais)
@@ -56,8 +64,15 @@ function cargarDatos(pais) {
 
                 })
         })
-        //while(capital==undefined && poblacion ==)
+   
 }
+
+
+
+
+
+
+
 cargarPaises = continente => {
     fetch("https://restcountries.com/v3.1/region/" + continente)
         .then(response => response.json())
@@ -70,7 +85,7 @@ cargarPaises = continente => {
             seleccionaPais.value = "0"
             seleccionaPais.text = "Selecciona pais";
             paisesSelect.appendChild(seleccionaPais);
-            
+
             paises.forEach(pais => {
 
                 let option = document.createElement("option");
@@ -81,5 +96,45 @@ cargarPaises = continente => {
             });
 
         })
+
+}
+
+dameDatosGenerales = () => {
+    let selectPaises = document.querySelector("#paises");
+    dameDatos( urlRestCountries + "/name/" + selectPaises.value)
+      .then((response) => response.json)
+      .then(pais => {
+        pintaDatosGenerales(pais)
+      })
+  }
+
+  
+pillarDatosGeograficos = () => {
+
+    var main = document.querySelector("main");
+    var div = document.createElement("div")
+    let h31 = document.createElement("h3")
+    let h32 = document.createElement("h3")
+    let h33 = document.createElement("h3")
+    let h34 = document.createElement("h3")
+    let h35 = document.createElement("h3")
+
+    h31.innerHTML = "Frontera 1" + pais.frontera   
+    h32.innerHTML = "Frontera 2" + pais.frontera
+    h33.innerHTML = "Frontera 3" + pais.frontera
+    h34.innerHTML = "Frontera 4" + pais.frontera
+    h35.innerHTML = "Frontera 5" + pais.frontera
+
+
+}
+
+
+
+
+datosGeograficos = () => {
+    let datosGeograficos = document.querySelector("#fronteras")
+    datosGeograficos.innerHTML = ""
+    let selectPaises = document.createElement("option")
+    selectPaises.value = "0"
 
 }
