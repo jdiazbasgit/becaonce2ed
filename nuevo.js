410409df6f23e9352fc0addb7d9e26ef193147a8
Your Name <you@example.com>
2022-12-01 12:10:40 +0100

    aa

---
+++ nuevo.js
@@ -0,0 +1,17 @@
+var capital
+var poblacion
+var dominios=[]
+var bandera=[]
+var escudos
+var monedas=[]
+var monedasDtos =^[]
+var traducciones=[]
+var fronterasNombres=[]
+var traduccionesNombres=[]
+
+function cargaDatos(pais) {
+    dameDatos ("https://restcountries.com/v3.1/name/" + pais)
+    .then (paises => {
+
+        })   
+}
