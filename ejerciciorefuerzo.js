0ebb5eed951ee00a7b44e0bced791c14ce8bce8c
CarlosC <valentinaemiliuscuad@gmail.com>
2022-12-02 23:07:12 +0100

    aa

--- ejerciciorefuerzo.js
+++
@@ -1,5 +0,0 @@
-function sumar(num1,num2) {
-var suma = num1 + num2
- console.log("+num1+" + "+num2+" +suma);
-}
-sumar(5+3);
---
+++ tarde.js
@@ -0,0 +1,3 @@
+function sumar();
+var suma = num1+num2;
+console.log("num1" + "num2" + suma);
