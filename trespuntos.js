2b9ab793d9f8ae62b593fdb6a41984af4cf41676
Danielcojico <116722667+Danielcojico@users.noreply.github.com>
2022-12-07 11:27:57 +0100

    a

---
+++ trespuntos.js
@@ -0,0 +1,21 @@
+function suma(...numeros){
+var resultado
+for (i=0;i<numeros.length;i++){
+    resultado=resultado+numeros^[i]
+}
+return resultado
+}
+console.log(suma(4,56,8))
+console.log(suma(3,2))
+console.log (suma(5,55,555,67898,98,765,908967,))
+
+numerosentrada=[3,4,5]
+function suma1(numeros)
+var resultado=0
+for (i=0;i<numeros.length;i++){
+    resultado=resultado+numeros[i]
+
+    return resultado
+}
+
+console.log(suma1(numerosentrada))
