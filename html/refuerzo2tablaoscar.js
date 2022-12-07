82f36895b33a4e5fad9151424399943d69a83a83
Your Name <you@example.com>
2022-12-07 19:07:05 +0100

    a

---
+++ html/refuerzo2tablaoscar.js
@@ -0,0 +1,25 @@
+function registrar() {
+    var usuario, clave, repetirclave, email, repetiremail;
+    usuario = document.getElementById("usuario").value
+    clave = document.getElementById("clave").value
+    repetirclave = document.getElementById("repetirclave").value
+    email = document.getElementById("email").value
+    repetiremail = document.getElementById("repetiremail").value
+
+    if (clave != repetirclave) {
+        alert("la contraseña no es igual")
+        return false
+
+    }
+   else {
+    return true
+   }
+    if (email != repetiremail){
+        alert ("el email no coincide")
+        return false
+    }
+    else {
+        return true
+    }
+}
+console.log(registrar())
