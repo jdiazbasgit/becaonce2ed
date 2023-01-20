5f4f2c89d46d351b8bb57f359a36fbecb903376f
jdiazbasgit <git@studiotapal.es>
2023-01-20 13:54:33 +0100

    arreglo

--- src/ejemploStream/input/LeerTexto.java
+++ src/ejemploStream/input/LeerTexto.java
@@ -8,32 +8,19 @@ import java.io.InputStreamReader;
 public class LeerTexto {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
 
 		try (FileInputStream fileInputStream = new FileInputStream("index.html");) {
 
 			InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
 			BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
 			while (bufferedReader.ready()) {
-=======
-		
-		try (FileInputStream fileInputStream= new FileInputStream("index.html");
-				InputStreamReader inputStreamReader= new InputStreamReader(fileInputStream);
-				BufferedReader bufferedReader= new BufferedReader(inputStreamReader);) {
-			
-			while(bufferedReader.ready()) {
->>>>>>> branch 'ejemploStream' of https://github.com/jdiazbasgit/becaonce2ed.git
+
 				System.out.println(bufferedReader.readLine());
 			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-<<<<<<< HEAD
-=======
-		
-		
->>>>>>> branch 'ejemploStream' of https://github.com/jdiazbasgit/becaonce2ed.git
 
 	}
 
