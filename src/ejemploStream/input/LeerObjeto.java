4e53b39962513a92f27212df76dd568345d5375a
OscarP72 <116722711+OscarP72@users.noreply.github.com>
2023-01-20 13:42:05 +0100

    Merge branch 'ejemploStream' of https://github.com/jdiazbasgit/becaonce2ed.git into autocloseable/ejemploStream/op

--- src/ejemploStream/input/LeerObjeto.java
+++ src/ejemploStream/input/LeerObjeto.java
@@ -9,29 +9,16 @@ import ejemploStream.output.Agenda;
 public class LeerObjeto {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
 
 		try (FileInputStream fileInputStream = new FileInputStream("pepe.agenda");) {
 
 			ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
 			Agenda pepe = (Agenda) objectInputStream.readObject();
 			System.out.println(pepe.getNombre() + " - " + pepe.getDireccion() + " - " + pepe.getTelefono());
-=======
-		
-		try (FileInputStream fileInputStream= new FileInputStream("pepe.agenda");
-				ObjectInputStream objectInputStream= new ObjectInputStream(fileInputStream);) {
-			
-			Agenda pepe= (Agenda) objectInputStream.readObject();
-			System.out.println(pepe.getNombre()+" - "+pepe.getDireccion()+" - "+pepe.getTelefono());
->>>>>>> branch 'ejemploStream' of https://github.com/jdiazbasgit/becaonce2ed.git
 		} catch (IOException | ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-<<<<<<< HEAD
-=======
-		
->>>>>>> branch 'ejemploStream' of https://github.com/jdiazbasgit/becaonce2ed.git
 
 	}
 
--- src/ejemploStream/output/EscribirObjeto.java
+++ src/ejemploStream/output/EscribirObjeto.java
@@ -8,20 +8,11 @@ import java.io.ObjectOutputStream;
 public class EscribirObjeto {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
 
 		try (FileOutputStream fileOutputStream = new FileOutputStream("pepe.agenda");) {
 
 			ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
 			Agenda pepe = new Agenda();
-=======
-	
-		
-		try (FileOutputStream fileOutputStream= new FileOutputStream("pepe.agenda");
-				ObjectOutputStream objectOutputStream= new ObjectOutputStream(fileOutputStream);){
-			
-			Agenda pepe= new Agenda();
->>>>>>> branch 'ejemploStream' of https://github.com/jdiazbasgit/becaonce2ed.git
 			pepe.setNombre("pepe garcia");
 			pepe.setDireccion("calle del pez,4");
 			pepe.setTelefono("654654654");
@@ -32,10 +23,6 @@ public class EscribirObjeto {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-<<<<<<< HEAD
-=======
-	
->>>>>>> branch 'ejemploStream' of https://github.com/jdiazbasgit/becaonce2ed.git
 
 	}
 
