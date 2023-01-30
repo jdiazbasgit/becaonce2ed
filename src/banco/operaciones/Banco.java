a9a00e0a2dcb0890598ead7056ba2c43785e5c63
DevSniffer <116722849+DevSniffer@users.noreply.github.com>
2023-01-30 13:36:37 +0100

    inicio

--- src/banco/operaciones/Banco.java
+++ src/banco/operaciones/Banco.java
@@ -29,7 +29,18 @@ public class Banco {
 
 	public  Map<Cuenta, List<Movimiento>> cuentas;
 	
-	
+	private String nombreDelBanco; 
+
+	public String getNombreDelBanco() {
+		return nombreDelBanco;
+	}
+	public void setNombreDelBanco(String nombreDelBanco) {
+		this.nombreDelBanco = nombreDelBanco;
+	}
+	public Banco(String nombreDelBanco) {
+		setNombreDelBanco(nombreDelBanco);
+	}
+
 
 
 	public void crearCuenta() {
