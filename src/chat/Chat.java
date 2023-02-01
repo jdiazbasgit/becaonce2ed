9e152163f7fa6b1d502877d08f366b97291ad815
LuisFer-DesignForAll <luisfer.designforall@gmail.com>
2023-02-02 00:01:41 +0100

    Ventana de chat

--- src/chat/Chat.java
+++ src/chat/Chat.java
@@ -1,11 +1,13 @@
 package chat;
 
 import java.awt.BorderLayout;
+import java.awt.Button;
 import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.Frame;
 import java.awt.Label;
 import java.awt.Panel;
+import java.awt.TextField;
 
 import javax.swing.JFrame;
 
@@ -15,86 +17,39 @@ import chat.eventos.ElQueSabeLoQueHayQueHacerConLaVentana;
 public class Chat extends Frame {
 	
 	public Chat() {
+
 		this.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		this.addWindowListener(new ElQueSabeLoQueHayQueHacerConLaVentana());
 		
-		Label l2 = new Label("dos");
-		l2.setBackground(Color.CYAN);
+	    Panel pSuperior = new Panel();
+	    pSuperior.setBackground(Color.YELLOW);
+	    pSuperior.setLayout(new FlowLayout(FlowLayout.CENTER,100,25));
+	    pSuperior.add(new Label("Usuario"));
+	    pSuperior.add(new TextField("Nombre...", 0));
+	    pSuperior.add(new Button("Registrar"));
 
-		Panel pSuperior= new Panel();
-		pSuperior.setLayout(new FlowLayout());
-		pSuperior.add(l2);
-		Panel pCentral= new Panel();
-		pCentral.setBackground(Color.LIGHT_GRAY);
-		pCentral.setLayout(new BorderLayout());
-		pCentral.add(pSuperior,BorderLayout.NORTH);
+	    Panel pInferior = new Panel();
+	    pInferior.setBackground(Color.GREEN);
+	    pInferior.setLayout(new FlowLayout(FlowLayout.CENTER,100,25));
+	    pInferior.add(new Label("Mensaje"));
+	    pInferior.add(new TextField("Comentar...", 0));
+	    pInferior.add(new Button("Enviar"));
 
+	    Panel pIzquierda = new Panel();
+	    pIzquierda.setBackground(Color.BLUE);
+	    pIzquierda.setLayout(new FlowLayout(FlowLayout.CENTER,25,100));
+	    pIzquierda.add(new Label("Usuarios"));
 
+	    Panel pCentro = new Panel();
+	    pCentro.setBackground(Color.PINK);
+	    pCentro.setLayout(new FlowLayout(FlowLayout.CENTER,25,100));
+	    pCentro.add(new Label("Area de mensajes"));
+
+	    this.add(pSuperior, BorderLayout.NORTH);
+	    this.add(pInferior, BorderLayout.SOUTH);
+	    this.add(pIzquierda, BorderLayout.WEST);
+	    this.add(pCentro);
+	    
 	}
 	
-/*
- * import java.awt.BorderLayout;
-import java.awt.Button;
-import java.awt.Checkbox;
-import java.awt.Color;
-import java.awt.FlowLayout;
-import java.awt.Frame;
-import java.awt.GridLayout;
-import java.awt.Label;
-import java.awt.Panel;
-
-@SuppressWarnings("serial")
-public class EjemploLayouts extends Frame {
-
-	public EjemploLayouts() {
-		this.setSize(800, 800);
-		this.setLayout(new GridLayout(2, 2));
-		
-		Label l1 = new Label("uno");
-		Label l2 = new Label("dos");
-		l2.setBackground(Color.CYAN);
-		Label l3 = new Label("tres");
-		Label l4 = new Label("cuatro");
-
-		Panel p1 = new Panel();
-		Panel p2 = new Panel();
-		Panel p3 = new Panel();
-		p1.setBackground(Color.RED);
-		p2.setBackground(Color.GREEN);
-		p3.setBackground(Color.YELLOW);
-		
-		Panel pCentral= new Panel();
-		pCentral.setBackground(Color.LIGHT_GRAY);
-		pCentral.setLayout(new BorderLayout());
-		pCentral.add(l1,BorderLayout.SOUTH);
-		Panel pSuperior= new Panel();
-		pSuperior.setLayout(new FlowLayout());
-		pSuperior.add(l2);
-		pCentral.add(pSuperior,BorderLayout.NORTH);
-		pCentral.add(l3,BorderLayout.EAST);
-		pCentral.add(l4,BorderLayout.WEST);
-		
-		this.add(pCentral);
-		this.add(p1);
-		this.add(p2);
-		//this.add(p3);
-		
-		
-		
-		Panel pFlow= new Panel();
-		pFlow.add(new Checkbox());
-		pFlow.add(new Checkbox());
-		pFlow.add(new Checkbox());
-		pFlow.add(new Checkbox());
-		pFlow.add(new Checkbox());
-		
-		pCentral.add(pFlow,BorderLayout.CENTER);
-		
-		
-
-	}
-}
-
-
- */
 }
