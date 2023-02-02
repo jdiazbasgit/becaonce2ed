977c1da481c8ca2558de7faf93a2bc944307780f
LuisFer-DesignForAll <luisfer.designforall@gmail.com>
2023-02-02 11:19:18 +0100

    cambio otra vez

--- src/chat/Chat.java
+++ src/chat/Chat.java
@@ -9,8 +9,7 @@ import java.awt.Frame;
 import java.awt.Label;
 import java.awt.Panel;
 import java.awt.TextArea;
-
-
+import java.awt.TextField;
 
 import javax.swing.JFrame;
 import chat.eventos.ElQueSabeLoQueHayQueHacerConLaVentana;
@@ -112,39 +111,5 @@ setExtendedState(JFrame.MAXIMIZED_BOTH);
 		this.lMensajes = lMensajes;
 	}
 
-=======
-
-	public Chat() {
-		//setExtendedState(JFrame.MAXIMIZED_BOTH);
-		this.setSize(800, 600);
-		this.addWindowListener(new ElQueSabeLoQueHayQueHacerConLaVentana());
-		this.setLayout(new BorderLayout());
-		Dimension tamanoActualizableFrame = this.getSize();
-		System.out.println(tamanoActualizableFrame);
-	    
-		
-		Panel pNorte = new Panel();
-	    Panel pSur = new Panel();
-	    Panel pOeste = new Panel();
-	    Panel pCentro = new Panel();
-	    
-	    pNorte.setBackground(Color.BLUE);
-	    pSur.setBackground(Color.DARK_GRAY);
-	    pOeste.setBackground(Color.ORANGE);
-	    pCentro.setBackground(Color.LIGHT_GRAY);
-
-	    
-	    this.add(pNorte, BorderLayout.NORTH);
-	    this.add(pSur, BorderLayout.SOUTH);
-	    this.add(pOeste, BorderLayout.WEST);
-	    this.add(pCentro, BorderLayout.CENTER);	  
-	    
-	    pNorte.setLayout(new FlowLayout(FlowLayout.CENTER,90,50));
-	    pNorte.add(new Label("Usuario"));
-	    pNorte.add(new TextField("Introduce tu nombre", 0));
-	    pNorte.add(new Button("Registrar"));
-	   
-	
-	}
 
 }
