3409db6276b92f55c0721dcab91a30e75a51a825
Your Name <you@example.com>
2023-02-02 11:19:44 +0100

    hh

--- src/chat/Chat.java
+++ src/chat/Chat.java
@@ -1,6 +1,14 @@
 package chat;
 
+import java.awt.BorderLayout;
+import java.awt.Button;
+import java.awt.Checkbox;
+import java.awt.Color;
+import java.awt.FlowLayout;
 import java.awt.Frame;
+import java.awt.GridLayout;
+import java.awt.Label;
+import java.awt.Panel;
 
 import javax.swing.JFrame;
 
@@ -12,8 +20,22 @@ public class Chat extends Frame {
 	public Chat() {
 		setExtendedState(JFrame.MAXIMIZED_BOTH);
 		this.addWindowListener(new ElQueSabeLoQueHayQueHacerConLaVentana());
-		
+	this.setLayout(new GridLayout(3,3));
 		//componentes
+		
+		Label label1= new Label("usuario");
+		Label label2 = new Label("mensaje");
+		Label label3 = new Label ("enviar");
+		
+		
+		Panel p1 = new Panel();
+		Panel p2 = new Panel();
+		Panel p3 = new Panel();
+		Panel p4 = new Panel();
+		Panel pCentral = new Panel();
+		
+		
+		
 	}
 	
 
