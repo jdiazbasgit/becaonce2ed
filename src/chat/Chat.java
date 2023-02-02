ed70814a7c1cf02627b9c8c99ffce04a4332f246
LuisFer-DesignForAll <luisfer.designforall@gmail.com>
2023-02-02 14:14:40 +0100

    Ventana chat completa, ok

--- src/chat/Chat.java
+++ src/chat/Chat.java
@@ -1,15 +1,16 @@
 package chat;
 
+import java.awt.BorderLayout;
 import java.awt.Button;
+import java.awt.Color;
+import java.awt.FlowLayout;
 import java.awt.Frame;
 import java.awt.Label;
 import java.awt.Panel;
 import java.awt.TextArea;
-
 import java.awt.TextField;
 
 import javax.swing.JFrame;
-
 
 import chat.eventos.ElQueSabeLoQueHayQueHacerConLaVentana;
 
@@ -25,17 +26,63 @@ public class Chat extends Frame {
 
 	public Chat() {
 		setExtendedState(JFrame.MAXIMIZED_BOTH);
+		
 		/*
 		 * Dimension dimension=Toolkit.getDefaultToolkit().getScreenSize();
 		 * setSize(dimension.width/2,dimension.height/2);
 		 * setLocation(dimension.width/4,dimension.height/4);
 		 */
-		this.addWindowListener(new ElQueSabeLoQueHayQueHacerConLaVentana());
-		setbRegistrar(new Button("Nick:"));
 
-		// componentes
+		this.addWindowListener(new ElQueSabeLoQueHayQueHacerConLaVentana());
+		
+		setbRegistrar(new Button("Registrar"));
+		setbEnviar(new Button("Enviar"));
+		
+		settNick(new TextField("Nombre...", 0));
+		settMensaje(new TextField("Comentar...", 0));
+		
+		setpSuperior(new Panel());
+		setpInferior(new Panel());
+		setpIzquierda(new Panel());
+		setpIzquierdaSuperior(new Panel());
+
+		setTaMensajes(new TextArea());
+		setTaUsuarios(new TextArea());
+		
+		setlNick(new Label("Nick:"));
+		setlUsuarios(new Label("Usuarios:"));
+		setlMensajes(new Label("Mensaje:"));
+		
+		pSuperior.setBackground(Color.YELLOW);
+		pSuperior.setLayout(new FlowLayout(FlowLayout.CENTER,100,25));
+	    pSuperior.add(getlNick());
+	    pSuperior.add(gettNick());
+	    pSuperior.add(getbRegistrar());
+	    this.add(pSuperior, BorderLayout.NORTH);
+		
+	    pInferior.setBackground(Color.GREEN);
+	    pInferior.setLayout(new FlowLayout(FlowLayout.CENTER,100,25));
+	    pInferior.add(getlMensajes());
+	    pInferior.add(gettMensaje());
+	    pInferior.add(getbEnviar());
+	    this.add(pInferior, BorderLayout.SOUTH);
+	    
+	    pIzquierdaSuperior.setBackground(Color.CYAN);
+	    pIzquierdaSuperior.setLayout(new FlowLayout());
+	    pIzquierdaSuperior.add(getlUsuarios());
+
+	    pIzquierda.setBackground(Color.BLUE);
+	    pIzquierda.setLayout(new BorderLayout());
+	    pIzquierda.add(getpIzquierdaSuperior(),BorderLayout.NORTH);
+	    pIzquierda.add(getTaUsuarios(),BorderLayout.CENTER);
+	    this.add(pIzquierda, BorderLayout.WEST);
+
+	    this.add(getTaMensajes(),BorderLayout.CENTER);
+
 	}
 
+	
+	
 	public Button getbRegistrar() {
 		return bRegistrar;
 	}
@@ -74,7 +121,6 @@ public class Chat extends Frame {
 
 	public void setpSuperior(Panel pSuperior) {
 		this.pSuperior = pSuperior;
-
 	}
 
 	public Panel getpInferior() {
