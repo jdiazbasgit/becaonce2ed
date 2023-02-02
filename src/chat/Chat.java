2206c91a24a1df9064571ad57e21234a00cb53a0
Your Name <you@example.com>
2023-02-02 09:09:44 +0100

    cambio commit

--- src/chat/Chat.java
+++ src/chat/Chat.java
@@ -1,20 +1,22 @@
 package chat;
-
+import java.awt.BorderLayout;
 import java.awt.Frame;
 
 import javax.swing.JFrame;
 
 import chat.eventos.ElQueSabeLoQueHayQueHacerConLaVentana;
-
 @SuppressWarnings("serial")
 public class Chat extends Frame {
 	
 	public Chat() {
 		setExtendedState(JFrame.MAXIMIZED_BOTH);
 		this.addWindowListener(new ElQueSabeLoQueHayQueHacerConLaVentana());
+		this.setSize(800, 800);
+		this.setLayout(new BorderLayout());
 		
-		//componentes
-	}
 	
+		
+		
+	}
 
 }
