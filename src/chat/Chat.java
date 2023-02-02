f10f9e46a9de5437361ccfbe863d41f32e35ab0e
OscarP72 <116722711+OscarP72@users.noreply.github.com>
2023-02-02 09:09:21 +0100

    propiedades

--- src/chat/Chat.java
+++ src/chat/Chat.java
@@ -1,10 +1,17 @@
 package chat;
 
+import java.awt.BorderLayout;
+import java.awt.Button;
+import java.awt.Color;
 import java.awt.Frame;
+import java.awt.Label;
+import java.awt.Panel;
+import java.awt.TextArea;
 
 import javax.swing.JFrame;
 
 import chat.eventos.ElQueSabeLoQueHayQueHacerConLaVentana;
+import javafx.scene.control.TextField;
 
 @SuppressWarnings("serial")
 public class Chat extends Frame {
@@ -13,7 +20,30 @@ public class Chat extends Frame {
 		setExtendedState(JFrame.MAXIMIZED_BOTH);
 		this.addWindowListener(new ElQueSabeLoQueHayQueHacerConLaVentana());
 		
-		//componentes
+		
+		Label lNick=new Label ("NICK");
+		Label lMensaje= new Label ("MENSAJE");
+		Label lUsuario= new Label("USUARIO");
+		
+		Button bButton= new Button ("REGISTRAR");
+		Button bButton1=new Button ("ENVIAR");
+		
+		TextArea tTextArea= new TextArea("USUARIOS");
+		TextArea tTextArea2=new TextArea("MENSAJES");
+		
+		TextField tNick= new TextField ();
+		TextField tMensaje= new TextField ();
+				
+		
+		
+		
+		Panel pSuperior= new Panel();
+		Panel pInferior= new Panel();
+		Panel pIzquierda=new Panel();
+		Panel pDerecha= new Panel();
+		pSuperior.setBackground(Color.gray);
+		pSuperior.setLayout(new BorderLayout());
+		
 	}
 	
 
