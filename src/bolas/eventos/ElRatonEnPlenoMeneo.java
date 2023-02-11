e568bba227842ae9bf50570d99a68d09d1b8ef57
CarlosC <valentinaemiliuscuad@gmail.com>
2023-02-11 01:08:37 +0100

    a

--- src/bolas/eventos/ElRatonEnPlenoMeneo.java
+++ src/bolas/eventos/ElRatonEnPlenoMeneo.java
@@ -1,92 +1,71 @@
 package bolas.eventos;
 
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseMotionListener;
+import java.awt.event.MouseListener;
 
+import bolas.bolas.Pelota;
+import bolas.hilos.PelotaHilo;
 import bolas.ventanas.VentanaPelota;
 
-
-public class ElRatonEnPlenoMeneo implements MouseMotionListener {
+public class ElRatonEnPlenoMeneo implements MouseListener {
 
 	
-
 	private VentanaPelota ventanaPelota;
 
 	public ElRatonEnPlenoMeneo(VentanaPelota ventanaPelota) {
 		this.ventanaPelota = ventanaPelota;
+
 	}
 
-	/*
-	 * public void mouseDragged(MouseEvent e) { if (e.getModifiersEx() == 1024)
-	 * getCursor().getExterno().setColor(Color.YELLOW); if (e.getModifiersEx() ==
-	 * 4096) getCursor().getExterno().setColor(Color.BLACK); colocarCursor(e); }
-	 * 
-	 * public void mouseMoved(MouseEvent e) {
-	 * getCursor().getExterno().setColor(Color.CYAN); colocarCursor(e);
-	 * 
-	 * }
-	 * 
-	 * private void colocarCursor(MouseEvent e) {
-	 * getCursor().setPosicionX(e.getX()); getCursor().setPosicionY(e.getY());
-	 * getCursor().repaint(); }
-	 */
 	public int dameIncremento() {
-		int valor = (int) (Math.random() * 20);
+		int valor = (int) (Math.random() * 10);
 		if (valor % 2 == 0)
 			return 1;
 		return 2;
 
 	}
 
-	
 	public int dameSentido() {
-		int valor = (int) (Math.random() * 20);
+		int valor = (int) (Math.random() * 10);
 		if (valor % 2 == 0)
-			
-		return 1;
+			return 1;
 		return -1;
 
 	}
 
-	public VentanaPelota getVentanaPelota() {
+	public void mouseClicked(MouseEvent e) {
+		Pelota pelota = new Pelota(e.getX(), e.getY(), dameIncremento(), dameIncremento(), dameSentido(), dameSentido(),
+				50);
+		PelotaHilo pelotaHilo = new PelotaHilo(pelota, getVentanaPelota());
+		pelotaHilo.start();
+		getVentanaPelota().getPelota();
+
+	}
+
+	public  VentanaPelota getVentanaPelota() {
 		return ventanaPelota;
 	}
 
-	public void setVentanaPelota(VentanaPelota ventanaPelota, VentanaPelota ventanaPelota1) {
-		this.ventanaPelota = ventanaPelota1;
+	@SuppressWarnings("unused")
+	private void  setVentanaPelota(VentanaPelota ventanaPelota) {
+       this.ventanaPelota = ventanaPelota;
+		
 	}
 
 	public void mousePressed(MouseEvent e) {
-		
+
 	}
 
 	public void mouseReleased(MouseEvent e) {
-		
 
 	}
 
 	public void mouseEntered(MouseEvent e) {
-		
+
 	}
 
 	public void mouseExited(MouseEvent e) {
-		
+
 	}
-
-	@Override
-	public void mouseDragged(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseMoved(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-
-	
-	
 
 }
--- src/bolas/ventanas/VentanaPelota.java
+++ src/bolas/ventanas/VentanaPelota.java
@@ -1,27 +1,100 @@
 package bolas.ventanas;
 
+import java.awt.Graphics;
+import java.awt.Image;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JFrame;
+
 import bolas.bolas.Pelota;
+import bolas.eventos.ElRatonEnPlenoMeneo;
+import bolas.hilos.Pintor;
 
-public class VentanaPelota {
+@SuppressWarnings("serial")
+public class VentanaPelota extends JFrame {
 
-	public void setVisible(boolean b) {
+	private Image image;
+	private Graphics externo;
+	private boolean primeravez;
+	private List<Pelota> pelota;
+	private Image imagen;
+
+	public VentanaPelota() {
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setExtendedState(JFrame.MAXIMIZED_BOTH);
+		setPrimeraVez(true);
+		setPelota(new ArrayList<>());
+		this.addMouseListener(new ElRatonEnPlenoMeneo(this));
+	}
+
+	private void setPelota(ArrayList arrayList) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	public Pelota[] getPelota() {
+	@SuppressWarnings("unused")
+	public void paint(Graphics g) {
+		if (isPrimeraVez()) {
+			setImagen(createImage(this.getWidth(), this.getHeight()));
+			setExterno(getImagen().getGraphics());
+			Pintor pintor = Pintor(this);
+			pintor.start();
+			setPrimeraVez(false);
+		}
+		try {
+		Thread.sleep(5);
+		}catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		getExterno().clearRect(0, 0, this.getWidth(), this.getHeight());
+		for (Pelota pelota : getPelota()) {
+			getExterno().fillOval(pelota.getPosicionX(), pelota.getPosicionY(), pelota.getDimension(), pelota.getDimension());
+		}
+		g.drawImage(getImagen(), 0, 0, this);
+	}
+	private Pintor Pintor(VentanaPelota ventanaPelota) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	public int getWidth() {
-		// TODO Auto-generated method stub
-		return 0;
+	public Image getImagen() {
+		return imagen;
 	}
 
-	public int getHeight() {
-		// TODO Auto-generated method stub
-		return 0;
+	public void setImagen(Image imagen) {
+		this.imagen = imagen;
 	}
 
+	public Graphics getExterno() {
+		return externo;
+	}
+
+	public void setExterno(Graphics externo) {
+		this.externo = externo;
+	}
+
+	public boolean isPrimeraVez() {
+		return primeravez;
+	}
+
+	public void setPrimeraVez(boolean primeraVez) {
+		this.primeravez = primeraVez;
+	}
+
+	/*
+	 * public Bola getBola() { return bola; }
+	 * 
+	 * public void setBola(Bola bola) { this.bola = bola; }
+	 */
+
+	public List<Pelota> getPelota() {
+		return pelota;
+	}
+
+	public void setBolas(List<Pelota> pelota) {
+		this.pelota = pelota;
+	}
+
+	
 }
