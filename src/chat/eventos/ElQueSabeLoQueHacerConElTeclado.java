492bc13a3d78088bb533e7dd4c1c3f1a03608b1a
Your Name <you@example.com>
2023-02-14 14:13:59 +0100

    Clases creadas

---
+++ src/chat/eventos/ElQueSabeLoQueHacerConElTeclado.java
@@ -0,0 +1,30 @@
+package chat.eventos;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class ElQueSabeLoQueHacerConElTeclado implements KeyListener {
+
+	public ElQueSabeLoQueHacerConElTeclado() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/chat/recepciones/servidor/EscuchaMensajeServidor.java
@@ -0,0 +1,21 @@
+package chat.recepciones.servidor;
+
+import java.net.Socket;
+
+import chat.ChatGridBag;
+import chat.recepciones.Servidor;
+
+public class EscuchaMensajeServidor extends Servidor {
+
+	public EscuchaMensajeServidor(int puerto, ChatGridBag chat) {
+		super(puerto, chat);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void hacerAlgo(Socket socket) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
