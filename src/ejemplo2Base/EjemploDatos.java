8d891cb89788a26df6e16e8309a1f5f6d15770e6
DevSniffer <you@example.com>
2023-02-22 14:01:46 +0100

    a

--- src/ejemplo2Base/EjemploDatos.java
+++ src/ejemplo2Base/EjemploDatos.java
@@ -19,34 +19,35 @@ public class EjemploDatos {
 			conexion = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/company?useSSL=false",
 					"curso", "Cursocurso1;");
 			
-			PreparedStatement preparedStatement3 = conexion
-					.prepareStatement("select name FROM  employees");
+			PreparedStatement preparedStatement3 = conexion.prepareStatement("select em.name, li.salary, ch.guys, ms.description \r\n"
+					+ "from employees as em, laboral_information as li, children as ch, marital_statuses as ms, personal_information as pi\r\n"
+					+ "\r\n"
+					+ "where ms.id=pi.marital_statuses_id\r\n"
+					+ "and ch.id=pi.children_id\r\n"
+					+ "and li.id=em.LABORAL_INFORMATION_ID\r\n"
+					+ "and em.PERSONAL_INFORMATION_ID=pi.id;");
 			preparedStatement3.execute();
 			ResultSet resultSet3 = preparedStatement3.executeQuery();
 			while (resultSet3.next()) {
-				System.out.println("los nombres son los siguientes: "+ resultSet3.getString(1) );
-				
-			}
-
+				System.out.println("Conozcamos a "+ resultSet3.getString(1).toUpperCase() 
+				+ ", tiene un salario humilde de "+ resultSet3.getString(2)
+				+ " granos de arroz, que emplea para cazar palomas. Las guisa junto a "+ resultSet3.getString(3)
+				+ " hijo gandul que acogió tras quedarse "+ resultSet3.getString(4).toLowerCase()+"\n");				
+			}			
 			
-			
-			
-			PreparedStatement preparedStatement2 = conexion
-					.prepareStatement("select girls FROM  children");
+			/*PreparedStatement preparedStatement2 = conexion.prepareStatement("select girls FROM  children");
 			preparedStatement2.execute();
 			ResultSet resultSet = preparedStatement2.executeQuery();
 			while (resultSet.next()) {
-				System.out.println("sus hijas son: "+ resultSet.getString(1) );
-				
+				System.out.println("sus hijas son: "+ resultSet.getString(1) );				
 			}
 
-			PreparedStatement preparedStatement = conexion
-					.prepareStatement("SELECT salary FROM laboral_information");
+			PreparedStatement preparedStatement = conexion.prepareStatement("SELECT salary FROM laboral_information");
 			
 			ResultSet resultSet1 = preparedStatement.executeQuery();
 			while (resultSet1.next()) {
 				System.out.println("el sueldo es el siguiente: "+ resultSet1.getString(1) );
-			}
+			}*/
 
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
