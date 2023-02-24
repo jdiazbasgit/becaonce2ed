00eac6f64eedf220fb49dd18c9dd4427d76bc358
OscarP72 <116722711+OscarP72@users.noreply.github.com>
2023-02-24 11:14:46 +0100

    ejemplo datos

--- src/baseDatos/DatosEmpleado.java
+++ src/baseDatos/DatosEmpleado.java
@@ -16,8 +16,8 @@ public class DatosEmpleado {
 		Connection conexion = null;
 		try {
 			DriverManager.registerDriver(new Driver());
-			conexion = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/company?useSSL=false",
-					"curso", "Cursocurso1;");
+			conexion = DriverManager.getConnection("jdbc:mysql://82.223.202.137:3306/COMPANY?useSSL=false", "curso",
+					"Cursocurso1;");
 			PreparedStatement preparedStatement = conexion.prepareStatement("SELECT name FROM employees");
 			PreparedStatement preparedStatement1 = conexion.prepareStatement("SELECT salary FROM laboral_information");
 			PreparedStatement preparedStatement2 = conexion.prepareStatement("SELECT guys FROM children");
