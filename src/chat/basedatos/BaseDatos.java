336c7f9ceabdd7d59547e0d87ac7d6f86d4e6ae2
OscarP72 <116722711+OscarP72@users.noreply.github.com>
2023-02-27 13:00:13 +0100

    s

--- src/chat/basedatos/BaseDatos.java
+++ src/chat/basedatos/BaseDatos.java
@@ -9,36 +9,37 @@ import java.sql.SQLException;
 import com.mysql.jdbc.Driver;
 
 public class BaseDatos {
-public static final String CADENA="jdbc:mysql://82.223.202.137:3306/CHAT";
-public static final String USUARIO="curso";
-public static final String PASWORD="Cursocurso1;";
-public static final String QUERY_CONSULTA="SELECT CHAT.USUARIO FROM CHAT.USUARIOS";
- public static final String QUERY_ALTA="INSERT INTO CHAT.USUSARIO (USUARIO,IP) VALUES (?,?)";
- public static final String QUERY_BAJA="DELETE FROM CHAT.USUARIOS WHERE USUARIO=?";
- public static final String QUERY_IP="SELECT IP FRON CHAT.USUSARIOS WHERE USUARIO=?";
+	public static final String CADENA = "jdbc:mysql://82.223.202.137:3306/CHAT";
+	public static final String USUARIO = "curso";
+	public static final String PASWORD = "Cursocurso1;";
+	public static final String QUERY_CONSULTA = "SELECT CHAT.USUARIO FROM CHAT.USUARIOS";
+	public static final String QUERY_ALTA = "INSERT INTO CHAT.USUSARIO (USUARIO,IP) VALUES (?,?)";
+	public static final String QUERY_BAJA = "DELETE FROM CHAT.USUARIOS WHERE USUARIO=?";
+	public static final String QUERY_IP = "SELECT IP FRON CHAT.USUSARIOS WHERE USUARIO=?";
+
 	public BaseDatos() {
 	}
-	
+
 	public Connection getConexion() throws SQLException {
-		Connection conexion= null;
+		Connection conexion = null;
 		try {
 			DriverManager.registerDriver(new Driver());
-			conexion=DriverManager.getConnection(BaseDatos.CADENA,BaseDatos.USUARIO,BaseDatos.PASWORD);
+			conexion = DriverManager.getConnection(BaseDatos.CADENA, BaseDatos.USUARIO, BaseDatos.PASWORD);
 			return conexion;
 		} catch (SQLException e) {
 			throw e;
 		}
-		
+
 	}
-	
+
 	public void desconexion(Connection conexion) throws SQLException {
 		conexion.close();
 	}
-	
+
 	public ResultSet dameUsuarios() {
 		ResultSet resultSet = null;
 		try {
-			PreparedStatement preparedStatement =  getConexion().prepareStatement(BaseDatos.QUERY_CONSULTA);
+			PreparedStatement preparedStatement = getConexion().prepareStatement(BaseDatos.QUERY_CONSULTA);
 			resultSet = preparedStatement.executeQuery();
 			while (resultSet.next()) {
 				System.out.println(resultSet.getString(1));
@@ -48,46 +49,46 @@ public static final String QUERY_CONSULTA="SELECT CHAT.USUARIO FROM CHAT.USUARIO
 		}
 		return resultSet;
 	}
-	public void agregarUsuario(String usuario,String ip) {
-	
+
+	public void agregarUsuario(String usuario, String ip) {
+
 		try {
-			PreparedStatement preparedStatement=  getConexion().prepareStatement(BaseDatos.QUERY_ALTA);
-			preparedStatement.setString(1,usuario);
+			PreparedStatement preparedStatement = getConexion().prepareStatement(BaseDatos.QUERY_ALTA);
+			preparedStatement.setString(1, usuario);
 			preparedStatement.setString(2, ip);
 			preparedStatement.execute();
-						
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
+
 	public void quitarUsuario(String usuario0) {
-		
+
 		try {
-			PreparedStatement preparedStatement= getConexion().prepareStatement(BaseDatos.QUERY_BAJA);
-			preparedStatement.setString(1,USUARIO);
+			PreparedStatement preparedStatement = getConexion().prepareStatement(BaseDatos.QUERY_BAJA);
+			preparedStatement.setString(1, USUARIO);
 			preparedStatement.execute();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
-	
+
 	public String dameIpDeUsuario(String usuario) {
-		ResultSet resulSet=null;
+		ResultSet resulSet = null;
 		try {
-			PreparedStatement preparedStatement=getConexion().prepareStatement(BaseDatos.QUERY_IP);
-			preparedStatement.setString(1,USUARIO);
-			resulSet=preparedStatement.executeQuery();
+			PreparedStatement preparedStatement = getConexion().prepareStatement(BaseDatos.QUERY_IP);
+			preparedStatement.setString(1, USUARIO);
+			resulSet = preparedStatement.executeQuery();
 			resulSet.next();
-			
-		
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		return usuario;
-		
-	
+
 	}
 
 }
