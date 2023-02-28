3897f72d6a48442d8a48cb975c1372b02850b960
DevSniffer <116722849+DevSniffer@users.noreply.github.com>
2023-02-28 08:51:11 +0100

    a

--- src/chat/basedatos/BaseDatos.java
+++ src/chat/basedatos/BaseDatos.java
@@ -2,6 +2,7 @@ package chat.basedatos;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -13,10 +14,17 @@ public class BaseDatos {
 	public static final String SERVER_USER = "curso";
 	public static final String SERVER_PASS = "Cursocurso1;";
 	
+
+	public static final String QUERY_TOTALUSERS="SELECT CHAT.USUARIO FROM CHAT.USUARIOS";
+	public static final String QUERY_INSERT="INSERT INTO CHAT.USUARIO (USUARIO,IP) VALUES (?,?)";
+	public static final String QUERY_DELETE="DELETE FROM CHAT.USUARIOS WHERE IP=?";
+	//public static final String QUERY_IP="SELECT IP FROM CHAT.USUSARIOS WHERE USUARIO=?";
+	
+	
 	public BaseDatos() {
 	}
 	
-	public Connection getConexion() throws SQLException {
+	public Connection establishConnection() throws SQLException {
 		Connection conexion= null;
 		try {
 			DriverManager.registerDriver(new Driver());
@@ -28,17 +36,42 @@ public class BaseDatos {
 		
 	}
 	
-	public void desconexion(Connection conexion) throws SQLException {
+	public void disconnect(Connection conexion) throws SQLException {
 		conexion.close();
 	}
 	
 	public ResultSet dameUsuarios() {
-		return null;
+		ResultSet resultSet = null;
+		try (PreparedStatement preparedStatement =  establishConnection().prepareStatement(BaseDatos.QUERY_TOTALUSERS)){
+			
+			resultSet = preparedStatement.executeQuery();
+			while (resultSet.next()) {
+				System.out.println(resultSet.getString(1));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return resultSet;
 	}
-	public void agregarUsuario(String usuario) {
-		
+	public void agregarUsuario(String usuario, String ip) {
+		try {
+			PreparedStatement preparedStatement=  establishConnection().prepareStatement(BaseDatos.QUERY_INSERT);
+			preparedStatement.setString(1, usuario);
+			preparedStatement.setString(2, ip);
+			preparedStatement.execute();
+						
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}		
 	}
-	public void quitarUsuario(String usuario0) {
+	public void quitarUsuario(String ip) {
+		try {
+			PreparedStatement preparedStatement= establishConnection().prepareStatement(BaseDatos.QUERY_DELETE);
+			preparedStatement.setString(1,ip);
+			preparedStatement.execute();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		
 	}
 	
