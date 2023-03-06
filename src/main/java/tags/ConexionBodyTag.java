470d1a6022aea4735ca473c9da928ae45427ef0a
OscarP72 <116722711+OscarP72@users.noreply.github.com>
2023-03-06 14:21:35 +0100

    ejercicio tag

---
+++ src/main/java/tags/ConexionBodyTag.java
@@ -0,0 +1,11 @@
+package tags;
+
+import javax.servlet.jsp.tagext.BodyTagSupport;
+
+public class ConexionBodyTag extends BodyTagSupport {
+
+	public ConexionBodyTag() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
