636da656f652082abc3368ba47a2357ca3ad8873
David Lira <114002545+DavidLira23@users.noreply.github.com>
2023-03-07 10:16:28 +0100

    Subir

---
+++ src/main/java/tags/RepeticionesBodyConexionTag.java
@@ -0,0 +1,47 @@
+package tags;
+
+import java.io.IOException;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.tagext.BodyTagSupport;
+
+import lombok.Data;
+
+@Data
+public class RepeticionesBodyConexionTag extends BodyTagSupport {
+	private String texto;
+	private int cantidad;
+	private int contador;
+
+	@Override
+	public int doStartTag() throws JspException {
+		setContador(0);
+		return EVAL_BODY_BUFFERED;
+	}
+
+	@Override
+	public int doAfterBody() throws JspException {
+		if (getContador() < getCantidad()) {
+
+			setContador(getContador() + 1);
+			return EVAL_BODY_AGAIN;
+
+		} else {
+			return SKIP_BODY;
+		}
+	}
+
+	@Override
+	public int doEndTag() throws JspException {
+		try {
+			getBodyContent().writeOut(getPreviousOut());
+			return EVAL_PAGE;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return SKIP_PAGE;
+		}
+	}
+
+}
+
---
+++ src/main/webapp/contags.jsp
@@ -0,0 +1,22 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1" pageEncoding="ISO-8859-1"%>
+<%@taglib prefix="conexion" uri="/WEB-INF/tlds/conexion.tld"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+<link rel="stylesheet"
+	href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+</head>
+<body>
+	<div class="container">
+		<ol>
+			<once:repeticiones texto="pepe" cantidad="10">
+				
+					<li><once:valor tipo="true" /></li>
+				
+			</once:repeticiones>
+		</ol>
+	</div>
+</body>
+</html>
