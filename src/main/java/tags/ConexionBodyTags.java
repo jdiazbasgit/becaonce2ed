1122850a2222ec9866099626c01b27778c589af1
recoded13 <adsanji@gmail.com>
2023-03-08 08:44:48 +0100

    cambios

--- src/main/java/tags/ConexionBodyTags.java
+++ src/main/java/tags/ConexionBodyTags.java
@@ -1,19 +1,43 @@
 package tags;
 
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.tagext.BodyTagSupport;
 
 public class ConexionBodyTags extends BodyTagSupport {
+	
+	private String cadena;
+	private String usuario;
+	private String clave;
+	private Connection conexion = null;
 
 	@Override
 	public int doStartTag() throws JspException {
-		// TODO Auto-generated method stub
-		return super.doStartTag();
+		
+		try {
+			DriverManager.registerDriver(new Driver());
+			setConexion(DriverManager.getConnection(cadena, usuario, clave));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+		return EVAL_BODY_BUFFERED();
 	}
 
 	@Override
 	public int doEndTag() throws JspException {
-		// TODO Auto-generated method stub
+		try {
+			getConexion().close();
+			getBodyContent().writeOut(getPreviousOut());
+		} catch (IOException |SQLException e) {
+			e.printStackTrace();
+		}
 		return super.doEndTag();
 	}
 
