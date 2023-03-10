558505b7b02450b0c72a245025c9f2c569425654
Your Name <you@example.com>
2023-03-10 13:39:59 +0100

    1

---
+++ src/main/java/Hola_Mundo.java
@@ -0,0 +1,11 @@
+
+public class Hola_Mundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 String saludo= "HolA mundo";
+		 System.out.println(saludo);
+
+	}
+
+}
