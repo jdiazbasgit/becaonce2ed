f5425f6ca4c8b41ea7d4d49177f756c2304d1a1c
CarlosC <valentinaemiliuscuad@gmail.com>
2023-03-12 13:36:10 +0100

    a

--- src/main/java/exepciones/concierto/SinSonidoException.java
+++ src/main/java/exepciones/concierto/SinSonidoException.java
@@ -3,24 +3,24 @@ package exepciones.concierto;
 public class SinSonidoException extends Exception {
 
 	public SinSonidoException(String mensaje) {
-		
-		super(mensaje);	
+
+		super(mensaje);
 	}
 
-/*public class Instrumento {
-	public String sonar(String sonido)throws SinSonidoException {
-		
-		if (sonido == "nada") {
-			throw new SinSonidoException("sin sonido");
+	public class Instrumento {
+		public String sonar(String sonido) throws SinSonidoException {
+
+			if (sonido == "nada") {
+				throw new SinSonidoException("sonido");
+			}
+
+			try {
+				Instrumento instrumento = new Instrumento();
+				instrumento.sonar("nada");
+			} catch (SinSonidoException e) {
+				System.out.println("Se produjo un error: " + e.getMessage());
+			}
+			return ("sonido");
 		}
-		
-		try {
-		    Instrumento instrumento = new Instrumento();
-		    instrumento.sonar("nada");
-		} catch (SinSonidoException e) {
-		    System.out.println("Se produjo un error: " + e.getMessage());
-		}
-		return ("sonido");
 	}
-	*/
 }
