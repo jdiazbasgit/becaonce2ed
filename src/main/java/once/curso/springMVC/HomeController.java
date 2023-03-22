31f253fbd882a3d591c4674956142ae39de476fd
recoded13 <adsanji@gmail.com>
2023-03-22 14:28:12 +0100

    MVC

--- src/main/java/once/curso/springMVC/HomeController.java
+++ src/main/java/once/curso/springMVC/HomeController.java
@@ -14,22 +14,23 @@ import org.springframework.web.bind.annotation.RequestMethod;
 /**
  * Handles requests for the application home page.
  */
-@Controller
+@Controller//metete al aramario(con amigos), eres un servle
+@g
 public class HomeController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);//logger imprime en consola com syso
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@RequestMapping(value = "/", method = RequestMethod.GET)//
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
-		String formattedDate = dateFormat.format(date);
+		String formattedDate = dateFormat.format(date);//del controlador al servidor
 		
 		model.addAttribute("serverTime", formattedDate );
 		
