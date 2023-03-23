542e39178a8948602e33586e95fc4f0ae39037c7
Your Name <you@example.com>
2023-03-23 10:18:45 +0100

    inicio

---
+++ src/main/java/once/curso/springMVC/HomeController.java
@@ -0,0 +1,102 @@
+package once.curso.springMVC;
+
+
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+import javax.annotation.Generated;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 *  En el value del RequestMapping pones el nombre de la url como la que queremos que se llame 
+	 */
+	@RequestMapping(value = "/prueba", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+	}
+	
+	@RequestMapping("/ejemplo")
+	public String texto(Model model, @RequestParam String texto) {
+		String salida= "Has escrito " + texto.toUpperCase() + " y tiene " + texto.length() + " letras ";
+		
+		model.addAttribute("salida", salida);
+		
+		return "texto";
+		
+	}
+	
+	
+	@RequestMapping("/lista")
+	public String lista (Model model) {
+		
+	
+		List<String> lista = new ArrayList<String>();
+		
+	lista.add("uno");
+	lista.add("dos");
+	lista.add("tres");
+	lista.add("cuatro");
+	lista.add("cinco");
+	lista.add("seis");
+	lista.add("siete");
+	lista.add("ocho");
+	lista.add("nueve");
+	lista.add("diez");
+	
+	model.addAttribute("lista", lista);
+	
+	return "lista";
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+}
