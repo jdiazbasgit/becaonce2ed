c75aacc53ace598793d78bd48ceafb8d9099ddd8
OscarP72 <116722711+OscarP72@users.noreply.github.com>
2023-03-24 14:05:38 +0100

    a

--- src/main/java/once/curso/springMVC/Persona.java
+++ src/main/java/once/curso/springMVC/Persona.java
@@ -5,9 +5,11 @@ import org.slf4j.LoggerFactory;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Persona {
 	
 	private String nombre, apellidos, telefono;
