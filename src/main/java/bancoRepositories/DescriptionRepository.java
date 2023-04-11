5ff8e262249d9c6c16bc9f105b3683ee512dc6fd
CarlosC <valentinaemiliuscuad@gmail.com>
2023-04-11 14:33:12 +0200

    a

--- src/main/java/bancoRepositories/DescriptionRepository.java
+++
@@ -1,9 +0,0 @@
-package bancoRepositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import bancoEntityes.Description;
-
-public interface DescriptionRepository extends CrudRepository<Description, Integer> {
-
-}
--- src/main/java/bancoRepositories/FeeRepository.java
+++
@@ -1,9 +0,0 @@
-package bancoRepositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import bancoEntityes.Fee;
-
-public interface FeeRepository extends CrudRepository<Fee,Integer> {
-
-}
