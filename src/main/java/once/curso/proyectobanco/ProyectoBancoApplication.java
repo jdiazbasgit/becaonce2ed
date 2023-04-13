08fbcaa9fd7ad571757e566960444e3ba00ef01e
jdiazbasgit <git@studiotapal.es>
2023-04-13 12:00:12 +0200

    carga incial

---
+++ src/main/java/once/curso/proyectobanco/ProyectoBancoApplication.java
@@ -0,0 +1,13 @@
+package once.curso.proyectobanco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoBancoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoBancoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/once/curso/proyectobanco/ProyectoBancoApplicationTests.java
@@ -0,0 +1,13 @@
+package once.curso.proyectobanco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoBancoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
