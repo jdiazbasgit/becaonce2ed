9e88314405efe1fdae9c7071a07fd24b222e82b2
Your Name <you@example.com>
2023-04-14 10:14:17 +0200

    primera carga

--- src/main/java/once/curso/proyectobanco/repositories/RolRepository.java
+++ src/main/java/once/curso/proyectobanco/repositories/RolRepository.java
@@ -3,11 +3,15 @@ package once.curso.proyectobanco.repositories;
 import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import once.curso.proyectobanco.entities.Rol;
 
-public interface RolCRUDRepository extends CrudRepository<Rol, Integer> {
 
+@Repository
+public interface RolRepository extends CrudRepository<Rol,Integer> {
+
+	
 	
 
 }
--- src/main/java/once/curso/proyectobanco/services/RolService.java
+++ src/main/java/once/curso/proyectobanco/services/RolService.java
@@ -4,66 +4,65 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import lombok.Data;
 import once.curso.proyectobanco.entities.Rol;
-import once.curso.proyectobanco.repositories.RolCRUDRepository;
+import once.curso.proyectobanco.repositories.RolRepository;
 
 @Data
 @Service
 public class RolService {
 	
 	@Autowired
-	private RolCRUDRepository rolCRUDRepository;
-	
-	public void damePapel() {
-		Iterable<Rol> papel =getRolCRUDRepository().findAll();
-	}
-	public <S extends Rol> S save(S entity) {
-		return getRolCRUDRepository().save(entity);
-	}
+	private RolRepository rolRepository;
 
-	public <S extends Rol> Iterable<S> saveAll(Iterable<S> entities) {
-		return getRolCRUDRepository().saveAll(entities);
-	}
 
-	public Optional<Rol> findById(Integer id) {
-		return getRolCRUDRepository().findById(id);
-	}
 
-	public boolean existsById(Integer id) {
-		return getRolCRUDRepository().existsById(id);
-	}
-
-	public Iterable<Rol> findAll() {
-		return getRolCRUDRepository().findAll();
-	}
-
-	public Iterable<Rol> findAllById(Iterable<Integer> ids) {
-		return getRolCRUDRepository().findAllById(ids);
-	}
-
-	public long count() {
-		return getRolCRUDRepository().count();
-	}
-
-	public void deleteById(Integer id) {
-		getRolCRUDRepository().deleteById(id);
-	}
-
-	public void delete(Rol entity) {
-		getRolCRUDRepository().delete(entity);
-	}
-
-	public void deleteAllById(Iterable<? extends Integer> ids) {
-		getRolCRUDRepository().deleteAllById(ids);
-	}
-
-	public void deleteAll(Iterable<? extends Rol> entities) {
-		getRolCRUDRepository().deleteAll(entities);
-	}
-
-	public void deleteAll() {
-		getRolCRUDRepository().deleteAll();
-	}
+public <S extends Rol> S save(S entity) {
+	return getRolRepository().save(entity);
 }
+
+public <S extends Rol> Iterable<S> saveAll(Iterable<S> entities) {
+	return getRolRepository().saveAll(entities);
+}
+
+public Optional<Rol> findById(Integer id) {
+	return getRolRepository().findById(id);
+}
+
+public boolean existsById(Integer id) {
+	return getRolRepository().existsById(id);
+}
+
+public Iterable<Rol> findAll() {
+	return getRolRepository().findAll();
+}
+
+public Iterable<Rol> findAllById(Iterable<Integer> ids) {
+	return getRolRepository().findAllById(ids);
+}
+
+public long count() {
+	return getRolRepository().count();
+}
+
+public void deleteById(Integer id) {
+	getRolRepository().deleteById(id);
+}
+
+public void delete(Rol entity) {
+	getRolRepository().delete(entity);
+}
+
+public void deleteAllById(Iterable<? extends Integer> ids) {
+	getRolRepository().deleteAllById(ids);
+}
+
+public void deleteAll(Iterable<? extends Rol> entities) {
+	getRolRepository().deleteAll(entities);
+}
+
+public void deleteAll() {
+	getRolRepository().deleteAll();
+}
+}
+
