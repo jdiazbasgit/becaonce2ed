72252e4b83b6ea264ec9b5472b0923476edd4bc9
DeafFaker <edupalomino.to@gmail.com>
2023-04-17 14:09:08 +0200

    subir

---
+++ src/test/java/once/curso/proyectobanco/CurrentAccountTest.java
@@ -0,0 +1,31 @@
+package once.curso.proyectobanco;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import lombok.Data;
+import once.curso.proyectobanco.entities.CurrentAccount;
+import once.curso.proyectobanco.services.CurrentAccountService;
+
+@SpringBootTest
+@Data
+public class CurrentAccountTest {
+
+	@Autowired
+	private CurrentAccountService currentAccountService;
+	
+	@Test
+	public void findAll() {
+		List<CurrentAccount> currentAccount=(List<CurrentAccount>)getCurrentAccountService().findAll();
+		assertNotEquals(currentAccount.size(),0);
+	}
+	
+	@Test
+	public void 
+	
+}
