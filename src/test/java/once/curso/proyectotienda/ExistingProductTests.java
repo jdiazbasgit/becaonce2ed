035ad0a85e8da9b8552ec172ae7f6ffd4de20309
David Lira <114002545+DavidLira23@users.noreply.github.com>
2023-04-18 09:59:14 +0200

    a

--- src/test/java/once/curso/proyectotienda/ExistingProductTests.java
+++ src/test/java/once/curso/proyectotienda/ExistingProductTests.java
@@ -4,7 +4,9 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,15 +26,13 @@ public class ExistingProductTests {
 	    @Test
 	    public void testGetAllExistingProduct() throws Exception {
 	        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(existingProductRestController).build();
-	        mockMvc.perform(get("/products"))
+	        mockMvc.perform(get("/products/"))
 	            .andExpect(status().isOk())
-	            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+	            .andExpect(view().name("products"));
+	        	//.andExpect(model().attribute("products",  hasSize(2)));
+	            /*.andExpect(content().contentType(MediaType.APPLICATION_JSON))
 	            .andExpect((ResultMatcher) jsonPath("$", hasSize(2)))
 	            .andExpect(jsonPath("$[0].name", is("Product 1")))
-	            .andExpect(jsonPath("$[1].name", is("Product 2")));
+	            .andExpect(jsonPath("$[1].name", is("Product 2")));*/
 	    }
-
-		private Object hasSize(int i) {
-			return null;
-		}
 }
