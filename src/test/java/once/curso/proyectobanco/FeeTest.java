f3e4bb29f22bd04525fed2ab3e55f7b8059ee5c6
DevSniffer <116722849+DevSniffer@users.noreply.github.com>
2023-04-18 14:20:44 +0200

    a

--- src/test/java/once/curso/proyectobanco/FeeTest.java
+++ src/test/java/once/curso/proyectobanco/FeeTest.java
@@ -3,6 +3,7 @@ package once.curso.proyectobanco;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 
@@ -12,7 +13,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import lombok.Data;
 import once.curso.proyectobanco.entities.Fee;
+import once.curso.proyectobanco.entities.Profile;
 import once.curso.proyectobanco.services.FeeService;
+import once.curso.proyectobanco.services.ProfileService;
 
 @SpringBootTest
 @Data
@@ -21,14 +24,48 @@ public class FeeTest {
 	@Autowired
 	private FeeService feeService;
 	
+	
+	//Probando a cómo sacar la entity
+	private Fee fee = new Fee();
+	
 	@Test
-	public void findAll() {
-		List<Fee> fees= (List<Fee>) getFeeService().findAll();
-		assertNotEquals(fees.size(), 0);
+	public void saveTest() {
+		//Fee fee= new Fee();
+		fee.setCurrent(Math.random());
+		getFeeService().save(fee);
+		assertTrue(getFeeService().findById(fee.getId()).isPresent());
+		
 	}
 	
-	
 	@Test
+	public void deleteByIdTest() {
+		Fee feeRecuperado= getFeeService().findById(fee.getId()).get();
+		getFeeService().delete(feeRecuperado);
+		assertFalse(getFeeService().findById(feeRecuperado.getId()).isPresent());
+		
+	}
+	
+	//@Test
+	public void findAll() {
+		Fee fee= new Fee();
+		fee.setCurrent(Math.random());
+		getFeeService().save(fee);
+		
+		Fee fee2= new Fee();
+		fee.setCurrent(Math.random());
+		getFeeService().save(fee2);
+		
+		List<Fee> fees= (List<Fee>) getFeeService().findAll();
+		assertNotEquals(fees.size(), 0);
+		
+		Fee feeRecuperado= getFeeService().findById(fee.getId()).get();
+		getFeeService().delete(feeRecuperado);
+		Fee feeRecuperado2= getFeeService().findById(fee2.getId()).get();
+		getFeeService().delete(feeRecuperado2);
+		
+	}
+	
+	//@Test
 	public void probarSaveFindDelete() {
 		
 		Fee fee= new Fee();
