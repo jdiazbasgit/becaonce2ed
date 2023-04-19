ca1c1e2d6dec3f8ee3017964b9b29ed83236301b
Your Name <you@example.com>
2023-04-19 12:08:50 +0200

    mr

--- src/main/java/once/curso/proyectobanco/restcontrollers/AwardFineRestControllers.java
+++ src/main/java/once/curso/proyectobanco/restcontrollers/AwardFineRestControllers.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,7 +41,7 @@ public class AwardFineRestControllers {
 
 	}
 
-	@PostMapping(value = "/awardFine")
+	@PutMapping(value = "/awardFine")
 	public List<AwardsFine> saveAll(@RequestBody List<AwardsFine> awardFine) {
 		return (List<AwardsFine>) getAwardFineServices().saveAll(awardFine);
 	}
--- src/main/java/once/curso/proyectobanco/restcontrollers/AwardsFinesConfigurationRestControllers.java
+++ src/main/java/once/curso/proyectobanco/restcontrollers/AwardsFinesConfigurationRestControllers.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,7 +39,7 @@ public class AwardsFinesConfigurationRestControllers {
 		return getAwardsFinesConfigurationServices().save(AwardsFinesConfiguration);
 	}
 	
-	@PostMapping(value = "/AwardsFinesConfiguration")
+	@PutMapping(value = "/AwardsFinesConfiguration")
 	public List<AwardsFinesConfiguration> saveAll(@RequestBody List<AwardsFinesConfiguration> AwardsFinesConfigurations){
 		return (List<AwardsFinesConfiguration>) getAwardsFinesConfigurationServices().findAll();
 	}
