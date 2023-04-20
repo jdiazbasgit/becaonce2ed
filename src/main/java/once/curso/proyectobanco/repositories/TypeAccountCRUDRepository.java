5ed4a67936bbad9c2a771a8eee007b8c11e39ca1
OscarP72 <116722711+OscarP72@users.noreply.github.com>
2023-04-20 12:27:22 +0200

    test por orden

--- src/main/java/once/curso/proyectobanco/repositories/TypeAccountCRUDRepository.java
+++ src/main/java/once/curso/proyectobanco/repositories/TypeAccountCRUDRepository.java
@@ -1,9 +1,11 @@
 package once.curso.proyectobanco.repositories;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import once.curso.proyectobanco.entities.TypeAccount;
 
 public interface TypeAccountCRUDRepository extends CrudRepository<TypeAccount, Integer> {
-
+//	@Query("from TypeAccount as ta where ta.description= :dato")
+	public TypeAccount findByDescriptionEquals(String dato);
 }
--- src/test/java/once/curso/proyectobanco/TypeAccountTest.java
+++ src/test/java/once/curso/proyectobanco/TypeAccountTest.java
@@ -1,7 +1,17 @@
 package once.curso.proyectobanco;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -11,15 +21,37 @@ import once.curso.proyectobanco.services.TypeAccountService;
 
 @SpringBootTest
 @Data
+@TestMethodOrder(OrderAnnotation.class)
 public class TypeAccountTest {
-	
+
 	@Autowired
 	private TypeAccountService typeAccountService;
-	
+
 	@Test
+	@Order(1)
 	public void testTypeAccount() {
-		Iterable<TypeAccount> typesAccounts=typeAccountService.dameTipoDeCuenta();
-		assertNotNull(typesAccounts);
+		List<TypeAccount> typesAccounts = (List<TypeAccount>) getTypeAccountService().findAll();
+		assertNotEquals(typesAccounts.size(), 0);
+	}
+
+	@Test
+	@Order(2)
+	public void testSaveTypeAccount() {
+		TypeAccount typeAccount = new TypeAccount();
+		typeAccount.setDescription("prueba de oscar");
+		getTypeAccountService().save(typeAccount);
+	//	assertNotEquals(typeAccount.getId(), 0);
+		assertTrue(getTypeAccountService().existsById(typeAccount.getId()));
+	}
+
+	@Test
+	@Order(3)
+	public void testDeleteTypeAccount() {
+		Optional<TypeAccount> typeAccount = getTypeAccountService().findByDescriptionEquals("prueba de oscar");
+		int id = typeAccount.get().getId();
+		if (typeAccount.isPresent())
+			getTypeAccountService().delete(typeAccount.get());
+		assertFalse(getTypeAccountService().existsById(id));
 	}
 
 }
