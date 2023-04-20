fdcedbc32f8fa5d773511f7f75a830ca42f5b8de
OscarP72 <116722711+OscarP72@users.noreply.github.com>
2023-04-20 14:02:33 +0200

    test saveAll

--- src/test/java/once/curso/proyectobanco/TypeAccountTest.java
+++ src/test/java/once/curso/proyectobanco/TypeAccountTest.java
@@ -1,10 +1,12 @@
 package once.curso.proyectobanco;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -40,7 +42,7 @@ public class TypeAccountTest {
 		TypeAccount typeAccount = new TypeAccount();
 		typeAccount.setDescription("prueba de oscar");
 		getTypeAccountService().save(typeAccount);
-	//	assertNotEquals(typeAccount.getId(), 0);
+		// assertNotEquals(typeAccount.getId(), 0);
 		assertTrue(getTypeAccountService().existsById(typeAccount.getId()));
 	}
 
@@ -52,6 +54,22 @@ public class TypeAccountTest {
 		if (typeAccount.isPresent())
 			getTypeAccountService().delete(typeAccount.get());
 		assertFalse(getTypeAccountService().existsById(id));
+	}
+
+	@Test
+	@Order(4)
+	public void testSaveAllTypeAccount() {
+		long cantidadAlEmpezar = getTypeAccountService().count();
+		List<TypeAccount> typesAccounts = new ArrayList<TypeAccount>();
+		TypeAccount typeAccount2 = new TypeAccount();
+		typeAccount2.setDescription("prueba de oscar2");
+		TypeAccount typeAccount3 = new TypeAccount();
+		typeAccount3.setDescription("prueba de oscar3");
+		typesAccounts.add(typeAccount2);
+		typesAccounts.add(typeAccount3);
+		getTypeAccountService().saveAll(typesAccounts);
+		assertNotEquals(getTypeAccountService().count(), cantidadAlEmpezar);
+
 	}
 
 }
