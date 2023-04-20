932b3a9a9a6257d0f0b77cc5cd2b672e1f81650f
Your Name <you@example.com>
2023-04-20 14:23:57 +0200

    Todos Los test de CardTypes practicamente hechos

--- src/main/java/once/curso/proyectotienda/services/CardTypeService.java
+++ src/main/java/once/curso/proyectotienda/services/CardTypeService.java
@@ -20,53 +20,57 @@ public class CardTypeService {
 	}
 
 	public CardType save(CardType entity) {
-
+//HECHO
 		return getCardTypeCRUDRepository().save(entity);
 	}
 
 	public Iterable<CardType> saveAll(Iterable<CardType> entities) {
-
+//HECHO
 		return getCardTypeCRUDRepository().saveAll(entities);
 	}
 
 	public Optional<CardType> findById(Integer id) {
-
+//HECHO
 		return getCardTypeCRUDRepository().findById(id);
 	}
 
 	public boolean existsById(Integer id) {
-
+//HECHO
 		return getCardTypeCRUDRepository().existsById(id);
 	}
 
 	public Iterable<CardType> findAll() {
-
+//HECHO
 		return getCardTypeCRUDRepository().findAll();
 	}
 
 	public Iterable<CardType> findAllById(Iterable<Integer> ids) {
-
+//HECHO
 		return getCardTypeCRUDRepository().findAllById(ids);
 	}
 
 	public long count() {
-
+//HECHO
 		return getCardTypeCRUDRepository().count();
 	}
 
 	public void deleteById(Integer id) {
+		//HECHO
 		getCardTypeCRUDRepository().deleteById(id);
 	}
 
 	public void delete(CardType entity) {
+		//HECHO
 		getCardTypeCRUDRepository().delete(entity);
 	}
 
 	public void deleteAllById(Iterable<? extends Integer> ids) {
+		//HECHO
 		getCardTypeCRUDRepository().deleteAllById(ids);
 	}
 
 	public void deleteAll(Iterable<? extends CardType> entities) {
+		//HECHO
 		getCardTypeCRUDRepository().deleteAll(entities);
 	}
 
