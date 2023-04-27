3cd1a577b3b08cc47831c7a3506ff3010a78d823
Your Name <you@example.com>
2023-04-27 14:21:16 +0200

    ab

--- src/app/generales/generales.component.ts
+++ src/app/generales/generales.component.ts
@@ -1,20 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { PaisesService } from '../paises.service';
 
 @Component({
   selector: 'app-generales',
   templateUrl: './generales.component.html',
   styleUrls: ['./generales.component.css']
 })
-export class GeneralesComponent {
-  paisEnRuta:string=""
+export class GeneralesComponent implements OnInit {
+  paisEnRuta:string="";
+  datos: any;
+  monedas:Array<any> = [];
 
-  constructor(private rutaActiva:ActivatedRoute){
+  constructor(private rutaActiva: ActivatedRoute, private service: PaisesService) {
+    this.paisEnRuta = this.rutaActiva.snapshot.params['pais'];
     
   }
    
     ngOnInit(): void {
       this.paisEnRuta=this.rutaActiva.snapshot.params['pais'];
+
+      this.service.dameDatos ("https://restcountries.com/v3.1/name/" + this.paisEnRuta)
+      .subscribe((datos: any) => {
+        this.datos = datos [0];
+        Object.keys (this.datos.currencies).forEach (moneda=>this.monedas.push (this.datos.currencies[moneda]))
+      });
     }
   
 }
