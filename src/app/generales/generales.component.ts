cc5e743243d21cb35ba23414d76e9e8b3b428e23
CarlosC <valentinaemiliuscuad@gmail.com>
2023-04-27 23:17:49 +0200

    subir

--- src/app/generales/generales.component.ts
+++ src/app/generales/generales.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PaisesService } from '../paises.service';
 
@@ -7,36 +7,32 @@ import { PaisesService } from '../paises.service';
   templateUrl: './generales.component.html',
   styleUrls: ['./generales.component.css']
 })
-export class GeneralesComponent {
-  paisEnRuta:string=""
-  capital: boolean = true
-  poblacion: string = ""
-  area: string = ""
-  dominio: string = ""
-  moneda: boolean = true
-  datos:any
+export class GeneralesComponent implements OnInit {
+  paisEnRuta: string = ""
 
-  constructor(private rutaActiva:ActivatedRoute, private service: PaisesService){
+  monedas: Array<any> = []
+  datos: any
+
+
+  constructor(private rutaActiva: ActivatedRoute, private service: PaisesService) {
     this.paisEnRuta = this.rutaActiva.snapshot.params['pais']
   }
-   
-    ngOnInit(): void {
-      this.paisEnRuta=this.rutaActiva.snapshot.params['pais'];
 
-      /*this.paisEnRuta = this.rutaActiva.snapshot.params['pais'];
-      console.log("pais:" + this.paisEnRuta)*/
-      this.service.dameDatos("https://restcountries.com/v3.1/name/" + this.paisEnRuta)
+  ngOnInit(): void {
+    this.paisEnRuta = this.rutaActiva.snapshot.params['pais'];
+
+    this.paisEnRuta = this.rutaActiva.snapshot.params['pais'];
+    console.log("pais:" + this.paisEnRuta)
+    this.service.dameDatos("https://restcountries.com/v3.1/name/" + this.paisEnRuta)
 
       .subscribe((datos: any) => {
-
         console.log(datos)
-        this.datos = datos [0];
-        this.capital = datos.boolean;
-        this.poblacion = datos.borders;
-        this.area = datos.borders;
-        this.dominio = datos.borders;
-        this.moneda = datos.boolean;
-      })
-    }
-  
+        this.datos = datos[0];
+
+        Object.keys(this.datos.currencies).forEach(moneda => this.monedas.push(this.datos.currencies[moneda]))
+
+      });
+  }
 }
+
+
