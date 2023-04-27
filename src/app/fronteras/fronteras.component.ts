d99c4c5c4b3be3c655891b11559285f82c99d69f
Your Name <you@example.com>
2023-04-27 09:22:34 +0200

    a

--- src/app/fronteras/fronteras.component.ts
+++ src/app/fronteras/fronteras.component.ts
@@ -2,6 +2,7 @@
 import { DoCheck, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { PaisesService } from '../paises.service';
 
 @Component({
   selector: 'app-fronteras',
@@ -12,16 +13,19 @@ export class FronterasComponent implements DoCheck {
 
   //@Input() pais:string | undefined="";
   paisEnRuta:string=""
+  datos:any;
+  
 
-constructor(private rutaActiva:ActivatedRoute){
+constructor(private rutaActiva:ActivatedRoute,  service:PaisesService){
   
 }
   ngDoCheck(): void {
     this.paisEnRuta=this.rutaActiva.snapshot.params['pais'];
-  }
-  
-  
+    this.service.dameDatos("https://restcountries"+this.paisEnRuta)
+    .suscribe((this.datos:any)=>{
+      this.datos=datos[0];
+    })
 
-  
+  }
 
 }
