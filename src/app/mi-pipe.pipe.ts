b5e99e46d0d8977ca968554a73096bfd9bcaf852
Your Name <you@example.com>
2023-04-28 14:20:26 +0200

    h

--- src/app/mi-pipe.pipe.ts
+++ src/app/mi-pipe.pipe.ts
@@ -18,6 +18,8 @@ export class MiPipePipe implements PipeTransform {
           case 5: return 'five';
           case 6: return 'six';
           case 7: return 'seven'; 
+          
+          default:return'el numero no tiene traduccion';
         }
       }
       if(args[0]=='portugues'){
@@ -28,7 +30,8 @@ export class MiPipePipe implements PipeTransform {
           case 4: return 'quatro';
           case 5: return 'cinco';
           case 6: return 'seis';
-          case 7: return 'sete';                        
+          case 7: return 'sete'; 
+          default:return'el numero no tiene traduccion';                       
         }        
       }
       if(args[0]=='spain'){
@@ -39,18 +42,22 @@ export class MiPipePipe implements PipeTransform {
           case 4: return 'cuatro';
           case 5: return 'cinco';
           case 6: return 'seis';
-          case 7: return 'siete';                        
+          case 7: return 'siete';
+          default:return'el numero no tiene traduccion';                        
         }  
       }
+      if(args[0]=='ruso'){
       switch (value) {
-        case 1: return 'uno';
-        case 2: return 'dos';
-        case 3: return 'tres';
-        case 4: return 'cuatro';
-        case 5: return 'cinco';
-        case 6: return 'seis';
-        case 7: return 'siete';                        
-      }  
+        case 1: return 'один (odin)';
+        case 2: return 'два (dva)';
+        case 3: return 'три (tri)';
+        case 4: return 'четыре (chetyre)';
+        case 5: return 'пять (pyat)';
+        case 6: return 'шесть (shest)';
+        case 7: return 'семь (sem)'; 
+        default:return'el numero no tiene traduccion';                       
+      } 
+    } 
     }
     return "No has escrito ningun idioma";
   }
--- src/app/pipe-personalizado/pipe-personalizado.component.ts
+++ src/app/pipe-personalizado/pipe-personalizado.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./pipe-personalizado.component.css']
 })
 export class PipePersonalizadoComponent {
-  numeros:Array<number>=[1,2,3,4,5,6,7]
+  numeros:Array<number>=[1,2,3,4,5,6,7,8,9]
 }
