451f14b874340b4d35c62d3958d13c5bccdd37c3
OscarP72 <116722711+OscarP72@users.noreply.github.com>
2023-05-03 14:06:07 +0200

    s

--- src/app/nombre-frontera.pipe.ts
+++ src/app/nombre-frontera.pipe.ts
@@ -6,8 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class NombreFronteraPipe implements PipeTransform {
 
   transform(value: string, ...args: number[]): string {
-    let salida:string=""
-    for (let i=0 ; i<args[0]; i++)
+    
     return "hola";
   }
 
