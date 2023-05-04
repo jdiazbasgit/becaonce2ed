f42a4690ebfb084ec5c2ca21709171a2e2bd9224
DevSniffer <116722849+DevSniffer@users.noreply.github.com>
2023-05-04 09:14:29 +0200

    a

--- src/app/pipe-acronimos.pipe.ts
+++ src/app/pipe-acronimos.pipe.ts
@@ -8,21 +8,28 @@ export class PipeAcronimosPipe implements PipeTransform {
 
   constructor(private service: PaisesService) { }
 
-  async transform(value: string, ...args: string[]): Promise<string> {
+  transform(value: string, ...args: string[]): string {
     console.log(value + " acronimo que llega al pipe")
     if (value.length == 3) {
-      await this.comprobar(value)
-    }
-    return value;
-  }
-
-  async comprobar(value:string){
-    await this.service.dameDatosPorHTTPService("https://restcountries.com/v3.1/alpha/" + value)
+      this.service.dameDatosPorHTTPService("https://restcountries.com/v3.1/alpha/" + value)
         .subscribe((datosPaisAcronimo: any) => {
           value = datosPaisAcronimo[0].name.common
           console.log(value + " nombre del pais que sale del pipe tras el fetch")
         }
         )
+      //this.comprobar(value)
+    }
+    return value;
   }
+
+  // async comprobar(value:string){
+  //   await this.service.dameDatosPorHTTPService("https://restcountries.com/v3.1/alpha/" + value)
+  //       .subscribe((datosPaisAcronimo: any) => {
+  //         value = datosPaisAcronimo[0].name.common
+  //         console.log(value + " nombre del pais que sale del pipe tras el fetch")
+  //         return value
+  //       }
+  //     )
+  // }
 
 }
