46b18eec3da6137f2a2c51325c312497a07500d8
Your Name <you@example.com>
2023-05-08 10:25:32 +0200

    jajaja

--- src/app/configuration.service.ts
+++ src/app/configuration.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConfigurationService {
 
-  constructor() { }
+  constructor(private service:HttpClient) { }
 }
+
+
