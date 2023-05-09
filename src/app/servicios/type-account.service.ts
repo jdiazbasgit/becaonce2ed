ca151c334e6e7268b1f7a1d702c6e66aaffb3c72
Your Name <you@example.com>
2023-05-09 11:33:44 +0200

    a

--- src/app/servicios/type-account.service.ts
+++ src/app/servicios/type-account.service.ts
@@ -1,10 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ProyectosService } from './proyectos.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class TypeAccountService {
+export class TypeAccountService extends ProyectosService {
 
-  constructor(private service:HttpClient) { }
+  constructor(private http: HttpClient) {
+    super(http);
+   }
 }
