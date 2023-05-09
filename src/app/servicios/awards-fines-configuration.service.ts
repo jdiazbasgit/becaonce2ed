0a9f305ca9cd5b9614700ad5425f302536812d24
Your Name <you@example.com>
2023-05-09 11:25:47 +0200

    a

--- src/app/servicios/awards-fines-configuration.service.ts
+++ src/app/servicios/awards-fines-configuration.service.ts
@@ -1,10 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ProyectosService } from './proyectos.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AwardsFinesConfigurationService {
+export class AwardsFinesConfigurationService extends ProyectosService {
 
-  constructor(private service: HttpClient) { }
+  constructor(private http: HttpClient) { 
+    super(http);
+   }
 }
