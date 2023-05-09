da7ea4e2e6961712a5dd6c343f0acec5307023c8
Your Name <you@example.com>
2023-05-09 11:33:23 +0200

    serviceHecho

--- src/app/card-type.service.ts
+++ src/app/card-type.service.ts
@@ -1,10 +1,23 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ProyectosService } from './servicios/proyectos.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class CardTypeService {
+export class CardTypeService extends ProyectosService{
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private http:HttpClient) {
+    super(http);
+    this.http.delete;
+    this.http.get;
+    this.http.head;
+    this.http.jsonp;
+    this.http.options;
+    this.http.patch;
+    this.http.post;
+    this.http.put;
+    this.http.request;
+    
+  }
 }
