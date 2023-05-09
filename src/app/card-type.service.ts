a526659a1e52f836286432d0fd3fa63648ae22f7
Your Name <you@example.com>
2023-05-09 11:35:16 +0200

    service

--- src/app/card-type.service.ts
+++ src/app/card-type.service.ts
@@ -9,15 +9,8 @@ export class CardTypeService extends ProyectosService{
 
   constructor(private http:HttpClient) {
     super(http);
-    this.http.delete;
-    this.http.get;
-    this.http.head;
-    this.http.jsonp;
-    this.http.options;
-    this.http.patch;
-    this.http.post;
-    this.http.put;
-    this.http.request;
+    
+  
     
   }
 }
