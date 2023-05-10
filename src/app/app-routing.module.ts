626b443cac503d3193ce13ac80cb5025f9d1ce1f
David Lira <114002545+DavidLira23@users.noreply.github.com>
2023-05-10 14:12:47 +0200

    Act.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)]
 })
-export class AppRoutingModule { }
+
+export class AppRoutingModule {
+
+}
