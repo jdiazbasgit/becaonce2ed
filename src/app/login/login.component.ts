33fc7c9505225c134f52c76276b01f6c043b3edc
DevSniffer <you@example.com>
2023-05-16 03:21:49 +0200

    login en pruebas css

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
+import { ProyectosService } from '../servicios/proyectos.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,51 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  logado:boolean=false
+  inputUsuario:string=""
+  inputPassword:string=""
+  usuario:string=""
 
+  constructor(private http:ProyectosService, private elementRef:ElementRef){
+  }
+
+  logarse(){    
+    this.http.getDatos('https://restcountries.com/v3.1/name/spain')
+    .subscribe((datos: any) => {
+      let userM = datos[0].name.common
+      let passM = datos[0].capital;
+      console.log(userM+passM)
+      if (this.inputUsuario == userM && this.inputPassword == passM){
+        this.logado=true
+        this.usuario=userM
+      }
+      else {
+        this.limpiarFormulario()      
+        let cont = this.elementRef.nativeElement.querySelector('.contenido')
+        cont.innerHTML = "Aqui pondremos mensaje error de autenticación y se restablece al cerrar el collapse"
+        cont.classList.add('bg-danger')
+      }
+      }
+    )
+    
+  }
+  deslogarse(){
+    this.logado=false
+  }
+  limpiarFormulario(){
+    this.inputUsuario=""
+    this.inputPassword=""
+  }
+  colorearBotonLoginAlPulsar() {
+    this.limpiarFormulario()
+    let btnLogin = this.elementRef.nativeElement.querySelector('.btnLogin');    
+    // console.log("hola!")
+    if (btnLogin.classList.contains('collapsed')) {
+      btnLogin.classList.remove('pulsado');
+      // console.log("hola2!")
+    }
+    else {
+      btnLogin.classList.add('pulsado');
+    }
+  }
 }
