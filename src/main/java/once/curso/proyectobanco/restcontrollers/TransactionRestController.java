44352f66a5c0e34acc9f6889f22b4d6f9cabda1f
OscarP72 <116722711+OscarP72@users.noreply.github.com>
2023-05-17 12:32:39 +0200

    hateoas que quedaba

--- src/main/java/once/curso/proyectobanco/restcontrollers/TransactionRestController.java
+++ src/main/java/once/curso/proyectobanco/restcontrollers/TransactionRestController.java
@@ -3,6 +3,9 @@ package once.curso.proyectobanco.restcontrollers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,30 +21,48 @@ import once.curso.proyectobanco.services.TransactionService;
 
 @RestController
 @Data
-@RequestMapping ("/once")
+@RequestMapping("/once")
 public class TransactionRestController {
-	
+
 	@Autowired
 	private TransactionService transactionService;
-	
+
 	@GetMapping(value="/transactions/{id}")
-	public Transaction findById(@PathVariable Integer id) {
-		return getTransactionService().findById(id).get();
+	public EntityModel<Transaction> findById(@PathVariable int id) {
+		Transaction transaction=getTransactionService().findById(id).get();
+			transaction.add(WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(TransactionRestController.class).
+					findById(transaction.getId())).withSelfRel());
+		return EntityModel.of(transaction);
 	}
+
 	@GetMapping(value = "/transactions")
-	public Iterable<Transaction> findAll(@PathVariable Integer id) {
-		return getTransactionService().findAll();
+	public CollectionModel<Transaction> findAll() {
+		Iterable<Transaction> transaction = getTransactionService().findAll();
+		transaction.forEach(t -> {
+			t.add(WebMvcLinkBuilder
+					.linkTo(WebMvcLinkBuilder.methodOn(TransactionRestController.class).findById(t.getId()))
+					.withSelfRel());
+			t.add(WebMvcLinkBuilder.
+					linkTo(WebMvcLinkBuilder.methodOn(DescriptionRestController.class).findById(t.getDescription().getId()))
+					.withRel("description"));
+			t.add(WebMvcLinkBuilder
+					.linkTo(WebMvcLinkBuilder.methodOn(CurrentAccountRestController.class).findById(t.getCurrentAccount().getId()))
+					.withRel("currentAccount"));
+		});
+		return CollectionModel.of(transaction);
 	}
 
 	@PostMapping(value = "/transactions")
 	public Transaction save(@RequestBody Transaction transaction) {
 		return getTransactionService().save(transaction);
 	}
-	@PutMapping(value="/transactions")
-	public List<Transaction> saveAll(@RequestBody List<Transaction> transaction){
-		return (List<Transaction>)getTransactionService().saveAll(transaction);
+
+	@PutMapping(value = "/transactions")
+	public List<Transaction> saveAll(@RequestBody List<Transaction> transaction) {
+		return (List<Transaction>) getTransactionService().saveAll(transaction);
 	}
-	@DeleteMapping(value="/transactions/{id}")
+
+	@DeleteMapping(value = "/transactions/{id}")
 	public void deleteById(@PathVariable Integer id) {
 		getTransactionService().deleteById(id);
 	}
