b19e5e7305f34a532cfcc2d45cb43c2bd05edf61
Your Name <you@example.com>
2023-05-25 13:53:32 +0200

    anadiendo pageables

--- src/main/java/once/curso/proyectobanco/models/AwardFineModelAssembler.java
+++ src/main/java/once/curso/proyectobanco/models/AwardFineModelAssembler.java
@@ -1,5 +1,35 @@
 package once.curso.proyectobanco.models;
 
-public class AwardFineModelAssembler {
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.SimpleRepresentationModelAssembler;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.stereotype.Component;
+
+import once.curso.proyectobanco.entities.AwardsFine;
+import once.curso.proyectobanco.restcontrollers.AwardFineRestControllers;
+
+@Component
+public class AwardFineModelAssembler implements SimpleRepresentationModelAssembler<AwardsFine>{
+
+	@Override
+	public void addLinks(EntityModel<AwardsFine> awardsFine) {
+		awardsFine.add(WebMvcLinkBuilder
+				.linkTo(WebMvcLinkBuilder.methodOn(AwardFineRestControllers.class)
+						.findById(awardsFine.getContent().getId()))
+				.withSelfRel());
+		
+	}
+
+	
+	public void addLinks(CollectionModel<EntityModel<AwardsFine>> awardFines) {
+		awardFines.forEach(c -> {
+			c.add(WebMvcLinkBuilder
+					.linkTo(WebMvcLinkBuilder.methodOn(AwardFineRestControllers.class)
+							.findById(c.getContent().getId()))
+					.withSelfRel());
+		});
+		
+	}
 
 }
