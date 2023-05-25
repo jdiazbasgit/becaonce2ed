cade1f186b7355cdd57b830e45a1d747c4c5bfd9
Elruedas001 <116722622+Elruedas001@users.noreply.github.com>
2023-05-25 14:05:15 +0200

    paginados

--- src/main/java/once/curso/proyectobanco/repositories/DescriptionCRUDRepository.java
+++ src/main/java/once/curso/proyectobanco/repositories/DescriptionCRUDRepository.java
@@ -1,12 +1,10 @@
 package once.curso.proyectobanco.repositories;
 
-import java.util.Optional;
-
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import once.curso.proyectobanco.entities.Description;
 
-public interface DescriptionCRUDRepository extends CrudRepository<Description, Integer> {
+public interface DescriptionCRUDRepository extends PagingAndSortingRepository <Description, Integer> {
 
 	
 
--- src/main/java/once/curso/proyectobanco/restcontrollers/DescriptionRestController.java
+++ src/main/java/once/curso/proyectobanco/restcontrollers/DescriptionRestController.java
@@ -1,9 +1,17 @@
 package once.curso.proyectobanco.restcontrollers;
 
+import java.util.StringTokenizer;
+
 import org.hibernate.EntityMode;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PagedResourcesAssembler;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,11 +19,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.Data;
 import once.curso.proyectobanco.entities.Configuration;
 import once.curso.proyectobanco.entities.Description;
+import once.curso.proyectobanco.models.DescriptionModelAssembler;
 import once.curso.proyectobanco.services.ConfigurationService;
 import once.curso.proyectobanco.services.DescriptionService;
 
@@ -26,6 +36,12 @@ public class DescriptionRestController {
 	@Autowired
 	private DescriptionService descriptionService;
 	
+	@Autowired
+	private PagedResourcesAssembler<Description> PagedResourcesAssembler;
+	
+	@Autowired 
+	private DescriptionModelAssembler descriptionModelAssembler;
+	
 	@GetMapping(value =" /descriptions/{id}")
 	public EntityModel<Description> findById(@PathVariable int id) {
 		Description description= getDescriptionService().findById(id).get();
@@ -34,6 +50,10 @@ public class DescriptionRestController {
 		 return EntityModel.of(description);
 	}
 	
+	@PostMapping(value = "/descriptions")
+	public Description save(@RequestBody Description description) {
+		return getDescriptionService().save(description);
+	}
 	
 	
 	@GetMapping(value = "/descriptions")
@@ -45,10 +65,27 @@ public class DescriptionRestController {
 		});
 		 return CollectionModel.of(descriptions);
 		 }
-	@PostMapping(value = "/descriptions")
-	public Description save(@RequestBody Description description) {
-		return getDescriptionService().save(description);
+	
+	@GetMapping(value = "/descriptionsPaginado")
+	public PagedModel<EntityModel<Description>> findAllPaginado(@RequestParam int size, @RequestParam int page, @RequestParam String sort){
+		StringTokenizer stringTokenizer =new StringTokenizer(sort,",");
+		Sort orden = Sort.by("a");
+		
+		String campo=stringTokenizer.nextToken();
+		String tipoOrden=stringTokenizer.nextToken();
+		
+		if(tipoOrden.contentEquals("asc"))
+			orden=Sort.by(campo).ascending();
+		else
+		
+			orden=Sort.by(campo).descending();
+		
+		Pageable pageable = PageRequest.of(page, size,orden);
+		Page<Description> description=getDescriptionService().findAll(pageable); 
+		
+		return getPagedResourcesAssembler().toModel(description,getDescriptionModelAssembler());
 	}
+	
 	@DeleteMapping(value="/description/{id}")
 	public void deleteById(@PathVariable Integer id) {
 		getDescriptionService().deleteById(id);
