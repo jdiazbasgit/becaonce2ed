3b5810ccf4edf3d73c6c5af8390c60f3899e9ce8
Your Name <you@example.com>
2023-05-25 09:12:04 +0200

    ac

--- src/main/java/once/curso/proyectobanco/restcontrollers/AwardFineRestControllers.java
+++ src/main/java/once/curso/proyectobanco/restcontrollers/AwardFineRestControllers.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import lombok.Data;
 import once.curso.proyectobanco.entities.AwardsFine;
-import once.curso.proyectobanco.entities.AwardsFinesType;
 import once.curso.proyectobanco.services.AwardsFinesServices;
 
 @RestController
