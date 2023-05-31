e2d3e7d7fbce7ec6eed3ca698f5ceceb5783c91b
DevSniffer <116722849+DevSniffer@users.noreply.github.com>
2023-05-31 11:57:46 +0200

    LoginFuncionando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,30 @@ import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './header/header.component';
+import { NavbarComponent } from './navbar/navbar.component';
 import { LoginComponent } from './login/login.component';
+import { FooterComponent } from './footer/footer.component';
+import { LandingComponent } from './landing/landing.component';
+import { CuentasComponent } from './cuentas/cuentas.component';
+import { DetallesCuentaComponent } from './detalles-cuenta/detalles-cuenta.component';
+import { MovimientosComponent } from './movimientos/movimientos.component';
+import { OperacionesComponent } from './operaciones/operaciones.component';
+import { ProductosComponent } from './productos/productos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    HeaderComponent,
+    NavbarComponent,
+    LoginComponent,
+    FooterComponent,
+    LandingComponent,
+    CuentasComponent,
+    DetallesCuentaComponent,
+    MovimientosComponent,
+    OperacionesComponent,
+    ProductosComponent
   ],
   imports: [
     BrowserModule,
