73b1fc5f24ee6ca5626f72f072e3826ef71338c7
jdiazbasgit <git@studiotapal.es>
2023-05-31 09:43:27 +0200

    security

--- src/main/java/once/curso/proyectotienda/repositories/UserCRUDRepository.java
+++ src/main/java/once/curso/proyectotienda/repositories/UserCRUDRepository.java
@@ -1,11 +1,18 @@
-package once.curso.proyectotienda.repositories;
+package once.curso.proyectobanco.repositories;
 
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import once.curso.proyectobanco.entities.User;
 
-import once.curso.proyectotienda.entities.User;
 
 @Repository
 public interface UserCRUDRepository extends CrudRepository<User, Integer> {
 
+	@Query("from User as u where u.user=:username")
+	public User loadUserByUsername(String username);
+
+
+	
 }
