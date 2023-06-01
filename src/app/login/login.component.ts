f9dd7943e98dd06f1bd2a25b8640a358259f071d
RMTAJhimbrows <moitico428@gmail.com>
2023-06-01 10:31:56 +0200

    a

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,8 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  template: `<form class="d-flex">
+  <input class="form-control me-2" type="text" placeholder="Usuario">
+  <input class="form-control me-2" type="text" placeholder="Contraseña">
+  <button class="btn btn-secondary" type="button">Ingresar</button>
+  &nbsp;
+  <button class="btn btn-secondary" type="button">Registrar</button>
+</form>`
 })
 export class LoginComponent {
 
