198b2b361ab9e5a6ae5624edddb962d8e0cc589b
Your Name <you@example.com>
2023-06-05 13:52:03 +0200

    arreglando imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NavComponent } from './nav/nav.component';
+import { NavComponent } from './componentes/nav/nav.component';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 
-import { HeaderComponent } from './componentes/header/header.component';
-import { LoginComponent } from './login/login.component';
 
-import { FooterComponent } from './componentes/footer/footer.component';
+import { LoginComponent } from './componentes/login/login.component';
+
+
 
 import { ProfileComponent } from './componentes/profile/profile.component';
-import { ProductComponent } from './product/product.component';
-import { OfferComponent } from './offer/offer.component';
+import { ProductComponent } from './componentes/product/product.component';
+import { OfferComponent } from './componentes/offer/offer.component';
 
 import { HeaderComponent } from './componentes/header/header.component';
 import { FooterComponent } from './componentes/footer/footer.component';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatTableModule } from '@angular/material/table';
-import { ProductDataComponent } from './product-data/product-data.component';
+
 import { MatPaginatorModule } from '@angular/material/paginator';
 
 import { ProductDataComponent } from './product-data/product-data.component';
-import { ProductEditComponent } from './product-edit/product-edit.component';
+
 import { CategoryComponent } from './componentes/category/category.component';
 import { SubcategoryComponent } from './componentes/subcategory/subcategory.component';
 import { DocumentTypeComponent } from './componentes/document-type/document-type.component';
