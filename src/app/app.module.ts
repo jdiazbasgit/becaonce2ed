15859e71cb111e3ca5c785daf44ff234b015959e
David Lira <114002545+DavidLira23@users.noreply.github.com>
2023-06-05 13:57:56 +0200

    Borrado product-data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,35 +5,26 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-
 import { HeaderComponent } from './header/header.component';
 import { LoginComponent } from './login/login.component';
-
-import { FooterComponent } from './footer/footer.component';
-
 import { ProfileComponent } from './profile/profile.component';
 import { ProductComponent } from './product/product.component';
 import { OfferComponent } from './offer/offer.component';
-
-import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatTableModule } from '@angular/material/table';
 import { ProductDataComponent } from './product-data/product-data.component';
 import { MatPaginatorModule } from '@angular/material/paginator';
 
-import { ProductDataComponent } from './product-data/product-data.component';
-import { ProductEditComponent } from './product-edit/product-edit.component';
-
 
 
 @NgModule({
   declarations: [
-    
+
     NavComponent,
     AppComponent,
     HeaderComponent,
-    LoginComponent, 
+    LoginComponent,
     ProfileComponent,
     ProductComponent,
     NavComponent,
