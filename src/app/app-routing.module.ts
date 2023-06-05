5f2f6e55de6fd9310c520281025c1954494fac4b
David Lira <yolira@msn.com>
2023-06-05 15:56:30 +0200

    Modificado perfil y producto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-import { ProductComponent } from './product/product.component';
-import { ProductDataComponent } from './product-data/product-data.component';
 import { ProfileComponent } from './componentes/profile/profile.component';
-import { OfferComponent } from './offer/offer.component';
+import { ProductComponent } from './componentes/product/product.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent },
   {path: 'product', component: ProductComponent },
-  {path: 'productdata', component: ProductDataComponent },
+  {path: 'productdata', component: ProductComponent },
   {path: 'profile', component: ProfileComponent },
   {path: 'offer', component: OfferComponent }
 ];
