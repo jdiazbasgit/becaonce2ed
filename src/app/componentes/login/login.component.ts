003d9946475d93946281e50cdcc8c3b2d8832b82
RMTAJhimbrows <moitico428@gmail.com>
2023-06-08 02:53:04 +0200

    desarrollando login

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -1,11 +1,144 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
+import { Router } from '@angular/router';
+import { catchError } from 'rxjs';
+import { DocumentTypeService } from 'src/app/servicios/document-type.service';
+import { LoginService } from 'src/app/servicios/login.service';
+import { ProyectosService } from 'src/app/servicios/proyectos.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
-  
+
 })
+
+
+
 export class LoginComponent {
+
+  logado: boolean = false;
+  inputUsuario: string = "";
+  inputPassword: string = "";
+  usuario: string = "";
+  claveErronea: boolean = false;
+  mensajeClave: String = "";
+  contadorTemporizadorDeInactividad: number = 30000;
+  temporizadorDeInactividad: any;
+  sinActividad: boolean = false;
+
+  constructor(private http: ProyectosService, private elementRef: ElementRef, 
+    private loginService: LoginService, private documentType: DocumentTypeService, private router: Router) {
+
+  }
+
+  ngDoCheck() {
+    if (sessionStorage['token'] != null && !this.logado) {
+      this.logado = true;
+      this.usuario = sessionStorage['user'];
+    }
+    if (!this.logado && this.router.url !== "/landing") {
+      this.router.navigateByUrl("landing");
+    }
+    if (!this.sinActividad && this.logado) {
+      clearTimeout(this.temporizadorDeInactividad);
+      this.temporizadorDeInactividad = setTimeout(() => {
+        console.log("deslogado por inactividad!!");
+        this.sinActividad = true;
+        this.deslogarse();
+      }, this.contadorTemporizadorDeInactividad);
+    }
+  }
+  logarse() {
+    let body = document.body;
+    body.setAttribute("style", "overflow: hidden");
+    let modalConectando = this.elementRef.nativeElement.querySelector('#modalConectando');
+    modalConectando.classList.remove('oculto');
+    setTimeout(() => {
+      this.loginService.identificar("http://localhost:8080/login", this.inputUsuario, this.inputPassword)
+        .pipe(
+          catchError(error => {
+            console.log(error)
+            if (error.status === 0 || error.status === 404) {
+              this.mensajeClaveErronea("No ha sido posible establecer la conexión. Intentelo más tarde")
+            }
+            modalConectando.classList.add('oculto')
+            body.removeAttribute("style")
+            return ""
+          })
+        )
+        .subscribe((datos: any) => {
+          console.log(datos)
+          if (datos.token == null) {
+            this.limpiarFormulario()
+            this.elementRef.nativeElement.querySelector('#inputP').blur()
+            this.elementRef.nativeElement.querySelector('#inputU').blur()
+            this.mensajeClaveErronea("El Usuario o la Clave introducidos no son correctos")
+          }
+          if (datos.token != null) {
+            console.log("acceso correcto")
+            let userM = this.inputUsuario
+            let passM = this.inputPassword
+            sessionStorage['user'] = datos.user
+            console.log(userM + passM)
+            sessionStorage['token'] = datos.token;
+            this.sinActividad = false
+            let cont = this.elementRef.nativeElement.querySelector('.contenido')
+            cont.innerHTML = "Bienvenido"
+            cont.classList.add('bg-success')
+            console.log(datos.token)
+          }
+          modalConectando.classList.add('oculto')
+          body.removeAttribute("style")
+
+        }
+        )
+
+    }, 1000)
+  }
+  deslogarse() {
+    sessionStorage.clear()
+    this.logado = false
+  }
+  limpiarFormulario() {
+    this.inputUsuario = ""
+    this.inputPassword = ""
+  }
+  colorearBotonLoginAlPulsar() {
+    this.limpiarFormulario()
+    let btnLogin = this.elementRef.nativeElement.querySelector('.btnLogin');
+    if (btnLogin.classList.contains('collapsed')) {
+      btnLogin.classList.remove('pulsado');
+    }
+    else {
+      btnLogin.classList.add('pulsado');
+      this.claveErronea = false
+    }
+  }
+  mensajeClaveErronea(mensaje: String) {
+    this.claveErronea = true
+    this.mensajeClave = mensaje
+  }
+
+  getFees() {
+    console.log("Sacando Fees con token: " + sessionStorage['token'])
+    this.documentType.getDatos("http://localhost:8080/once/documentsTypes")
+    .subscribe({
+      next: (response: any) => {
+        console.log("status ok:"+response.status)
+        console.log(response)
+        response._embedded.fees.forEach((element: any) => {
+          console.log(element.current)
+        });
+      }, 
+      error: (error: any) =>{
+        console.log("status ko:"+error.status)
+      }
+    })
+  }
+
+  meterTokenfalso() {
+    sessionStorage['token'] = "EsteEsUnTokenFalso"
+    console.log("token: " + sessionStorage['token'])
+  }
 
 }
