232f27dc9a0af71d6e1ab17acf499b7e4635b5e5
David Lira <yolira@msn.com>
2023-06-08 20:24:26 +0200

    Act

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
+//import { LoginComponent } from './login/login.component';
 import { ProfileComponent } from './componentes/profile/profile.component';
 import { ProductComponent } from './componentes/product/product.component';
 
 const routes: Routes = [
-  {path: 'login', component: LoginComponent },
+  //{path: 'login', component: LoginComponent },
   {path: 'product', component: ProductComponent },
   {path: 'productdata', component: ProductComponent },
   {path: 'profile', component: ProfileComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+ // imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 
