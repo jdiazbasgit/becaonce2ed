9247e9a01b70267c2c86cdcd4b2ddd358db8f3f3
Your Name <you@example.com>
2023-06-09 10:00:58 +0200

    sd

--- src/app/componentes/awards-fine/awards-fine.component.ts
+++ src/app/componentes/awards-fine/awards-fine.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { ProyectosService } from 'src/app/servicios/proyectos.service';
+import { ModalAwardsFineComponent } from '../modal-awards-fine/modal-awards-fine.component';
 
 @Component({
   selector: 'app-awards-fine',
   templateUrl: './awards-fine.component.html',
   styleUrls: ['./awards-fine.component.css']
 })
-export class AwardsFineComponent {
 
+export class AwardsFineComponent implements OnInit {
+
+  @ViewChild(ModalAwardsFineComponent) modal: any
+  id: number = 0
+  titulo: string;
+  descripciones: any[];
+  mensaje: string = ""
+
+  @Input() evetoDeHijo: string = ""
+  constructor(private service: ProyectosService) {
+    this.titulo = "TIPO DE PREMIOS Y MULTAS"
+    this.descripciones = [];
+  }
+
+  eliminar(id: any) {
+    if (confirm("Esta seguro de borrar el tipo de documento")) {
+      this.service.delete("http://localhost:8080/once/awardFine/" + id)
+        .subscribe((dato: boolean) => {
+          if (!dato) {
+            this.mensaje = "Se ha borrado correctamente"
+            this.ngOnInit();
+          }
+          else
+            this.mensaje = "El registro no se ha borrado"
+        })
+    }
+
+  }
+
+  realizarComunicacion(event: any) {
+    //this.evetoDeHijo = event.salida
+    this.mensaje = ""
+    if (event.salida === "OK")
+      this.ngOnInit();
+  }
+
+  ngOnInit(): void {
+
+    this.descripciones = []
+    this.service.getDatos("http://localhost:8080/once/awardFine/")
+      .subscribe((datos: any) => {
+        this.descripciones = datos._embedded.documentsTypes;
+      })
+  }
+
+  modificar(descripcion: any) {
+    this.mensaje = ""
+    let ruta = descripcion.links.self.href
+    this.modal.id = parseInt(ruta.subscribe(ruta.lastIndexOf("/") + 1))
+    //console.log(this.id)
+  }
 }
