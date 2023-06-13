c934ff49de5cbcb0854e1c0ac017f1d66fc8e619
DevSniffer <you@example.com>
2023-06-13 04:17:29 +0200

    a

--- src/app/panel-administrador/panel-administrador.component.ts
+++ src/app/panel-administrador/panel-administrador.component.ts
@@ -1,6 +1,6 @@
 import { Component, ElementRef } from '@angular/core';
 import { ProyectosService } from '../servicios/proyectos.service';
-import { concatMap, forkJoin } from 'rxjs';
+import { Observable, concat, concatMap, delay, forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-panel-administrador',
@@ -102,7 +102,7 @@ export class PanelAdministradorComponent {
           })
 
           if (this.mappingNombres.length >= 1) {
-            this.service.getDatos(this.url + "mappingFKDescriptions")
+            this.service.getDatos(this.url + "mappingFKDescriptions").pipe(delay(1000))
               .subscribe({
                 next: (mapping) => {
                   console.log("status ok:" + mapping.status)
@@ -112,15 +112,16 @@ export class PanelAdministradorComponent {
                     if (this.linksForaneosTabla.includes(mapped.table)) {
 
                       console.log(mapped.table)
-                      this.service.getDatos(this.url + mapped.table)
+                      this.service.getDatos(this.url + mapped.table).pipe(delay(1000))
                         .subscribe({
                           next: (t) => {
                             this.linkForaneoAka.push(t._embedded[mapped.table])
-                            console.log(t._embedded.users[0])
+                            //console.log(t._embedded.users[0])
                             //console.log(table[mapped.description])
-                          },
+                            console.log(this.linkForaneoAka + "guayy1")
+                          }
                         })
-                      console.log(this.linkForaneoAka + "guayy")
+                      console.log(this.linkForaneoAka + "guayy2")
                     }
 
 
@@ -131,6 +132,9 @@ export class PanelAdministradorComponent {
                   this.mappingNombres.forEach((mn: any) => {
                     console.log("guuuuu " + mn)
                   })
+                  this.linkForaneoAka.forEach((lfk: any) => {
+                    console.log(lfk)
+                  }) 
                 }
               })
           }
@@ -157,33 +161,148 @@ export class PanelAdministradorComponent {
       })
   }
 
-  /*ejemplo*/
-  fetchData(): void {
-    let countries
-    this.service
-      .getDatos('URL_PRIMERA_LLAMADA')
-      .pipe(
-        concatMap((acronyms: string[]) => {
-          const observables: any = acronyms.map((acronym: string) =>
-            this.service.getDatos('URL/' + acronym)
-          );
 
-          return forkJoin([observables]);
+
+
+  /*ejemplo*/
+  fetchData(nombre: string) {
+    this.datosBrutos = []
+    this.mappingNombres = []
+    this.linksForaneosTabla = []
+    this.tablaAConsultar = nombre
+    this.mostrarTabla = true
+
+
+    this.service.getDatos(this.url + nombre)
+      .pipe(
+        concatMap((response: any) => {
+          console.log("status ok:" + response.subscribeStatus)
+          console.log(response)
+
+          let tablas = Object.keys(response._embedded) //aka profiles
+          let cabeceras = Object.keys(response._embedded[tablas[0]][0]) //aka profiles[0]
+          let tamanoDatos = Object.keys(response._embedded[tablas[0]]).length //aka profiles 0,1,2,3...
+          //cabeceras.unshift("id")
+          console.log(cabeceras)
+          console.log(tamanoDatos)
+          // tablas.forEach(tabla => {
+          //   cabeceras.push(" " + response[0].currencies[tabla].name)
+          // })
+          let links = Object.keys(response._embedded[tablas[0]][0]._links)
+          console.log(links)
+
+          let unaVuelta: boolean = true
+
+          for (let index = 0; index < tamanoDatos; index++) {
+            let linkself = response._embedded[tablas[0]][index]._links.self.href
+            let id = parseInt(linkself.substring(linkself.lastIndexOf("/") + 1))
+            let filaDatos: any = []
+            filaDatos.push(id)
+            cabeceras.forEach(cabecera => {
+              if (cabecera !== "_links")
+                filaDatos.push(response._embedded[tablas[0]][index][cabecera])
+            });
+            links.forEach(linkForaneo => {
+              if (linkForaneo !== "self") {
+                //console.log(response._embedded[tablas[0]][index]._links[linkForaneo])
+                let hrefLink = response._embedded[tablas[0]][index]._links[linkForaneo].href
+                let idLink = parseInt(hrefLink.substring(hrefLink.lastIndexOf("/") + 1))
+                console.log("cucuuuuu " + idLink)
+                console.log("cucuuuuu " + hrefLink)
+                filaDatos.push(idLink)
+                if (unaVuelta) {
+                  let enPartes = hrefLink.split("/");
+                  this.linksForaneosTabla.push(enPartes[enPartes.length - 2])
+                }
+
+              }
+            });
+            unaVuelta = false
+            this.datosBrutos.push(filaDatos)
+          }
+          console.log(this.datosBrutos)
+
+          cabeceras.unshift("id")
+          cabeceras.splice(cabeceras.length - 1) // aka sin _links
+          console.log(cabeceras)
+
+          this.propiedadesLocales = cabeceras
+          this.linksForaneos = links
+
+          links.forEach((link: string) => {
+            if (link !== "self") {
+              cabeceras.push(link)
+              console.log("cucuuuuu " + link)
+              this.mappingNombres.push(link)
+            }
+
+          })
+          this.cabecerasTabla = cabeceras
+          console.log(cabeceras)
+          // if (this.mappingNombres.length >= 1) {
+          // }
+          const observables: any = this.service.getDatos(this.url + "mappingFKDescriptions")
+
+          return forkJoin(observables);
+
         }),
-        concatMap((countryNames: string[]) => {
-          const observables: any = countryNames.map((countryName: string) =>
-            this.service.getDatos('URL2/' + countryName)
-          );
+        concatMap((response2: any) => {
+          const observables: any = response2.map((fk: string) => {
+            this.service.getDatos(this.url + "mappingFKDescriptions")
+            console.log(fk)
+          }
+          )
+
+          return forkJoin(observables);
+        }),
+        concatMap((response3: any) => {
+          const observables: any = response3.map((mapped: any) => {
+            if (this.linksForaneosTabla.includes(mapped.table)) {
+              console.log(mapped.table)
+              this.service.getDatos(this.url + mapped.table)
+                .subscribe({
+                  next: (t) => {
+                    this.linkForaneoAka.push(t._embedded[mapped.table])
+                    console.log(t._embedded.users[0])
+                    //console.log(table[mapped.description])
+                  },
+                })
+              console.log(this.linkForaneoAka + "guayy")
+            }
+          }
+          )
 
           return forkJoin(observables);
         })
       )
-      .subscribe((capitals: string[]) => {
-        countries = capitals;
+      .subscribe((datos: any) => {
+        //countries = capitals;
+        //console.log(datos)
+        //console.log(this.linksForaneosTabla)
+        // datos.forEach((mapped: any) => {
+        //   if (this.linksForaneosTabla.includes(mapped.table)) {
 
-        // Puedes hacer cualquier otra operación con las capitales obtenidas
-        console.log(countries);
-      });
+        //     console.log(mapped.table)
+        //     this.service.getDatos(this.url + mapped.table)
+        //       .subscribe({
+        //         next: (t) => {
+        //           this.linkForaneoAka.push(t._embedded[mapped.table])
+        //           console.log(t._embedded.users[0])
+        //           //console.log(table[mapped.description])
+        //         },
+        //       })
+        //     console.log(this.linkForaneoAka + "guayy")
+        //   }
+
+
+
+        //   //this.mappingNombres.push(mapped)
+        // });
+
+        // this.mappingNombres.forEach((mn: any) => {
+        //   console.log("guuuuu " + mn)
+        // })
+      })
   }
 
 
