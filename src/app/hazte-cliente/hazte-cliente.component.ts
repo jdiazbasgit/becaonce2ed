762c375efc9829bf74de208508bb248c8c2cb830
Your Name <you@example.com>
2023-06-14 14:26:38 +0200

    seguir

--- src/app/hazte-cliente/hazte-cliente.component.ts
+++ src/app/hazte-cliente/hazte-cliente.component.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { UrlHandlingStrategy } from '@angular/router';
 import { ProfileService } from '../servicios/profile.service';
+import { ProyectosService } from '../servicios/proyectos.service';
 
 @Component({
   selector: 'app-hazte-cliente',
@@ -16,30 +17,51 @@ export class HazteClienteComponent {
   documentodeidentidad: string;
   telefono: string;
   email: string;
-  emailBaseDeDatos:Array<any>
-  services: any;
+  emailBaseDeDatos:string
+  telefonoBaseDatos:string
  
 
 
-  constructor(private profilServices:ProfileService) {
+  constructor(private profilServices:ProfileService, private services:ProyectosService) {
     this.nombre = "";
     this.apellidos = "";
     this.documentodeidentidad = "";
     this.telefono = "";
     this.email = "";
-    this.emailBaseDeDatos=[]
+    this.emailBaseDeDatos=""
+    this.telefonoBaseDatos=""
     
 
   }
+
+  ngOnInit(){
+    this.sacarEmail()
+  }
   sacarEmail(){
-    this.emailBaseDeDatos=[]
+    this.emailBaseDeDatos=""
+    this.telefonoBaseDatos=""
     this.services.getDatos("http://localhost:8080/once/profiles")
     .subscribe((datos:any)=>{
       console.log(datos)
-      for (let index = 0; index < datos.length; index++) {
-        console.log(datos[index]._embedded.profiles.email)
+      console.log(datos._embedded.profiles.length)
+      for (let index = 0; index < datos._embedded.profiles.length; index++) {
+        //console.log(datos._embedded.profiles[index].email)
+        console.log(index)
+        this.emailBaseDeDatos=datos._embedded.profiles[index].email
+        this.telefonoBaseDatos=datos._embedded.profiles[index].phone
+        console.log(this.telefonoBaseDatos)
+      console.log(this.emailBaseDeDatos + " estoy aqui")
+      if (this.emailBaseDeDatos==this.email ) {
+        console.log("ya existe") 
+      }
+      if (this.telefonoBaseDatos==this.telefono) {
+        console.log("el numero ya existe")
         
       }
+     
+      }
+      this.guardaDatos()
+      
     })
 
     
