9088ea03956297b43f94fdb22cf1db5f60bc8ce1
Your Name <you@example.com>
2023-06-15 13:32:02 +0200

    mirarSeguir

--- src/app/beans/ProfileBeans.ts
+++ src/app/beans/ProfileBeans.ts
@@ -1,14 +1,14 @@
 class ProfileBeans {
-    name!:string;
-    second_name!:string;
-  identification!:string;
-  phone!:string;
-  email!:string;
+  name!: string;
+  second_name!: string;
+  identification!: string;
+  phone!: string;
+  email!: string;
   identificationType!: string;
   user!: string;
 
 
-  constructor(name:string,second_name:string,identification:string,phone:string,email:string, identificationType:string,user:string){
+  constructor(name: string, second_name: string, identification: string, phone: string, email: string, identificationType: string, user: string) {
     this.name;
     this.second_name;
     this.identification;
--- src/app/hazte-cliente/hazte-cliente.component.ts
+++ src/app/hazte-cliente/hazte-cliente.component.ts
@@ -17,83 +17,96 @@ export class HazteClienteComponent {
   documentodeidentidad: string;
   telefono: string;
   email: string;
-  emailBaseDeDatos:string
-  telefonoBaseDatos:string
-  usuario:string
-  clave:string
- 
+  emailBaseDeDatos: string
+  telefonoBaseDatos: string
+  usuarioBaseDatos: string
+  clave: string
+  usuario: string
+  tipoDeDocumento:string
 
 
-  constructor(private profilServices:ProfileService,private services:ProyectosService) {
+
+  constructor(private profilServices: ProfileService, private services: ProyectosService) {
     this.nombre = "";
     this.apellidos = "";
     this.documentodeidentidad = "";
     this.telefono = "";
     this.email = "";
-    this.emailBaseDeDatos=""
-    this.telefonoBaseDatos=""
+    this.emailBaseDeDatos = ""
+    this.telefonoBaseDatos = ""
+    this.usuarioBaseDatos = ""
+    this.clave = ""
     this.usuario=""
-    this.clave=""
-    
+    this.tipoDeDocumento=""
 
   }
 
-  ngOnInit(){
-    this.comprobarDatos()
+  ngOnInit() {
+    this.comprobarDatos();
   }
-  comprobarDatos(){
-    this.usuario=""
-    this.clave=""
-    this.emailBaseDeDatos=""
-    this.telefonoBaseDatos=""
+
+  comprobarDatos() {
+    this.usuarioBaseDatos = '';
+    this.clave = '';
+    this.emailBaseDeDatos = '';
+    this.telefonoBaseDatos = '';
+
     this.services.getDatos("http://localhost:8080/once/profiles")
-    .subscribe((datos:any)=>{
-      console.log(datos)
-      console.log(datos._embedded.profiles.length)
+      .subscribe((datos: any) => {
+        console.log(datos);
+        console.log(datos._embedded.profiles.length);
 
-      datos._embedded.profiles.forEach((profile:any) => {
-        console.log(profile.email)
-        console.log(profile)
-        this.emailBaseDeDatos=profile.email
-        this.telefonoBaseDatos=profile.phone
-        console.log(this.telefonoBaseDatos)
-     // console.log(this.emailBaseDeDatos + " estoy aqui")
-      if (this.emailBaseDeDatos==this.email ) {
-        console.log("el email ya existe") 
-      }
-      if (this.telefonoBaseDatos==this.telefono) {
-        console.log("el numero ya existe")
-     
-      }
-        
+        datos._embedded.profiles.forEach((profile: any) => {
+          console.log(profile.email);
+          console.log(profile);
+          this.emailBaseDeDatos = profile.email;
+          this.telefonoBaseDatos = profile.phone;
+          console.log(this.telefonoBaseDatos);
+
+          if (this.emailBaseDeDatos == this.email) {
+            console.log("El email ya existe");
+          }
+          if (this.telefonoBaseDatos == this.telefono) {
+            console.log("El número ya existe");
+          }
+        });
       });
-       
-    })
-    this.services.getDatos("http://localhost:8080/once/users")
-    .subscribe((datos:any)=>{
-      console.log(datos + "usuario")
-      console.log(datos._embedded.users.user)
-    })
 
-    
+    this.services.getDatos("http://localhost:8080/once/users")
+      .subscribe((datos: any) => {
+        console.log(datos);
+        console.log(datos._embedded.users);
+
+        datos._embedded.users.forEach((users: any) => {
+          this.usuarioBaseDatos = users.user;
+          console.log(`${this.usuarioBaseDatos} este es el cliente`);
+
+          if (this.usuarioBaseDatos == this.usuario) {
+            
+          }
+        });
+      });
   }
 
+  guardaDatos() {
 
 
-  guardaDatos (){
-    
+    this.profilServices.saveOrUpdate("http://localhost:8080/once/profiles",
+    new ProfileBeans(this.nombre,this.apellidos,this.documentodeidentidad,this.telefono,this.email,this.tipoDeDocumento,this.usuario))
+    const clienteGuardado = {
+      name: this.nombre,
+      second_name: this.apellidos,
+     identification: this.documentodeidentidad,
+      phone: this.telefono,
+      email: this.email,
+      identificationType:this.tipoDeDocumento
+      ,user:this.usuario
+      
 
-    this.profilServices.saveOrUpdate("http://localhost:8080/once/profiles","")
-    const clienteGuardado={
-    name:this.nombre,
-    second_name:this.apellidos,
-  identification:this.documentodeidentidad,
-  phone:this.telefono,
-  email:this.email,
 
 
-  };
-}
+    };
+  }
 
 }
 
