1682310158a085a0b2f95852abc40c0ba314ac82
Elruedas001 <116722622+Elruedas001@users.noreply.github.com>
2023-06-16 14:02:37 +0200

    Llamada al listado de movimientos de la base de datos y creación de beans de Description y de CurrentAccount

---
+++ src/app/beans/currentAccountBean.ts
@@ -0,0 +1,15 @@
+class CurrentAccountBean {
+    number: number = 0;
+    calendar: any;
+    fee: any;
+    typeAccount: any;
+    user: any;
+
+    constructor(number: number, calendar: any, fee: any, typeAccount: any, user: any) {
+        this.number = number
+        this.calendar = calendar
+        this.fee = fee
+        this.typeAccount = typeAccount
+        this.user = user
+    }
+}
--- src/app/movimientos/movimientos.component.ts
+++ src/app/movimientos/movimientos.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TransactionService } from '../servicios/transaction.service';
+
 
 @Component({
   selector: 'app-movimientos',
   templateUrl: './movimientos.component.html',
   styleUrls: ['./movimientos.component.css']
 })
-export class MovimientosComponent {
+export class MovimientosComponent implements OnInit {
+  operaciones: any[] = [];
 
+  constructor(private transactionService: TransactionService) {}
+
+  ngOnInit(): void {
+    this.obtenerMovimientos();
+  }
+
+  urlTransaction:string="http://localhost:8080/once/transactions"
+  obtenerMovimientos() {
+    this.transactionService.getDatos(this.urlTransaction).subscribe(
+      (datos: any) => {
+        this.operaciones = datos._embedded.transactions;
+      }
+    );
+  }
 }
