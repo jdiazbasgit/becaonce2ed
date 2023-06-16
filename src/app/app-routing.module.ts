d6569b34af0adc0d3f027b551fadc31572932476
LuisFer-DesignForAll <luisfer.designforall@gmail.com>
2023-06-16 14:13:14 +0200

    componente home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,8 +6,10 @@ import { ProfileComponent } from './componentes/profile/profile.component';
 import { OfferComponent } from './componentes/offer/offer.component';
 import { ConfigurationComponent } from './componentes/configuration/configuration.component';
 import { CategoryComponent } from './componentes/category/category.component';
+import { HomeComponent } from './componentes/home/home.component';
 
 const routes: Routes = [
+  {path: '', component: HomeComponent },
   {path: 'login', component: LoginComponent },
   {path: 'product', component: ProductComponent },
   {path: 'profile', component: ProfileComponent },
