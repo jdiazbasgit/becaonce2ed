6f59aded5170628c86bb8a745e625d81b4a3f783
jdiazbasgit <git@studiotapal.es>
2023-06-21 13:29:58 +0200

    servicio patch

--- src/app/servicios/proyectos.service.ts
+++ src/app/servicios/proyectos.service.ts
@@ -19,5 +19,8 @@ export class ProyectosService {
   delete(url:string):Observable<any>{
     return this.httpClient.delete(url);
   }
+  patch(url:string,user:string):Observable<any>{
+    return this.httpClient.patch(url,user);
+  }
 
 }
