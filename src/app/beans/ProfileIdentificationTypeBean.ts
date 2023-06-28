a0b51cf2aa6910975af33c5ae5c0af2591bf24f7
Your Name <you@example.com>
2023-06-28 14:23:39 +0200

    seguir

---
+++ src/app/beans/ProfileIdentificationTypeBean.ts
@@ -0,0 +1,15 @@
+import IdentificationTypeBean from "./IdentificationTypeBean";
+
+ class ProfileIdentificationTypeBen {
+
+    identificationType:number 
+	 identification: string;
+
+
+     constructor(identificationType:number,identification:string){
+     this.identificationType=identificationType
+     this.identification=identification
+ }
+
+ }
+ export default ProfileIdentificationTypeBen
