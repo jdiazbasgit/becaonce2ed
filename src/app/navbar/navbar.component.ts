18abc7f4beb64e5989c3e90bd2622140fc12ad1b
Your Name <you@example.com>
2023-06-28 08:57:14 +0200

    mnb

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -26,3 +27,23 @@ function highlightActiveButton(activeButtonId: string) {
   } 
 }
 
+@Directive({
+  selector: '[highlightActiveButton]'
+})
+export class HighlightActiveButtonDirective implements OnInit {
+  constructor(private router: Router, private el: ElementRef, private renderer: Renderer2) {}
+
+    ngOnInit() {
+      this.router.events.subscribe(event => {
+        if (event instanceof NavigationEnd) {
+          const currentRoute = this.router.url.slice(1);
+
+          if (currentRoute === this.el.nativeElement.id) {
+            this.renderer.addClass(this.el.nativeElement, 'active');
+          } else {
+            this.renderer.removeClass(this.el.nativeElement, 'active');
+          } 
+        }
+      });
+    }
+  }
