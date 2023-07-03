85098965c8fc138d4bbad46c1d6a4c6ff49b69e5
LuisFer-DesignForAll <luisfer.designforall@gmail.com>
2023-07-03 14:15:47 +0200

    Test

--- src/test/java/once/curso/proyectotienda/ConfigurationTest.java
+++ src/test/java/once/curso/proyectotienda/ConfigurationTest.java
@@ -3,6 +3,7 @@ package once.curso.proyectotienda;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,48 +30,68 @@ public class ConfigurationTest {
 	@Order(1)
 	@Test
 	public void testFindAll() {
+		
 		List<Configuration> configurations = (List<Configuration>) getConfigurationService().findAll();
-		assertNotEquals(0, configurations.size());
+		assertNotEquals(0, configurations.size()); // TestFindId
+		
 	}
 
 	@Order(2)
 	@Test
-	public void testSaveFindDeleteId() {
-		Long contadorRegistro = getConfigurationService().count();
+	public void testExistsById() {
 
-		Configuration configurationNuevo = new Configuration();
-		configurationNuevo.setStockAlarm(1111);
-		contadorRegistro++;
+		Configuration configurationPrueba = new Configuration();
+		configurationPrueba.setStockAlarm(1111);
+		getConfigurationService().save(configurationPrueba);
 
-		getConfigurationService().save(configurationNuevo);
-		assertEquals(contadorRegistro, getConfigurationService().count());
-		
-		Configuration configurationRecuperado = getConfigurationService().findById(configurationNuevo.getId()).get();
-		contadorRegistro--;
-		getConfigurationService().delete(configurationRecuperado);
-		assertEquals(contadorRegistro, getConfigurationService().count());
+		Integer numero = configurationPrueba.getId();
+		System.out.println("numero: "+numero);
+		assertTrue(getConfigurationService().existsById(numero)); //TestExists
 
 	}
 
 	@Order(3)
 	@Test
-	public void testSaveAll() {
+	public void testSaveFindDeleteId() {
+		
+		Long contadorRegistro = getConfigurationService().count();
+
+		Configuration configurationNuevo = new Configuration();
+		configurationNuevo.setStockAlarm(2222);
+		contadorRegistro++;
+
+		getConfigurationService().save(configurationNuevo);
+		assertEquals(contadorRegistro, getConfigurationService().count()); //TestSave
+		
+		Configuration configurationRecuperado = getConfigurationService().findById(configurationNuevo.getId()).get();
+		assertEquals(configurationNuevo.getStockAlarm(), configurationRecuperado.getStockAlarm()); //TestFind
+		contadorRegistro--;
+
+		getConfigurationService().delete(configurationRecuperado);
+		assertEquals(contadorRegistro, getConfigurationService().count()); //TestDelete
+
+	}
+
+	@Order(4)
+	@Test
+	public void testSaveFindDeleteAll() {
+		
 		Long contadorRegistro = getConfigurationService().count();
 
 		List<Configuration> configurations = new ArrayList<Configuration>();
 
 		Configuration configurationNuevo1 = new Configuration();
-		configurationNuevo1.setStockAlarm(2222);
+		configurationNuevo1.setStockAlarm(3333);
 		configurations.add(configurationNuevo1);
 		contadorRegistro++;
 
 		Configuration configurationNuevo2 = new Configuration();
-		configurationNuevo2.setStockAlarm(3333);
+		configurationNuevo2.setStockAlarm(4444);
 		configurations.add(configurationNuevo2);
 		contadorRegistro++;
 
 		getConfigurationService().saveAll(configurations);
-		assertEquals(contadorRegistro, getConfigurationService().count());
+		assertEquals(contadorRegistro, getConfigurationService().count()); //TestSaveAll
 		
 		List<Integer> numeros = new ArrayList<Integer>();
 		
@@ -80,12 +101,17 @@ public class ConfigurationTest {
 		numeros.add(configurationNuevo2.getId());
 		contadorRegistro--;
 
+		Iterable<Configuration> configurationRecuperado = getConfigurationService().findAllById(numeros);
+		assertEquals(numeros.size(), configurationRecuperado().count()); //TestFindAll
+		contadorRegistro--;
+		
 		getConfigurationService().deleteAllById(numeros);
 		
-		assertEquals(contadorRegistro, getConfigurationService().count());
+		assertEquals(contadorRegistro, getConfigurationService().count()); //TestDeleteAll
+		
 	}
 
-	@Order(4)
+	@Order(5)
 	@Test
 	public void deleteByIDConfiguration() {
 		/*
@@ -122,25 +148,13 @@ public class ConfigurationTest {
 	 * assertNotEquals(getConfigurationService().count(), cantidadAlEmpezar + 0); }
 	 */
 
-	@Order(5)
+	@Order(6)
 	@Test
 	public void DelAllConfiguration() {
 		/*
 		 * getConfigurationService().deleteAll(); assertEquals(0,
 		 * getConfigurationService().count());
 		 */
-	}
-
-	@Order(6)
-	@Test
-	public void existsById() {
-		 //int numID = 229;
-		Configuration configuration = new Configuration();
-		configuration.setStockAlarm(1234);
-		getConfigurationService().existsById(229);
-		//getConfigurationService().save(configuration);
-		assertFalse(getConfigurationService().existsById(configuration.getId()));
-
 	}
 
 	@Order(7)
