fe05a883483d5d1498dd2e27f8e7c0a7bc12e1df
Your Name <you@example.com>
2023-07-07 10:10:59 +0200

    asd

--- src/app/productos-asociados/productos-asociados.component.ts
+++ src/app/productos-asociados/productos-asociados.component.ts
@@ -1,23 +1,25 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ProductosAsociadosComponent } from './productos-asociados.component';
+// import { ProductosAsociadosComponent } from './productos-asociados.component';
 
-describe('ProductosAsociadosComponent', () => {
-  let component: ProductosAsociadosComponent;
-  let fixture: ComponentFixture<ProductosAsociadosComponent>;
+// describe('ProductosAsociadosComponent', () => {
+//   let component: ProductosAsociadosComponent;
+//   let fixture: ComponentFixture<ProductosAsociadosComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ProductosAsociadosComponent ]
-    })
-    .compileComponents();
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       declarations: [ ProductosAsociadosComponent ]
+//     })
+//     .compileComponents();
 
-    fixture = TestBed.createComponent(ProductosAsociadosComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+//     fixture = TestBed.createComponent(ProductosAsociadosComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
+
+// export { ProductosAsociadosComponent };
