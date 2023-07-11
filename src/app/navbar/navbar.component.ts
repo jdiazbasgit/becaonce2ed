da40ad23f24df777da7014932e0f03bc00ede9d0
Your Name <you@example.com>
2023-07-11 18:53:43 +0200

    a

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -15,32 +15,32 @@ export class NavbarComponent {
   constructor(private elementRef: ElementRef, private router: Router) { }
 
   ngDoCheck() {
-    if (sessionStorage['cuenta'] != undefined && !this.cuentaElegida) {
-      this.cuentaElegida = true
-      this.cuenta = sessionStorage['cuenta']
+    //if (sessionStorage['cuenta'] != undefined && !this.cuentaElegida) {
+      //this.cuentaElegida = true
+      //this.cuenta = sessionStorage['cuenta']
       // this.elementRef.nativeElement.querySelector("#home").classList.remove("seleccionado")
-    }
+    //}
     // if (this.router.url == "/cuentas" && sessionStorage['cuenta'] != undefined){
     //   this.deselegirCuenta()
     // }
 
-    if (this.router.url == "/cuentas" && !this.elementRef.nativeElement.querySelector("#home").classList.contains("seleccionado")){
+    if (this.router.url == "/cuentas" && !this.elementRef.nativeElement.querySelector("#home").classList.contains("seleccionado")) {
       this.limpiarActives()
       this.elementRef.nativeElement.querySelector("#home").classList.add("seleccionado")
     }
-    if (this.router.url == "/detalles" && !this.elementRef.nativeElement.querySelector("#detalles").classList.contains("seleccionado")){
+    if (this.router.url == "/detalles" && !this.elementRef.nativeElement.querySelector("#detalles").classList.contains("seleccionado")) {
       this.limpiarActives()
       this.elementRef.nativeElement.querySelector("#detalles").classList.add("seleccionado")
     }
-    if (this.router.url == "/movimientos" && !this.elementRef.nativeElement.querySelector("#movimientos").classList.contains("seleccionado")){
+    if (this.router.url == "/movimientos" && !this.elementRef.nativeElement.querySelector("#movimientos").classList.contains("seleccionado")) {
       this.limpiarActives()
       this.elementRef.nativeElement.querySelector("#movimientos").classList.add("seleccionado")
     }
-    if (this.router.url == "/operaciones" && !this.elementRef.nativeElement.querySelector("#operaciones").classList.contains("seleccionado")){
+    if (this.router.url == "/operaciones" && !this.elementRef.nativeElement.querySelector("#operaciones").classList.contains("seleccionado")) {
       this.limpiarActives()
       this.elementRef.nativeElement.querySelector("#operaciones").classList.add("seleccionado")
     }
-    if (this.router.url == "/paneadministrador" && !this.elementRef.nativeElement.querySelector("#admin").classList.contains("seleccionado")){
+    if (this.router.url == "/paneadministrador" && !this.elementRef.nativeElement.querySelector("#admin").classList.contains("seleccionado")) {
       this.limpiarActives()
       this.elementRef.nativeElement.querySelector("#admin").classList.add("seleccionado")
     }
@@ -53,24 +53,28 @@ export class NavbarComponent {
   //   this.elementRef.nativeElement.querySelector("#admin")?.classList.remove("seleccionado")
   // }
 
-  abrirPanelAdmin(){
+  abrirPanelAdmin() {
     this.elementRef.nativeElement.querySelector("#home").classList.remove("seleccionado")
     this.elementRef.nativeElement.querySelector("#admin").classList.add("seleccionado")
   }
 
-  comprobarRolAdmin(){
-    if(sessionStorage['rol'] == "ROLE_ADMIN")
-    return true
+  comprobarRuta() {
+    return this.router.url
+  }
+
+  comprobarRolAdmin() {
+    if (sessionStorage['rol'] == "ROLE_ADMIN")
+      return true
     return false
   }
 
-  limpiarActives(){
+  limpiarActives() {
     let links = this.elementRef.nativeElement.querySelectorAll(".nav-link")
-    links.forEach((link:any) => {
+    links.forEach((link: any) => {
       link.classList.remove("seleccionado")
     });
   }
-  
+
 }
 
 
