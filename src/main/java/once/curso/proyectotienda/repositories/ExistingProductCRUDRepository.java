1c38592d3a9e45ed3827014fb04e4eaf15190ec4
jdiazbasgit <git@studiotapal.es>
2023-07-11 14:25:55 +0200

    a

--- src/main/java/once/curso/proyectotienda/repositories/ExistingProductCRUDRepository.java
+++ src/main/java/once/curso/proyectotienda/repositories/ExistingProductCRUDRepository.java
@@ -3,6 +3,7 @@ package once.curso.proyectotienda.repositories;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
@@ -18,4 +19,34 @@ public interface ExistingProductCRUDRepository extends PagingAndSortingRepositor
 	@Query(" from ExistingProduct e where e.subcategory.category.id = :categoryId AND e.subcategory.id = :subcategoryId")
 	public List<ExistingProduct> getExistingProductByCategoryAndSubCategory( int categoryId, int subcategoryId);
 
+	
+	default double countExistingProductByCategory(int category) {
+		
+		List<ExistingProduct> products = (List<ExistingProduct>) findAll();
+		
+		products.stream().filter(e->e.getSubcategory().getCategory().getId()==category).mapToDouble(e->e.getPrice()).forEach(p->System.out.println(p));
+		
+		return products.stream().filter(e->e.getSubcategory().getCategory().getId()==category).mapToDouble(e->e.getPrice()).sum();
+	}
+	
+	default List<ExistingProduct> a(List<ExistingProduct> productos, int category){
+		List<ExistingProduct> salida= new ArrayList<ExistingProduct>();
+		for (ExistingProduct existingProduct : salida) {
+			if(existingProduct.getSubcategory().getCategory().getId()==category)
+				salida.add(existingProduct);
+		}
+		
+		return salida;
+		
+	}
 }
+
+
+
+
+
+
+
+
+
+
---
+++ src/test/java/once/curso/proyectotienda/EjemploSumaTest.java
@@ -0,0 +1,33 @@
+package once.curso.proyectotienda;
+
+import org.junit.jupiter.api.TestMethodOrder;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import lombok.Data;
+import once.curso.proyectotienda.repositories.CardTypeCRUDRepository;
+import once.curso.proyectotienda.repositories.ExistingProductCRUDRepository;
+import once.curso.proyectotienda.services.CardTypeService;
+
+@SpringBootTest
+@Data
+@TestMethodOrder(value = OrderAnnotation.class)
+public class EjemploSumaTest {
+	
+	@Autowired
+	private ExistingProductCRUDRepository existingProductCRUDRepository;
+
+	@Test
+	public void probarSuma() {
+		double suma=getExistingProductCRUDRepository().countExistingProductByCategory(568);
+		System.out.println(suma);
+		assertTrue(true);
+		
+	}
+	
+}
