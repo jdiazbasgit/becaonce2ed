090a43848e066100bd43ed50dc4899ad7d029b8d
Your Name <you@example.com>
2023-07-12 14:20:37 +0200

    VACALOLA1

--- src/main/java/once/curso/proyectobanco/security/SecurityConfig.java
+++ src/main/java/once/curso/proyectobanco/security/SecurityConfig.java
@@ -20,7 +20,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests().antMatchers("/profiles/save").permitAll();
+		http.authorizeHttpRequests().antMatchers("/once/profiles/save").permitAll();
+		http.authorizeHttpRequests().antMatchers("/once/profiles/comprobar").permitAll();
 		http.authorizeHttpRequests().antMatchers("/haztecliente").permitAll();
 		http.authorizeHttpRequests().antMatchers("/login").permitAll();
 		http.authorizeHttpRequests().antMatchers("/once/identificationsTypes").permitAll();
