a14d0ef2cf7abaa7aaeb2b8bc6259baf22c900dd
DevSniffer <116722849+DevSniffer@users.noreply.github.com>
2023-07-12 13:24:14 +0200

    acceso libre a login, identificationsTypes y arrancar

--- src/main/java/once/curso/proyectobanco/security/SecurityConfig.java
+++ src/main/java/once/curso/proyectobanco/security/SecurityConfig.java
@@ -22,7 +22,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		
 		http.authorizeHttpRequests().antMatchers("/login").permitAll();
-		http.authorizeHttpRequests().antMatchers("/once/typesAccounts").permitAll();
+		http.authorizeHttpRequests().antMatchers("/once/identificationsTypes").permitAll();
 		http.authorizeHttpRequests().antMatchers("/once/arrancar").permitAll();
 		//http.authorizeHttpRequests().antMatchers("/once/*").permitAll();
 		//http.authorizeHttpRequests().antMatchers("/once/*/*").permitAll();
