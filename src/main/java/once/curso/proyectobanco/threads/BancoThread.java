e7f73cd64ebb0be71ddb379354f1020231c8b1d8
Elruedas001 <116722622+Elruedas001@users.noreply.github.com>
2023-07-14 14:15:24 +0200

    a

--- src/main/java/once/curso/proyectobanco/threads/BancoThread.java
+++ src/main/java/once/curso/proyectobanco/threads/BancoThread.java
@@ -1,6 +1,7 @@
 package once.curso.proyectobanco.threads;
 
 import java.awt.SystemColor;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,15 +9,29 @@ import org.springframework.stereotype.Component;
 
 import lombok.Data;
 import once.curso.proyectobanco.entities.Configuration;
+import once.curso.proyectobanco.entities.CurrentAccount;
+import once.curso.proyectobanco.entities.TransactionProgramer;
+import once.curso.proyectobanco.entities.User;
 import once.curso.proyectobanco.services.ConfigurationService;
+import once.curso.proyectobanco.services.CurrentAccountService;
+import once.curso.proyectobanco.services.TransactionProgramerService;
+import once.curso.proyectobanco.services.UserService;
 @Data
-@Component
 public class BancoThread extends Thread {
 	
 	private ConfigurationService configurationService;
 	
-	public BancoThread(ConfigurationService configurationService) {
+	private TransactionProgramerService transactionProgramerService;
+	
+	private CurrentAccountService currentAccountService;
+	
+	private UserService  userService;
+	
+	public BancoThread(ConfigurationService configurationService,CurrentAccountService currentAccountService, TransactionProgramerService transactionProgramerService,UserService  userService) {
 		this.configurationService = configurationService;
+		this.currentAccountService = currentAccountService;
+		this.transactionProgramerService = transactionProgramerService;
+		this.userService = userService;
 	}
 	
 	
@@ -26,22 +41,33 @@ public class BancoThread extends Thread {
 		int tiempo=configuraciones.get(0).getLiveTime();
 		long tiempoActual=System.currentTimeMillis();
 		while(true) {
+			GregorianCalendar fecha = new GregorianCalendar();
 			if(System.currentTimeMillis()>=tiempoActual+tiempo) {
 				tiempoActual=System.currentTimeMillis();
-				comprobarTransaccionesautomaticas();
-				comprobarPremiosMultas();
+				comprobarTransaccionesautomaticas(fecha);
+				comprobarPremiosMultas(fecha);
 			}
 		}
 	}
 
 
-	private void comprobarPremiosMultas() {
+	private void comprobarPremiosMultas(GregorianCalendar fecha) {
 	}
 
 
-	private void comprobarTransaccionesautomaticas() {
+	private void comprobarTransaccionesautomaticas(GregorianCalendar fecha) {
 
-		
+		List<User> users = (List<User>) getUserService().findAll();
+		users.stream().forEach(u->{
+			List<CurrentAccount> currentAccounts = getCurrentAccountService().getCurrentAccountsByUser(u.getUser());
+			currentAccounts.stream().forEach(c->{
+				List<TransactionProgramer> transactionProgramers = getTransactionProgramerService().getTransactionsByCurrentAccount(c.getId());
+				transactionProgramers.stream().forEach(t->{
+					
+				});
+			});
+		});
+
 	}
 
 
@@ -49,3 +75,23 @@ public class BancoThread extends Thread {
 	
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
