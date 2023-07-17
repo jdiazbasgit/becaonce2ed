edd98fa92b72641b1104e64f3bef0a744b6947b9
DevSniffer <116722849+DevSniffer@users.noreply.github.com>
2023-07-17 13:19:44 +0200

    vuelta al navbar Marcheline

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
-import { Component, ElementRef } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
@@ -7,69 +8,49 @@ import { Router } from '@angular/router';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
-  mostrarCuenta: boolean = false
-  cuenta: string = ""
+menuVisible: boolean = false;
 
-  constructor(private elementRef: ElementRef, private router: Router) { }
+constructor(private router: Router) {}
 
-  ngDoCheck() {
-    //console.log("hola soy ngDoCheck :)")
-    if (sessionStorage['cuenta'] != undefined || sessionStorage['cuenta'] == this.cuenta && !this.mostrarCuenta) {
-      this.mostrarCuenta = true
-      this.cuenta = sessionStorage['cuenta']
-    }
-
-    if (this.router.url == "/cuentas" || this.router.url == "/paneladministrador" && this.mostrarCuenta)
-       this.mostrarCuenta = false
-
-    if (this.router.url == "/cuentas" && !this.elementRef.nativeElement.querySelector("#home")?.classList.contains("seleccionado")) {
-      this.limpiarActives()
-      this.elementRef.nativeElement.querySelector("#home")?.classList.add("seleccionado")
-    }
-    if (this.router.url == "/detalles" && !this.elementRef.nativeElement.querySelector("#detalles")?.classList.contains("seleccionado")) {
-      this.limpiarActives()
-      this.elementRef.nativeElement.querySelector("#detalles")?.classList.add("seleccionado")
-    }
-    if (this.router.url == "/movimientos" && !this.elementRef.nativeElement.querySelector("#movimientos")?.classList.contains("seleccionado")) {
-      this.limpiarActives()
-      this.elementRef.nativeElement.querySelector("#movimientos")?.classList.add("seleccionado")
-    }
-    if (this.router.url == "/operaciones" && !this.elementRef.nativeElement.querySelector("#operaciones")?.classList.contains("seleccionado")) {
-      this.limpiarActives()
-      this.elementRef.nativeElement.querySelector("#operaciones")?.classList.add("seleccionado")
-    }
-    if (this.router.url == "/paneladministrador" && !this.elementRef.nativeElement.querySelector("#admin")?.classList.contains("seleccionado")) {
-      this.limpiarActives()
-      this.elementRef.nativeElement.querySelector("#admin")?.classList.add("seleccionado")
-    }
-  }
-
-  abrirPanelAdmin() {
-    this.elementRef.nativeElement.querySelector("#home").classList.remove("seleccionado")
-    this.elementRef.nativeElement.querySelector("#admin").classList.add("seleccionado")
-  }
-
-  comprobarRuta() {
-    return this.router.url
-  }
-
-  comprobarRolAdmin() {
-    if (sessionStorage['rol'] == "ROLE_ADMIN")
-      return true
-    return false
-  }
-
-  comprobarCuentaElegida() {
-    if (sessionStorage['cuenta'] != undefined)
-      return true
-    return false
-  }
-
-  limpiarActives() {
-    let links = this.elementRef.nativeElement.querySelectorAll(".nav-link")
-    links.forEach((link: any) => {
-      link.classList.remove("seleccionado")
-    });
-  }
-
+toggleMenu(): void {
+  this.menuVisible = !this.menuVisible;
 }
+
+isRouteActive(route: string): boolean {
+  return this.router.isActive(route, true);
+}
+}
+
+function highlightActiveButton(activeButtonId: string) {
+  const buttons = document.querySelectorAll('.nav-link button');
+
+  buttons.forEach(button => {
+    button.classList.remove('active');
+  });
+
+  const activeButton = document.getElementById(activeButtonId);
+  if (activeButton) {
+    activeButton.classList.add('active');
+  } 
+}
+
+@Directive({
+  selector: '[highlightActiveButton]'
+})
+export class HighlightActiveButtonDirective implements OnInit {
+  constructor(private router: Router, private el: ElementRef, private renderer: Renderer2) {}
+
+    ngOnInit() {
+      this.router.events.subscribe(event => {
+        if (event instanceof NavigationEnd) {
+          const currentRoute = this.router.url.slice(1);
+
+          if (currentRoute === this.el.nativeElement.id) {
+            this.renderer.addClass(this.el.nativeElement, 'active');
+          } else {
+            this.renderer.removeClass(this.el.nativeElement, 'active');
+          } 
+        }
+      });
+    }
+  }
