93511ea54a54e048e9a8c1386a73590a0bf1f27f
DevSniffer <116722849+DevSniffer@users.noreply.github.com>
2023-07-18 14:25:25 +0200

    a

--- src/main/java/once/curso/proyectobanco/restcontrollers/MappingFKDescriptionRestController.java
+++ src/main/java/once/curso/proyectobanco/restcontrollers/MappingFKDescriptionRestController.java
@@ -3,14 +3,10 @@ package once.curso.proyectobanco.restcontrollers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.Data;
-import once.curso.proyectobanco.entities.Description;
 import once.curso.proyectobanco.entities.MappingFKDescription;
 import once.curso.proyectobanco.services.MappingFKDescriptionService;
 
@@ -28,6 +24,11 @@ public class MappingFKDescriptionRestController {
 	@GetMapping(value = "/mappingFKDescriptions")
 	public Iterable<MappingFKDescription> findAll() {
 		return getMappingFKDescriptionService().findAll();
+	}
+	
+	@GetMapping("/obtenerTablas")
+	public Iterable<String> getTablas(){
+		return getMappingFKDescriptionService().getTables();
 	}
 	
 }
