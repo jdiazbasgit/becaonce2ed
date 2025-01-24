05c48285b38d3c7ab92fbebd9dc098ecb90c89b1
David Lira <yolira@msn.com>
2025-01-24 15:06:54 +0100

    Limpieza

--- src/app/componentes/nav/nav.component.ts
+++ src/app/componentes/nav/nav.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { ModalLoginComponent } from '../modal-login/modal-login.component';
 import { AuthService } from 'src/app/servicios/auth.service';
 
 @Component({
